6ac558c2383d1b90dae2070e6f24deea496acb70
gulatim <gulatim@ncias-d1508-v.nci.nih.gov>
2008-10-27 13:57:54 +0000

    updating build script

--- pro-ctcae/buildWeb.sh
+++ pro-ctcae/buildWeb.sh
@@ -1,13 +1,9 @@
 export CATALINA_HOME=/Users/saurabhagrawal/tomcats/ae-tomcat-5.5.26
 export JPDA_ADDRESS=9000
 echo [refreshDEV]: $CATALINA_HOME
-cd core
-mvn clean
-mvn install -Dmaven.test.skip=true
-cd ..
+ant publish-all -Dskip.test=true -Doffline=true
 cd web
-
-mvn compile war:inplace catman:go -Dmanager.username=admin -Dmanager.password=admin -Dmanager.url=http://localhost:8040/manager/
+ant -f ivy-build.xml deploy-ctcae -Doffline=true
 echo [refreshDEV]:  'sleeping 5 seconds'
 sleep 5
 cd ..
