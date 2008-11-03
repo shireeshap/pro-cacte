f94ed04f42371a6ff1033a12644188b7183f485e
agrawals <agrawals@ncias-d1508-v.nci.nih.gov>
2008-11-03 13:30:01 +0000

    updating org

--- pro-ctcae/core/src/main/java/gov/nih/nci/ctcae/core/query/OrganizationQuery.java
+++ pro-ctcae/core/src/main/java/gov/nih/nci/ctcae/core/query/OrganizationQuery.java
@@ -1,12 +1,12 @@
 package gov.nih.nci.ctcae.core.query;
 
 /**
- * @author  Vinay Kumar
+ * @author Vinay Kumar
  * @crated Oct 7, 2008
  */
 public class OrganizationQuery extends AbstractQuery {
 
-    private static String queryString = "SELECT o from Organization o order by o.id";
+    private static String queryString = "SELECT distinct(o) from Organization o order by o.id";
 
     private static String ORGANIZATION_NAME = "name";
 
@@ -22,9 +22,10 @@ public class OrganizationQuery extends AbstractQuery {
         andWhere("lower(o.name) LIKE :" + ORGANIZATION_NAME);
         setParameter(ORGANIZATION_NAME, searchString);
     }
+
     public void filterByOrganizationNameOrNciInstituteCode(final String text) {
         String searchString = "%" + text.toLowerCase() + "%";
-        andWhere(String.format("(lower(o.name) LIKE :%s or lower(o.nciInstituteCode) LIKE :%s)" , ORGANIZATION_NAME,NCI_CODE));
+        andWhere(String.format("(lower(o.name) LIKE :%s or lower(o.nciInstituteCode) LIKE :%s)", ORGANIZATION_NAME, NCI_CODE));
         setParameter(ORGANIZATION_NAME, searchString);
         setParameter(NCI_CODE, searchString);
     }
