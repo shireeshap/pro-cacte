1783f2021333d1b0dfa260258e50bd2d29712642
kumarvi <kumarvi@ncias-d1508-v.nci.nih.gov>
2008-11-04 16:39:28 +0000

    adding test cases.

--- pro-ctcae/web/src/test/java/gov/nih/nci/ctcae/web/table/SortRowsCallbackImplTest.java
+++ pro-ctcae/web/src/test/java/gov/nih/nci/ctcae/web/table/SortRowsCallbackImplTest.java
@@ -1,17 +1,15 @@
 package gov.nih.nci.ctcae.web.table;
 
-import gov.nih.nci.ctcae.web.WebTestCase;
 import gov.nih.nci.ctcae.core.domain.Study;
+import gov.nih.nci.ctcae.core.domain.StudyCoordinatingCenter;
+import gov.nih.nci.ctcae.core.domain.Organization;
 import org.extremecomponents.table.core.TableModel;
 import org.extremecomponents.table.core.TableModelImpl;
 import org.extremecomponents.table.context.HttpServletRequestContext;
 import org.extremecomponents.table.bean.Table;
-import org.extremecomponents.table.bean.Row;
 import org.extremecomponents.table.limit.TableLimit;
 import org.extremecomponents.table.limit.ModelLimitFactory;
 
-import java.util.Collection;
-import java.util.Map;
 import java.util.List;
 import java.util.ArrayList;
 
@@ -46,18 +44,14 @@ public class SortRowsCallbackImplTest extends AbstractTableModelTestCase {
         Table table = model.getTableInstance();
         model.addTable(table);
 
-        study1 = new Study();
-        study2 = new Study();
-        study3 = new Study();
         string1 = "String 1";
-
-        study1.setShortTitle(string1);
-
         string2 = "another String";
-        study2.setShortTitle(string2);
-
         string3 = "third String";
-        study3.setShortTitle(string3);
+
+        study1 = createStudy(string1);
+        study2 = createStudy(string2);
+        study3 = createStudy(string3);
+
 
         rows = new ArrayList();
         rows.add(study1);
@@ -68,14 +62,61 @@ public class SortRowsCallbackImplTest extends AbstractTableModelTestCase {
 
     }
 
+    private Study createStudy(String string) {
+        Study study = new Study();
+        study.setShortTitle(string);
+        StudyCoordinatingCenter studyCoordinatingCenter1 = new StudyCoordinatingCenter();
+        Organization organization1 = new Organization();
+        organization1.setName(string);
+        studyCoordinatingCenter1.setOrganization(organization1);
+        study.setStudyCoordinatingCenter(studyCoordinatingCenter1);
+
+        return study;
+    }
+
     public void testSortInASCOrder() throws Exception {
         limit = new TableLimit(new ModelLimitFactory(model));
         model.setLimit(limit);
         callback.sortRows(model, rows);
-        callback.sortRows(model, rows);
         assertEquals("must  sort rows in asc order", string2, rows.get(0).getShortTitle());
         assertEquals("must  sort rows in asc order", string1, rows.get(1).getShortTitle());
         assertEquals("must  sort rows in asc order", string3, rows.get(2).getShortTitle());
+
+
+    }
+
+    public void testSortInASCOrderForCollection() throws Exception {
+        parameterMap.put("ec_s_studyCoordinatingCenter.organization.name", "asc");
+
+        context = new HttpServletRequestContext(request, parameterMap);
+        model = new TableModelImpl(context);
+        Table table = model.getTableInstance();
+        model.addTable(table);
+        limit = new TableLimit(new ModelLimitFactory(model));
+        model.setLimit(limit);
+
+        callback.sortRows(model, rows);
+        assertEquals("must  sort rows in asc order", string2, rows.get(0).getStudyCoordinatingCenter().getOrganization().getName());
+        assertEquals("must  sort rows in asc order", string1, rows.get(1).getStudyCoordinatingCenter().getOrganization().getName());
+        assertEquals("must  sort rows in asc order", string3, rows.get(2).getStudyCoordinatingCenter().getOrganization().getName());
+
+
+    }
+
+    public void testSortInDescOrderForCollection() throws Exception {
+        parameterMap.put("ec_s_studyCoordinatingCenter.organization.name", "desc");
+
+        context = new HttpServletRequestContext(request, parameterMap);
+        model = new TableModelImpl(context);
+        Table table = model.getTableInstance();
+        model.addTable(table);
+        limit = new TableLimit(new ModelLimitFactory(model));
+        model.setLimit(limit);
+
+        callback.sortRows(model, rows);
+        assertEquals("must  sort rows in asc order", string3, rows.get(0).getStudyCoordinatingCenter().getOrganization().getName());
+        assertEquals("must  sort rows in asc order", string1, rows.get(1).getStudyCoordinatingCenter().getOrganization().getName());
+        assertEquals("must  sort rows in asc order", string2, rows.get(2).getStudyCoordinatingCenter().getOrganization().getName());
 
 
     }
