6a5f145ca23f9fd095aabc9aa14394f7239d8224
kumarvi <kumarvi@ncias-d1508-v.nci.nih.gov>
2008-11-11 18:16:45 +0000

    adding test cases..

---
+++ pro-ctcae/web/src/test/java/gov/nih/nci/ctcae/web/study/AddStudySiteControllerTest.java
@@ -0,0 +1,41 @@
+package gov.nih.nci.ctcae.web.study;
+
+import gov.nih.nci.ctcae.web.ControllersUtils;
+import gov.nih.nci.ctcae.web.WebTestCase;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * @author Vinay Kumar
+ * @crated Oct 18, 2008
+ */
+public class AddStudySiteControllerTest extends WebTestCase {
+
+    private AddStudySiteController controller;
+
+    private CreateStudyController createStudyController;
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        controller = new AddStudySiteController();
+        createStudyController = new CreateStudyController();
+
+
+    }
+
+    public void testSupportedMethod() {
+        assertEqualArrays("only get is supported", new String[]{"GET"}, controller.getSupportedMethods());
+    }
+
+    public void testHandleRequest() throws Exception {
+
+        createStudyController.handleRequest(request, response);
+        Object command = ControllersUtils.getFormCommand(request, createStudyController);
+        assertNotNull("command must present in session", command);
+
+        ModelAndView modelAndView = controller.handleRequestInternal(request, response);
+        assertNotNull("index must be present", modelAndView.getModelMap().get("index"));
+
+
+    }
+}
