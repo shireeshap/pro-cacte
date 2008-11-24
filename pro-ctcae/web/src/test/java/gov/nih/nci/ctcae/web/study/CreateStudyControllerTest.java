c5c09650e3fb7dd266b5d4968f39179a9ebcda41
kumarvi <kumarvi@ncias-d1508-v.nci.nih.gov>
2008-11-24 21:43:00 +0000

    adding test case

---
+++ pro-ctcae/web/src/test/java/gov/nih/nci/ctcae/web/study/CreateStudyControllerTest.java
@@ -0,0 +1,68 @@
+package gov.nih.nci.ctcae.web.study;
+
+import gov.nih.nci.ctcae.core.domain.Study;
+import gov.nih.nci.ctcae.core.repository.StudyRepository;
+import gov.nih.nci.ctcae.web.ControllersUtils;
+import gov.nih.nci.ctcae.web.WebTestCase;
+import gov.nih.nci.ctcae.web.validation.validator.WebControllerValidator;
+import gov.nih.nci.ctcae.web.validation.validator.WebControllerValidatorImpl;
+import static org.easymock.EasyMock.expect;
+import static org.easymock.EasyMock.isA;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Map;
+
+/**
+ * @author Vinay Kumar
+ * @crated Nov 5, 2008
+ */
+public class CreateStudyControllerTest extends WebTestCase {
+	private CreateStudyController controller;
+	private WebControllerValidator validator;
+	private StudyRepository studyRepository;
+
+	private Study study;
+
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+		controller = new CreateStudyController();
+		validator = new WebControllerValidatorImpl();
+		studyRepository = registerMockFor(StudyRepository.class);
+		controller.setStudyRepository(studyRepository);
+		controller.setWebControllerValidator(validator);
+
+		study = new Study();
+	}
+
+	public void testGetRequest() throws Exception {
+		request.setMethod("GET");
+		ModelAndView modelAndView = controller.handleRequest(request, response);
+		Map model = modelAndView.getModel();
+		Object command = model.get("command");
+		assertNotNull("must find command object", command);
+		assertTrue(command instanceof StudyCommand);
+
+	}
+
+	public void testPostRequest() throws Exception {
+		request.setMethod("GET");
+
+		controller.handleRequest(request, response);
+		assertNotNull("must have command in session", ControllersUtils.getStudyCommand(request));
+		request.setMethod("POST");
+		request.setParameter("_finish", "true");
+		expect(studyRepository.save(isA(Study.class))).andReturn(study);
+
+		replayMocks();
+
+		ModelAndView modelAndView = controller.handleRequest(request, response);
+
+		verifyMocks();
+		Map model = modelAndView.getModel();
+		Object command = model.get("command");
+		assertNotNull("must  find command object", command);
+
+
+	}
+}
