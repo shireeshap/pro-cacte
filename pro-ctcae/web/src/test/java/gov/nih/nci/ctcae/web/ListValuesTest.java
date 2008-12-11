62efb27a6e894c8d880a8a23ba598964733b3c7f
kumarvi <kumarvi@ncias-d1508-v.nci.nih.gov>
2008-12-11 20:27:39 +0000

    adding test cases

---
+++ pro-ctcae/web/src/test/java/gov/nih/nci/ctcae/web/ListValuesTest.java
@@ -0,0 +1,35 @@
+package gov.nih.nci.ctcae.web;
+
+import junit.framework.TestCase;
+
+import java.util.List;
+
+/**
+ * @author Vinay Kumar
+ * @crated Dec 11, 2008
+ */
+public class ListValuesTest extends TestCase {
+
+	private ListValues listValues;
+
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+		listValues = new ListValues();
+
+	}
+
+	public void testParticipantSearchType() {
+		List<ListValues> participantSearchType = listValues.getParticipantSearchType();
+		assertEquals("must be 3 search type", Integer.valueOf(3), Integer.valueOf(participantSearchType.size()));
+		ListValues values = participantSearchType.get(0);
+	}
+
+	public void testGetterAndSetter() {
+		listValues.setCode("code");
+		listValues.setDesc("desc");
+
+		assertEquals("code", listValues.getCode());
+		assertEquals("desc", listValues.getDesc());
+	}
+}
--- pro-ctcae/web/src/test/java/gov/nih/nci/ctcae/web/participant/CreateParticipantControllerTest.java
+++ pro-ctcae/web/src/test/java/gov/nih/nci/ctcae/web/participant/CreateParticipantControllerTest.java
@@ -1,83 +1,79 @@
 package gov.nih.nci.ctcae.web.participant;
 
-import gov.nih.nci.ctcae.core.domain.CRF;
-import gov.nih.nci.ctcae.core.domain.ProCtcQuestion;
+import gov.nih.nci.ctcae.core.Fixture;
 import gov.nih.nci.ctcae.core.domain.Organization;
 import gov.nih.nci.ctcae.core.domain.Study;
 import gov.nih.nci.ctcae.core.repository.FinderRepository;
-import gov.nih.nci.ctcae.core.repository.ParticipantRepository;
 import gov.nih.nci.ctcae.core.repository.OrganizationRepository;
+import gov.nih.nci.ctcae.core.repository.ParticipantRepository;
 import gov.nih.nci.ctcae.core.repository.StudyRepository;
-import gov.nih.nci.ctcae.core.Fixture;
 import gov.nih.nci.ctcae.web.WebTestCase;
-import gov.nih.nci.ctcae.web.ListValues;
-import gov.nih.nci.ctcae.web.form.CreateFormCommand;
 import static org.easymock.EasyMock.expect;
-import static org.easymock.classextension.EasyMock.replay;
-import static org.easymock.classextension.EasyMock.verify;
 import org.springframework.validation.BindException;
 
-import java.util.Map;
-import java.util.List;
 import java.util.ArrayList;
+import java.util.Map;
 
 /**
  * @author Vinay Kumar
  * @crated Nov 4, 2008
  */
 public class CreateParticipantControllerTest extends WebTestCase {
-    private CreateParticipantController createParticipantController;
-    private ParticipantCommand participantCommand;
-    private BindException errors;
-    private ParticipantRepository participantRepository;
-    private FinderRepository finderRepository;
-    private OrganizationRepository organizationRepository;
-    private StudyRepository studyRepository;
+	private CreateParticipantController createParticipantController;
+	private ParticipantCommand participantCommand;
+	private BindException errors;
+	private ParticipantRepository participantRepository;
+	private FinderRepository finderRepository;
+	private OrganizationRepository organizationRepository;
+	private StudyRepository studyRepository;
+	private ArrayList<Organization> organizations;
 
 
-    @Override
-    protected void setUp() throws Exception {
-        super.setUp();
-        createParticipantController = new CreateParticipantController();
-        participantCommand = (ParticipantCommand) createParticipantController.formBackingObject(request);
-        errors = registerMockFor(BindException.class);
-        participantRepository = registerMockFor(ParticipantRepository.class);
-        finderRepository = registerMockFor(FinderRepository.class);
-        organizationRepository = registerMockFor(OrganizationRepository.class);
-        studyRepository = registerMockFor(StudyRepository.class);
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+		createParticipantController = new CreateParticipantController();
+		participantCommand = (ParticipantCommand) createParticipantController.formBackingObject(request);
+		errors = registerMockFor(BindException.class);
+		participantRepository = registerMockFor(ParticipantRepository.class);
+		finderRepository = registerMockFor(FinderRepository.class);
+		organizationRepository = registerMockFor(OrganizationRepository.class);
+		studyRepository = registerMockFor(StudyRepository.class);
 
 
-        createParticipantController.setFinderRepository(finderRepository);
-        createParticipantController.setParticipantRepository(participantRepository);
-        createParticipantController.setOrganizationRepository(organizationRepository);
+		createParticipantController.setFinderRepository(finderRepository);
+		createParticipantController.setParticipantRepository(participantRepository);
+		createParticipantController.setOrganizationRepository(organizationRepository);
 
-        Organization organization = Fixture.createOrganization("TEST", "TEST");
-        Study study = Fixture.createStudyWithStudySite("Test", "Test", "Test", organization);
-    }
+		Organization organization = Fixture.createOrganization("TEST", "TEST");
+		Study study = Fixture.createStudyWithStudySite("Test", "Test", "Test", organization);
+		organizations = new ArrayList<Organization>();
+		organizations.add(nci);
+	}
 
-    public void testConstructor() {
-        assertEquals("participant/createParticipant", createParticipantController.getFormView());
-    }
+	public void testConstructor() {
+		assertEquals("participant/createParticipant", createParticipantController.getFormView());
+	}
 
-    public void testFormBackingObject() throws Exception {
-        assertNotNull(participantCommand);
-        assertNotNull(participantCommand.getParticipant());
-        assertEquals(0, participantCommand.getSiteId());
-        assertNull(participantCommand.getSiteName());
-        assertNull(participantCommand.getStudyId());
-    }
+	public void testFormBackingObject() throws Exception {
+		assertNotNull(participantCommand);
+		assertNotNull(participantCommand.getParticipant());
+		assertEquals(0, participantCommand.getSiteId());
+		assertNull(participantCommand.getSiteName());
+		assertNull(participantCommand.getStudyId());
+	}
 
-    public void testReferenceData() throws Exception {
-        expect(organizationRepository.findOrganizationsForStudySites()).andReturn(new ArrayList<Organization>());
-        replayMocks();
-        Map data = createParticipantController.referenceData(request, participantCommand, errors);
-        verifyMocks();
+	public void testReferenceData() throws Exception {
+		expect(organizationRepository.findOrganizationsForStudySites()).andReturn(organizations);
+		replayMocks();
+		Map data = createParticipantController.referenceData(request, participantCommand, errors);
+		verifyMocks();
 
-        assertNotNull(data.get("genders"));
-        assertNotNull(data.get("ethnicities"));
-        assertNotNull(data.get("races"));
-        assertNotNull(data.get("studysites"));
+		assertNotNull(data.get("genders"));
+		assertNotNull(data.get("ethnicities"));
+		assertNotNull(data.get("races"));
+		assertNotNull(data.get("studysites"));
 
-    }
+	}
 
 }
