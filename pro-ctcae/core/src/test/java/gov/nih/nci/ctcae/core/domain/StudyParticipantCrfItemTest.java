feae171b0d70a953127a91078fbb069ac39bdbca
gulatim <gulatim@ncias-d1508-v.nci.nih.gov>
2008-12-11 20:52:16 +0000

    code coverage

--- pro-ctcae/core/src/test/java/gov/nih/nci/ctcae/core/domain/StudyParticipantCrfItemTest.java
+++ pro-ctcae/core/src/test/java/gov/nih/nci/ctcae/core/domain/StudyParticipantCrfItemTest.java
@@ -26,12 +26,40 @@ public class StudyParticipantCrfItemTest extends TestCase {
         studyParticipantCrfItem.setCrfItem(crfItem);
         studyParticipantCrfItem.setProCtcValidValue(proCtcValidValue);
         studyParticipantCrfItem.setStudyParticipantCrfSchedule(studyParticipantCrfSchedule);
+        studyParticipantCrfItem.setId(1);
 
         assertEquals(crfItem, studyParticipantCrfItem.getCrfItem());
+        assertEquals(new Integer(1), studyParticipantCrfItem.getId());
         assertEquals(proCtcValidValue, studyParticipantCrfItem.getProCtcValidValue());
         assertEquals(studyParticipantCrfSchedule, studyParticipantCrfItem.getStudyParticipantCrfSchedule());
 
-        
+
+    }
+
+    public void testEqulasHashCode() {
+        studyParticipantCrfItem = new StudyParticipantCrfItem();
+        StudyParticipantCrfItem studyParticipantCrfItem2 = new StudyParticipantCrfItem();
+
+        assertEquals(studyParticipantCrfItem2.hashCode(), studyParticipantCrfItem.hashCode());
+
+        CrfItem crfItem = new CrfItem();
+        ProCtcValidValue proCtcValidValue = new ProCtcValidValue();
+        StudyParticipantCrfSchedule studyParticipantCrfSchedule = new StudyParticipantCrfSchedule();
+
+        studyParticipantCrfItem.setCrfItem(crfItem);
+        studyParticipantCrfItem.setProCtcValidValue(proCtcValidValue);
+        studyParticipantCrfItem.setStudyParticipantCrfSchedule(studyParticipantCrfSchedule);
+        studyParticipantCrfItem.setId(1);
+
+
+        assertFalse(studyParticipantCrfItem.equals(studyParticipantCrfItem2));
+        studyParticipantCrfItem2.setCrfItem(crfItem);
+        studyParticipantCrfItem2.setProCtcValidValue(proCtcValidValue);
+        studyParticipantCrfItem2.setStudyParticipantCrfSchedule(studyParticipantCrfSchedule);
+        studyParticipantCrfItem2.setId(1);
+        assertTrue(studyParticipantCrfItem.equals(studyParticipantCrfItem2));
+
+
     }
 
 
