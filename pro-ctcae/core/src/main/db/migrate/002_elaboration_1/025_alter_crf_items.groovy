3a69a461140bf48b9530765ab7cbbe52dd7602da
kumarvi <kumarvi@ncias-d1508-v.nci.nih.gov>
2008-12-11 18:44:28 +0000

    fixing groovy script

--- pro-ctcae/core/src/main/db/migrate/002_elaboration_1/025_alter_crf_items.groovy
+++ pro-ctcae/core/src/main/db/migrate/002_elaboration_1/025_alter_crf_items.groovy
@@ -1,8 +1,10 @@
 class AlterCrfItems extends edu.northwestern.bioinformatics.bering.Migration {
 	void up() {
-
-		execute('ALTER TABLE crf_items ALTER COLUMN crf_id DROP NOT NULL')
-
+		if (databaseMatches("oracle")) {
+			execute('ALTER TABLE crf_items modify crf_id integer NULL')
+		} else {
+			execute('ALTER TABLE crf_items ALTER COLUMN crf_id DROP NOT NULL')
+		}
 	}
 
 	void down() {
