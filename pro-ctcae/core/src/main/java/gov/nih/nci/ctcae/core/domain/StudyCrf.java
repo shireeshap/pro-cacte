adfcf947a44070e417fee8297d480fe6ebdc3708
gulatim <gulatim@ncias-d1508-v.nci.nih.gov>
2008-12-12 15:51:11 +0000

    removed equals and hashcode

--- pro-ctcae/core/src/main/java/gov/nih/nci/ctcae/core/domain/StudyCrf.java
+++ pro-ctcae/core/src/main/java/gov/nih/nci/ctcae/core/domain/StudyCrf.java
@@ -89,41 +89,6 @@ public class StudyCrf extends BaseVersionable {
 
 	}
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((crf == null) ? 0 : crf.hashCode());
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		result = prime * result + ((study == null) ? 0 : study.hashCode());
-		return result;
-	}
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		StudyCrf other = (StudyCrf) obj;
-		if (crf == null) {
-			if (other.crf != null)
-				return false;
-		} else if (!crf.equals(other.crf))
-			return false;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		if (study == null) {
-			if (other.study != null)
-				return false;
-		} else if (!study.equals(other.study))
-			return false;
-		return true;
-	}
 
 }
