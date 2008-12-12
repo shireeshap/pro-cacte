6e7f6feed7a839fce80c608197fb543ec4079ffc
kumarvi <kumarvi@ncias-d1508-v.nci.nih.gov>
2008-12-12 13:57:54 +0000

    adding test cases

--- pro-ctcae/web/src/test/java/gov/nih/nci/ctcae/web/tools/ObjectToolsTest.java
+++ pro-ctcae/web/src/test/java/gov/nih/nci/ctcae/web/tools/ObjectToolsTest.java
@@ -1,5 +1,6 @@
 package gov.nih.nci.ctcae.web.tools;
 
+import gov.nih.nci.ctcae.core.exception.CtcAeSystemException;
 import junit.framework.TestCase;
 
 import java.util.Arrays;
@@ -11,80 +12,91 @@ import java.util.List;
  */
 
 public class ObjectToolsTest extends TestCase {
-    public void testBuildReduced() throws Exception {
-        Bean src = new Bean("A", 1, 14L, (byte) 2);
-        Bean reduced = ObjectTools.reduce(src, "string", "primitiveLong");
+	public void testBuildReduced() throws Exception {
+		Bean src = new Bean("A", 1, 14L, (byte) 2);
+		Bean reduced = ObjectTools.reduce(src, "string", "primitiveLong");
 
-        assertBean(src.getString(), null, src.getPrimitiveLong(), (byte) 0, reduced);
-    }
+		assertBean(src.getString(), null, src.getPrimitiveLong(), (byte) 0, reduced);
+	}
 
-    public void testReduceAll() throws Exception {
-        List<Bean> src = Arrays
-                .asList(new Bean("A", 1, 2, (byte) 3), new Bean("E", 3, 6, (byte) 9));
-        List<Bean> reduced = ObjectTools.reduceAll(src, "integer", "primitiveLong");
-        assertEquals("Wrong number of beans copied", 2, reduced.size());
-        assertBean(null, 1, 2, (byte) 0, reduced.get(0));
-        assertBean(null, 3, 6, (byte) 0, reduced.get(1));
-    }
+	public void testReduceAll() throws Exception {
+		List<Bean> src = Arrays
+			.asList(new Bean("A", 1, 2, (byte) 3), new Bean("E", 3, 6, (byte) 9));
+		List<Bean> reduced = ObjectTools.reduceAll(src, "integer", "primitiveLong");
+		assertEquals("Wrong number of beans copied", 2, reduced.size());
+		assertBean(null, 1, 2, (byte) 0, reduced.get(0));
+		assertBean(null, 3, 6, (byte) 0, reduced.get(1));
+	}
 
-    private static void assertBean(String expectedString, Integer expectedInteger,
-                                   long expectedLong, byte expectedByte, Bean actual) {
-        assertEquals("Wrong string", expectedString, actual.getString());
-        assertEquals("Wrong integer", expectedInteger, actual.getInteger());
-        assertEquals("Wrong long", expectedLong, actual.getPrimitiveLong());
-        assertEquals("Wrong byte", expectedByte, actual.getPrimitiveByte());
-    }
+	public void testReduceThrowsCtcAeSystemException() throws Exception {
+		List src = Arrays
+			.asList(1);
+		try {
+			ObjectTools.reduceAll(src, "invalid property");
+			fail("Failed to instantiate java.lang.Integer");
+		} catch (CtcAeSystemException e) {
 
-    private static class Bean {
-        private String string;
+		}
+	}
 
-        private Integer integer;
+	private static void assertBean(String expectedString, Integer expectedInteger,
+								   long expectedLong, byte expectedByte, Bean actual) {
+		assertEquals("Wrong string", expectedString, actual.getString());
+		assertEquals("Wrong integer", expectedInteger, actual.getInteger());
+		assertEquals("Wrong long", expectedLong, actual.getPrimitiveLong());
+		assertEquals("Wrong byte", expectedByte, actual.getPrimitiveByte());
+	}
 
-        private long primitiveLong;
+	private static class Bean {
+		private String string;
 
-        private byte primitiveByte;
+		private Integer integer;
 
-        public Bean() {
-        }
+		private long primitiveLong;
 
-        public Bean(String string, Integer integer, long primitive, byte primitiveByte) {
-            this.string = string;
-            this.integer = integer;
-            this.primitiveLong = primitive;
-            this.primitiveByte = primitiveByte;
-        }
+		private byte primitiveByte;
 
-        public String getString() {
-            return string;
-        }
+		public Bean() {
+		}
 
-        public void setString(String string) {
-            this.string = string;
-        }
+		public Bean(String string, Integer integer, long primitive, byte primitiveByte) {
+			this.string = string;
+			this.integer = integer;
+			this.primitiveLong = primitive;
+			this.primitiveByte = primitiveByte;
+		}
 
-        public Integer getInteger() {
-            return integer;
-        }
+		public String getString() {
+			return string;
+		}
 
-        public void setInteger(Integer integer) {
-            this.integer = integer;
-        }
+		public void setString(String string) {
+			this.string = string;
+		}
 
-        public long getPrimitiveLong() {
-            return primitiveLong;
-        }
+		public Integer getInteger() {
+			return integer;
+		}
 
-        public void setPrimitiveLong(long primitiveLong) {
-            this.primitiveLong = primitiveLong;
-        }
+		public void setInteger(Integer integer) {
+			this.integer = integer;
+		}
 
-        public byte getPrimitiveByte() {
-            return primitiveByte;
-        }
+		public long getPrimitiveLong() {
+			return primitiveLong;
+		}
 
-        public void setPrimitiveByte(byte primitiveByte) {
-            this.primitiveByte = primitiveByte;
-        }
-    }
+		public void setPrimitiveLong(long primitiveLong) {
+			this.primitiveLong = primitiveLong;
+		}
+
+		public byte getPrimitiveByte() {
+			return primitiveByte;
+		}
+
+		public void setPrimitiveByte(byte primitiveByte) {
+			this.primitiveByte = primitiveByte;
+		}
+	}
 }
 
