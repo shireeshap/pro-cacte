85bfc06665bcad7ad70e7063eb311c7464107464
gulatim <gulatim@ncias-d1508-v.nci.nih.gov>
2008-12-12 15:58:11 +0000

    added equals and hashcode

--- pro-ctcae/core/src/main/java/gov/nih/nci/ctcae/core/domain/CrfItem.java
+++ pro-ctcae/core/src/main/java/gov/nih/nci/ctcae/core/domain/CrfItem.java
@@ -123,6 +123,42 @@ public class CrfItem extends BasePersistable {
 		return copiedCrfItem;
 	}
 
+	@Override
+	public boolean equals(final Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
 
+		final CrfItem crfItem = (CrfItem) o;
+
+		if (crf != null ? !crf.equals(crfItem.crf) : crfItem.crf != null) return false;
+		if (crfItemAllignment != crfItem.crfItemAllignment) return false;
+		if (displayOrder != null ? !displayOrder.equals(crfItem.displayOrder) : crfItem.displayOrder != null)
+			return false;
+		if (instructions != null ? !instructions.equals(crfItem.instructions) : crfItem.instructions != null)
+			return false;
+		if (proCtcQuestion != null ? !proCtcQuestion.equals(crfItem.proCtcQuestion) : crfItem.proCtcQuestion != null)
+			return false;
+		if (responseRequired != null ? !responseRequired.equals(crfItem.responseRequired) : crfItem.responseRequired != null)
+			return false;
+
+		return true;
+	}
+
+	@Override
+	public int hashCode() {
+		int result = displayOrder != null ? displayOrder.hashCode() : 0;
+		result = 31 * result + (crf != null ? crf.hashCode() : 0);
+		result = 31 * result + (proCtcQuestion != null ? proCtcQuestion.hashCode() : 0);
+		result = 31 * result + (responseRequired != null ? responseRequired.hashCode() : 0);
+		result = 31 * result + (instructions != null ? instructions.hashCode() : 0);
+		result = 31 * result + (crfItemAllignment != null ? crfItemAllignment.hashCode() : 0);
+		return result;
+	}
+
+	//    @Override
+//    public String toString() {
+//        return "[DISPLAY ORDER: CRF : QUESTION] " + displayOrder + " : "
+//                + crf.getTitle() + " : " + proCtcQuestion.getQuestionText();
+//    }
 
 }
