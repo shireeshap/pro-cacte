8f8296823e19bbb05cb61c3f912712a004d6f98a
kumarvi <kumarvi@ncias-d1508-v.nci.nih.gov>
2008-12-15 16:03:44 +0000

    adding test cases for equals and hash code

--- pro-ctcae/core/src/test/java/gov/nih/nci/ctcae/core/domain/StudyParticipantCrfScheduleTest.java
+++ pro-ctcae/core/src/test/java/gov/nih/nci/ctcae/core/domain/StudyParticipantCrfScheduleTest.java
@@ -9,74 +9,101 @@ import java.util.Date;
  * @crated Dec 12, 2008
  */
 public class StudyParticipantCrfScheduleTest extends TestCase {
-    private StudyParticipantCrfSchedule studyParticipantCrfSchedule;
+	private StudyParticipantCrfSchedule studyParticipantCrfSchedule;
 
-    public void testConstructor() {
-        studyParticipantCrfSchedule = new StudyParticipantCrfSchedule();
-        assertEquals(0, studyParticipantCrfSchedule.getStudyParticipantCrfItems().size());
-        assertNull(studyParticipantCrfSchedule.getId());
-        assertNull(studyParticipantCrfSchedule.getDueDate());
-        assertNull(studyParticipantCrfSchedule.getStartDate());
-        assertEquals(CrfStatus.SCHEDULED, studyParticipantCrfSchedule.getStatus());
-        assertNull(studyParticipantCrfSchedule.getStudyParticipantCrf());
-
-
-    }
-
-    public void testGetterAndSetter() {
-        studyParticipantCrfSchedule = new StudyParticipantCrfSchedule();
-        Date d = new Date();
-        studyParticipantCrfSchedule.setId(1);
-        studyParticipantCrfSchedule.setDueDate(d);
-        studyParticipantCrfSchedule.setStartDate(d);
-        studyParticipantCrfSchedule.setStatus(CrfStatus.INPROGRESS);
-        studyParticipantCrfSchedule.setStudyParticipantCrf(new StudyParticipantCrf());
-        studyParticipantCrfSchedule.addStudyParticipantCrfItem(null);
-
-        assertEquals(0, studyParticipantCrfSchedule.getStudyParticipantCrfItems().size());
-        assertEquals(d, studyParticipantCrfSchedule.getDueDate());
-        assertEquals(d, studyParticipantCrfSchedule.getStartDate());
-        assertEquals(CrfStatus.INPROGRESS, studyParticipantCrfSchedule.getStatus());
-        assertNotNull(studyParticipantCrfSchedule.getStudyParticipantCrf());
-
-        studyParticipantCrfSchedule.addStudyParticipantCrfItem(new StudyParticipantCrfItem());
-
-        assertEquals(1, studyParticipantCrfSchedule.getStudyParticipantCrfItems().size());
+	public void testConstructor() {
+		studyParticipantCrfSchedule = new StudyParticipantCrfSchedule();
+		assertEquals(0, studyParticipantCrfSchedule.getStudyParticipantCrfItems().size());
+		assertNull(studyParticipantCrfSchedule.getId());
+		assertNull(studyParticipantCrfSchedule.getDueDate());
+		assertNull(studyParticipantCrfSchedule.getStartDate());
+		assertEquals(CrfStatus.SCHEDULED, studyParticipantCrfSchedule.getStatus());
+		assertNull(studyParticipantCrfSchedule.getStudyParticipantCrf());
 
 
-    }
+	}
 
-    public void testEqulasHashCode() {
-        studyParticipantCrfSchedule = new StudyParticipantCrfSchedule();
-        StudyParticipantCrfSchedule studyParticipantCrfSchedule2 = new StudyParticipantCrfSchedule();
-        Date d = new Date();
-        StudyParticipantCrfItem studyParticipantCrfItem = new StudyParticipantCrfItem();
-        StudyParticipantCrf studyParticipantCrf = new StudyParticipantCrf();
+	public void testGetterAndSetter() {
+		studyParticipantCrfSchedule = new StudyParticipantCrfSchedule();
+		Date d = new Date();
+		studyParticipantCrfSchedule.setId(1);
+		studyParticipantCrfSchedule.setDueDate(d);
+		studyParticipantCrfSchedule.setStartDate(d);
+		studyParticipantCrfSchedule.setStatus(CrfStatus.INPROGRESS);
+		studyParticipantCrfSchedule.setStudyParticipantCrf(new StudyParticipantCrf());
+		studyParticipantCrfSchedule.addStudyParticipantCrfItem(null);
 
-        assertEquals(studyParticipantCrfSchedule.hashCode(), studyParticipantCrfSchedule2.hashCode());
-        assertEquals(studyParticipantCrfSchedule, studyParticipantCrfSchedule2);
+		assertEquals(0, studyParticipantCrfSchedule.getStudyParticipantCrfItems().size());
+		assertEquals(d, studyParticipantCrfSchedule.getDueDate());
+		assertEquals(d, studyParticipantCrfSchedule.getStartDate());
+		assertEquals(CrfStatus.INPROGRESS, studyParticipantCrfSchedule.getStatus());
+		assertNotNull(studyParticipantCrfSchedule.getStudyParticipantCrf());
 
-        studyParticipantCrfSchedule.setId(1);
-        studyParticipantCrfSchedule.setDueDate(d);
-        studyParticipantCrfSchedule.setStartDate(d);
-        studyParticipantCrfSchedule.setStatus(CrfStatus.INPROGRESS);
-        studyParticipantCrfSchedule.setStudyParticipantCrf(studyParticipantCrf);
-        studyParticipantCrfSchedule.addStudyParticipantCrfItem(studyParticipantCrfItem);
+		studyParticipantCrfSchedule.addStudyParticipantCrfItem(new StudyParticipantCrfItem());
 
-        assertFalse(studyParticipantCrfSchedule.equals(studyParticipantCrfSchedule2));
-
-        studyParticipantCrfSchedule2.setId(1);
-        studyParticipantCrfSchedule2.setDueDate(d);
-        studyParticipantCrfSchedule2.setStartDate(d);
-        studyParticipantCrfSchedule2.setStatus(CrfStatus.INPROGRESS);
-        studyParticipantCrfSchedule2.setStudyParticipantCrf(studyParticipantCrf);
-        studyParticipantCrfSchedule2.addStudyParticipantCrfItem(studyParticipantCrfItem);
-
-        assertEquals(studyParticipantCrfSchedule.hashCode(), studyParticipantCrfSchedule2.hashCode());
-        assertEquals(studyParticipantCrfSchedule, studyParticipantCrfSchedule2);
+		assertEquals(1, studyParticipantCrfSchedule.getStudyParticipantCrfItems().size());
 
 
-    }
+	}
+
+	public void testEqulasHashCode() {
+		studyParticipantCrfSchedule = new StudyParticipantCrfSchedule();
+		StudyParticipantCrfSchedule studyParticipantCrfSchedule2 = new StudyParticipantCrfSchedule();
+		Date d = new Date();
+		StudyParticipantCrfItem studyParticipantCrfItem = new StudyParticipantCrfItem();
+		StudyParticipantCrf studyParticipantCrf = new StudyParticipantCrf();
+
+		assertEquals(studyParticipantCrfSchedule.hashCode(), studyParticipantCrfSchedule2.hashCode());
+		assertEquals(studyParticipantCrfSchedule, studyParticipantCrfSchedule2);
+
+		studyParticipantCrfSchedule.setDueDate(d);
+		assertFalse(studyParticipantCrfSchedule.equals(studyParticipantCrfSchedule2));
+		studyParticipantCrfSchedule2.setDueDate(d);
+		assertEquals(studyParticipantCrfSchedule.hashCode(), studyParticipantCrfSchedule2.hashCode());
+		assertEquals(studyParticipantCrfSchedule, studyParticipantCrfSchedule2);
+
+
+		studyParticipantCrfSchedule.setStartDate(d);
+		assertFalse(studyParticipantCrfSchedule.equals(studyParticipantCrfSchedule2));
+		studyParticipantCrfSchedule2.setStartDate(d);
+		assertEquals(studyParticipantCrfSchedule.hashCode(), studyParticipantCrfSchedule2.hashCode());
+		assertEquals(studyParticipantCrfSchedule, studyParticipantCrfSchedule2);
+
+		studyParticipantCrfSchedule.setStatus(CrfStatus.INPROGRESS);
+		assertFalse(studyParticipantCrfSchedule.equals(studyParticipantCrfSchedule2));
+		studyParticipantCrfSchedule2.setStatus(CrfStatus.INPROGRESS);
+		assertEquals(studyParticipantCrfSchedule.hashCode(), studyParticipantCrfSchedule2.hashCode());
+		assertEquals(studyParticipantCrfSchedule, studyParticipantCrfSchedule2);
+
+
+		studyParticipantCrfSchedule.setStudyParticipantCrf(studyParticipantCrf);
+		assertFalse(studyParticipantCrfSchedule.equals(studyParticipantCrfSchedule2));
+		studyParticipantCrfSchedule2.setStudyParticipantCrf(studyParticipantCrf);
+		assertEquals(studyParticipantCrfSchedule.hashCode(), studyParticipantCrfSchedule2.hashCode());
+		assertEquals(studyParticipantCrfSchedule, studyParticipantCrfSchedule2);
+
+
+		studyParticipantCrfSchedule.addStudyParticipantCrfItem(studyParticipantCrfItem);
+		assertEquals("must not consider crf item", studyParticipantCrfSchedule.hashCode(), studyParticipantCrfSchedule2.hashCode());
+		assertEquals("must not consider crf item", studyParticipantCrfSchedule, studyParticipantCrfSchedule2);
+
+
+	}
+
+	public void testEqulasHashCodeMustNotConsiderId() {
+		studyParticipantCrfSchedule = new StudyParticipantCrfSchedule();
+		StudyParticipantCrfSchedule studyParticipantCrfSchedule2 = new StudyParticipantCrfSchedule();
+
+		studyParticipantCrfSchedule.setId(1);
+		assertEquals("must not consider id", studyParticipantCrfSchedule.hashCode(), studyParticipantCrfSchedule2.hashCode());
+		assertEquals("must not consider id", studyParticipantCrfSchedule, studyParticipantCrfSchedule2);
+
+		studyParticipantCrfSchedule2.setId(2);
+		assertEquals("must not consider id", studyParticipantCrfSchedule.hashCode(), studyParticipantCrfSchedule2.hashCode());
+		assertEquals("must not consider id", studyParticipantCrfSchedule, studyParticipantCrfSchedule2);
+
+
+	}
 
 
 }
