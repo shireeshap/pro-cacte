bad0224974855c31cb3a6656a9b3b75b10becff1
kumarvi <kumarvi@ncias-d1508-v.nci.nih.gov>
2008-12-15 15:36:28 +0000

    adding query test case

--- pro-ctcae/core/src/test/java/gov/nih/nci/ctcae/core/query/ClinicalStaffQueryTest.java
+++ pro-ctcae/core/src/test/java/gov/nih/nci/ctcae/core/query/ClinicalStaffQueryTest.java
@@ -8,42 +8,52 @@ import junit.framework.TestCase;
  */
 public class ClinicalStaffQueryTest extends TestCase {
 
-    public void testQueryConstructor() throws Exception {
-        ClinicalStaffQuery clinicalStaffQuery = new ClinicalStaffQuery();
-        clinicalStaffQuery.filterByClinicalStaffFirstName("John");
-        assertEquals("SELECT i from ClinicalStaff i WHERE lower(i.firstName) LIKE :firstName order by i.id",
-                clinicalStaffQuery.getQueryString());
+	public void testQueryConstructor() throws Exception {
+		ClinicalStaffQuery clinicalStaffQuery = new ClinicalStaffQuery();
+		clinicalStaffQuery.filterByClinicalStaffFirstName("John");
+		assertEquals("SELECT i from ClinicalStaff i WHERE lower(i.firstName) LIKE :firstName order by i.id",
+			clinicalStaffQuery.getQueryString());
 
-    }
+	}
 
-    public void testFilterByFirstName() throws Exception {
-        ClinicalStaffQuery clinicalStaffQuery = new ClinicalStaffQuery();
-        clinicalStaffQuery.filterByClinicalStaffFirstName("John");
-        assertEquals("SELECT i from ClinicalStaff i WHERE lower(i.firstName) LIKE :firstName order by i.id",
-                clinicalStaffQuery.getQueryString());
-        assertEquals("wrong number of parameters", clinicalStaffQuery.getParameterMap().size(), 1);
-        assertTrue("missing parameter name", clinicalStaffQuery.getParameterMap().containsKey("firstName"));
-        assertEquals("wrong parameter value", clinicalStaffQuery.getParameterMap().get("firstName"), "%john%");
-    }
+	public void testFilterByFirstName() throws Exception {
+		ClinicalStaffQuery clinicalStaffQuery = new ClinicalStaffQuery();
+		clinicalStaffQuery.filterByClinicalStaffFirstName("John");
+		assertEquals("SELECT i from ClinicalStaff i WHERE lower(i.firstName) LIKE :firstName order by i.id",
+			clinicalStaffQuery.getQueryString());
+		assertEquals("wrong number of parameters", clinicalStaffQuery.getParameterMap().size(), 1);
+		assertTrue("missing parameter name", clinicalStaffQuery.getParameterMap().containsKey("firstName"));
+		assertEquals("wrong parameter value", clinicalStaffQuery.getParameterMap().get("firstName"), "%john%");
+	}
 
-    public void testFilterByLastName() throws Exception {
-        ClinicalStaffQuery clinicalStaffQuery = new ClinicalStaffQuery();
-        clinicalStaffQuery.filterByClinicalStaffLastName("Dow");
-        assertEquals("SELECT i from ClinicalStaff i WHERE lower(i.lastName) LIKE :lastName order by i.id",
-                clinicalStaffQuery.getQueryString());
-        assertEquals("wrong number of parameters", clinicalStaffQuery.getParameterMap().size(), 1);
-        assertTrue("missing parameter name", clinicalStaffQuery.getParameterMap().containsKey("lastName"));
-        assertEquals("wrong parameter value", clinicalStaffQuery.getParameterMap().get("lastName"), "%dow%");
-    }
+	public void testFilterByNciIdentifier() throws Exception {
+		ClinicalStaffQuery clinicalStaffQuery = new ClinicalStaffQuery();
+		clinicalStaffQuery.filterByNciIdentifier("John");
+		assertEquals("SELECT i from ClinicalStaff i WHERE lower(i.nciIdentifier) LIKE :nciIdentifier order by i.id",
+			clinicalStaffQuery.getQueryString());
+		assertEquals("wrong number of parameters", clinicalStaffQuery.getParameterMap().size(), 1);
+		assertTrue("missing parameter name", clinicalStaffQuery.getParameterMap().containsKey("nciIdentifier"));
+		assertEquals("wrong parameter value", clinicalStaffQuery.getParameterMap().get("nciIdentifier"), "%john%");
+	}
 
-    public void testFilterByFullName() throws Exception {
-        ClinicalStaffQuery clinicalStaffQuery = new ClinicalStaffQuery();
-        clinicalStaffQuery.filterByClinicalStaffFirstName("John");
-        clinicalStaffQuery.filterByClinicalStaffLastName("Dow");
-        assertEquals("SELECT i from ClinicalStaff i WHERE lower(i.lastName) LIKE :lastName AND lower(i.firstName) LIKE :firstName order by i.id",
-                clinicalStaffQuery.getQueryString());
-        assertEquals("wrong number of parameters", clinicalStaffQuery.getParameterMap().size(), 2);
+	public void testFilterByLastName() throws Exception {
+		ClinicalStaffQuery clinicalStaffQuery = new ClinicalStaffQuery();
+		clinicalStaffQuery.filterByClinicalStaffLastName("Dow");
+		assertEquals("SELECT i from ClinicalStaff i WHERE lower(i.lastName) LIKE :lastName order by i.id",
+			clinicalStaffQuery.getQueryString());
+		assertEquals("wrong number of parameters", clinicalStaffQuery.getParameterMap().size(), 1);
+		assertTrue("missing parameter name", clinicalStaffQuery.getParameterMap().containsKey("lastName"));
+		assertEquals("wrong parameter value", clinicalStaffQuery.getParameterMap().get("lastName"), "%dow%");
+	}
 
-    }
+	public void testFilterByFullName() throws Exception {
+		ClinicalStaffQuery clinicalStaffQuery = new ClinicalStaffQuery();
+		clinicalStaffQuery.filterByClinicalStaffFirstName("John");
+		clinicalStaffQuery.filterByClinicalStaffLastName("Dow");
+		assertEquals("SELECT i from ClinicalStaff i WHERE lower(i.lastName) LIKE :lastName AND lower(i.firstName) LIKE :firstName order by i.id",
+			clinicalStaffQuery.getQueryString());
+		assertEquals("wrong number of parameters", clinicalStaffQuery.getParameterMap().size(), 2);
+
+	}
 
 }
