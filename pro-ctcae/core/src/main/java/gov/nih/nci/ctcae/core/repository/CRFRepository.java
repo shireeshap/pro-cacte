dc5c3c0341fbb928b2786ac199bda4bf4029f9ce
gulatim <gulatim@ncias-d1508-v.nci.nih.gov>
2008-12-23 21:43:33 +0000

    reverted versioning changes

--- pro-ctcae/core/src/main/java/gov/nih/nci/ctcae/core/repository/CRFRepository.java
+++ pro-ctcae/core/src/main/java/gov/nih/nci/ctcae/core/repository/CRFRepository.java
@@ -30,11 +30,7 @@ public class CRFRepository extends AbstractRepository<CRF, CRFQuery> {
     @Transactional(propagation = Propagation.REQUIRED, readOnly = false)
     public void updateStatusToReleased(CRF crf) {
 
-        SimpleDateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy");
-
-        CRF tmpCrf = null;
         crf.setStatus(CrfStatus.RELEASED);
-        Hashtable<Integer, List<StudyParticipantCrfSchedule>> newSchedulesHash = new Hashtable<Integer, List<StudyParticipantCrfSchedule>>();
 
         StudyCrf studyCrf = crf.getStudyCrf();
         if (studyCrf != null) {
@@ -42,60 +38,12 @@ public class CRFRepository extends AbstractRepository<CRF, CRFQuery> {
 
             for (StudySite studySite : study.getStudySites()) {
                 for (StudyParticipantAssignment studyParticipantAssignment : studySite.getStudyParticipantAssignments()) {
-                    for (StudyParticipantCrf studyParticipantCrf : studyParticipantAssignment.getStudyParticipantCrfs()) {
-
-                        boolean isParentCrf = false;
-                        tmpCrf = studyParticipantCrf.getStudyCrf().getCrf();
-                        while (tmpCrf.getNextVersionId() != null) {
-                            if (tmpCrf.getNextVersionId().equals(crf.getId())) {
-                                tmpCrf.setEffectiveEndDate(new Date(crf.getEffectiveStartDate().getTime() -  1000 * 60 * 60 *24));
-                                isParentCrf = true;
-                                break;
-                            } else {
-                                tmpCrf = findById(tmpCrf.getNextVersionId());
-                            }
-                        }
-
-                        if (isParentCrf) {
-                            List<StudyParticipantCrfSchedule> removeSchedules = new ArrayList<StudyParticipantCrfSchedule>();
-                            for (StudyParticipantCrfSchedule studyParticipantCrfSchedule : studyParticipantCrf.getStudyParticipantCrfSchedules()) {
-                                if (studyParticipantCrfSchedule.getStatus().equals(CrfStatus.SCHEDULED)) {
-                                    if (crf.getEffectiveStartDate().before(studyParticipantCrfSchedule.getStartDate()) ||
-                                            dateFormat.format(studyParticipantCrfSchedule.getStartDate()).equals(dateFormat.format(crf.getEffectiveStartDate()))) {
-                                        StudyParticipantCrfSchedule newSchedule = new StudyParticipantCrfSchedule();
-
-                                        newSchedule.setStartDate(studyParticipantCrfSchedule.getStartDate());
-                                        newSchedule.setDueDate(studyParticipantCrfSchedule.getDueDate());
-                                        newSchedule.setStatus(CrfStatus.SCHEDULED);
-                                        if (newSchedulesHash.get(studyParticipantAssignment.getId()) == null) {
-                                            newSchedulesHash.put(studyParticipantAssignment.getId(), new ArrayList<StudyParticipantCrfSchedule>());
-                                        }
-                                        newSchedulesHash.get(studyParticipantAssignment.getId()).add(newSchedule);
-                                        removeSchedules.add(studyParticipantCrfSchedule);
-                                    }
-                                }
-                            }
-                            for(StudyParticipantCrfSchedule studyParticipantCrfSchedule : removeSchedules){
-                                studyParticipantCrf.removeCrfSchedule(studyParticipantCrfSchedule);
-                            }
-                        }
-                    }
-                }
-                for (StudyParticipantAssignment studyParticipantAssignment : studySite.getStudyParticipantAssignments()) {
                     StudyParticipantCrf studyParticipantCrf = new StudyParticipantCrf();
                     studyParticipantCrf.setStudyParticipantAssignment(studyParticipantAssignment);
                     studyCrf.addStudyParticipantCrf(studyParticipantCrf);
-                    if (newSchedulesHash.containsKey(studyParticipantAssignment.getId())) {
-                        List<StudyParticipantCrfSchedule> newSchedules = newSchedulesHash.get(studyParticipantAssignment.getId());
-                        for (StudyParticipantCrfSchedule studyParticipantCrfSchedule : newSchedules) {
-                            studyParticipantCrf.addStudyParticipantCrfSchedule(studyParticipantCrfSchedule);
-                        }
-                    }
                 }
             }
         }
-        if (tmpCrf != null)
-            save(tmpCrf);
         save(crf);
     }
 
