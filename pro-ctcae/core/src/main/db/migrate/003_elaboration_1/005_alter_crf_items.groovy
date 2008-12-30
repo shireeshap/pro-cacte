e986b5b761c767d532516d259e87ac083ebd768c
gulatim <gulatim@ncias-d1508-v.nci.nih.gov>
2008-12-30 17:00:33 +0000

    pulling crfs from study

--- pro-ctcae/core/src/main/db/migrate/003_elaboration_1/005_alter_crf_items.groovy
+++ pro-ctcae/core/src/main/db/migrate/003_elaboration_1/005_alter_crf_items.groovy
@@ -8,7 +8,7 @@ class AlterCrfItemstoPageItem extends edu.northwestern.bioinformatics.bering.Mig
 
 
 		dropColumn("CRF_PAGE_ITEMS","crf_id")
-		addColumn("CRF_PAGE_ITEMS","crf_page_id","string",nullable:false) 
+		addColumn("CRF_PAGE_ITEMS","crf_page_id","integer",nullable:false) 
 		execute('ALTER TABLE CRF_PAGE_ITEMS ADD CONSTRAINT fk_crf_page_items_ques FOREIGN KEY (pro_ctc_question_id) REFERENCES PRO_CTC_QUESTIONS')
 		execute("ALTER TABLE CRF_PAGE_ITEMS ADD CONSTRAINT un_crf_page_terms UNIQUE (crf_page_id,pro_ctc_question_id)")
 		execute('ALTER TABLE CRF_PAGE_ITEMS ADD CONSTRAINT fk_crf_page_items_crf FOREIGN KEY (crf_page_id) REFERENCES CRF_PAGES')
