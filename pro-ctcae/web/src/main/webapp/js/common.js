97eccf65128489c8e7c4e75715ad4f6d95fab305
gulatim <gulatim@ncias-d1508-v.nci.nih.gov>
2009-01-02 16:41:54 +0000

    make it work on IE

--- pro-ctcae/web/src/main/webapp/js/common.js
+++ pro-ctcae/web/src/main/webapp/js/common.js
@@ -275,8 +275,7 @@ function initializeAutoCompleter(basename, name, id) {
     if (name != '') {
         $(basename + '-input').value = name;
         $(basename).value = id;
-        //commented below line to make it work on IE. Did not see any difference in look and feel after commenting
-        //$(basename + '-input').class = 'autocomplete';
+        $(basename + '-input').addClassName('autocomplete');
 
     }
 }
