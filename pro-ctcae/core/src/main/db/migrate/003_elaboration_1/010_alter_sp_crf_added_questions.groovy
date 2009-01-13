97a3d100c2953b9de7df96bb4c40f819379eaf1a
kumarvi <kumarvi@ncias-d1508-v.nci.nih.gov>
2009-01-13 19:11:53 +0000

    renaming groovy scripts

--- pro-ctcae/core/src/main/db/migrate/003_elaboration_1/010_alter_sp_crf_added_questions.groovy
+++
@@ -1,9 +0,0 @@
-class CreateAddedItems extends edu.northwestern.bioinformatics.bering.Migration {
-	     void up() {
-     			addColumn('sp_crf_added_questions','page_number', 'integer', nullable: true)
-     	}
-
-     	void down() {
-     	}
-
-}
