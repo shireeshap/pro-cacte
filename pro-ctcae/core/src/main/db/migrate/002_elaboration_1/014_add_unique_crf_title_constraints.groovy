1387cf56ef8c0ebceaa9497d9ec9e6e427768301
gulatim <gulatim@ncias-d1508-v.nci.nih.gov>
2009-01-19 16:06:33 +0000

    delete old groovy scripts

--- pro-ctcae/core/src/main/db/migrate/002_elaboration_1/014_add_unique_crf_title_constraints.groovy
+++
@@ -1,9 +0,0 @@
-class AddUniqueCrfTitleConstraints extends edu.northwestern.bioinformatics.bering.Migration {
-	void up() {
-		execute("ALTER TABLE CRFS ADD CONSTRAINT un_crfs_title UNIQUE (title)")
-	}
-
-	void down() {
-	}
-}
-
