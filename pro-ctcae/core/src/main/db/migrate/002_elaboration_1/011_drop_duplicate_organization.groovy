5bfc555600747767fbdca6babc2a4f12cdb01f59
gulatim <gulatim@ncias-d1508-v.nci.nih.gov>
2009-01-19 16:42:14 +0000

    new groovy scripts

--- pro-ctcae/core/src/main/db/migrate/002_elaboration_1/011_drop_duplicate_organization.groovy
+++ pro-ctcae/core/src/main/db/migrate/002_elaboration_1/011_drop_duplicate_organization.groovy
@@ -1,9 +1,7 @@
 class DropDuplicateOrganization extends edu.northwestern.bioinformatics.bering.Migration {
 	void up() {
 		execute('delete from organizations where id=-6')
-		execute('delete from organizations where id=104873')
 		execute('delete from organizations where id=103999')
-		execute('delete from organizations where id=104880')
 		execute('delete from organizations where id=104276')
 		execute('delete from organizations where id=106534')
 
