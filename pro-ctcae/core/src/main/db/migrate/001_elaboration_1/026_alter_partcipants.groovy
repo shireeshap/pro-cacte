ae4aa393755f1885ae1aa93e915cee0eaf69f6af
gulatim <gulatim@ncias-d1508-v.nci.nih.gov>
2009-01-19 16:07:07 +0000

    deleting old groovy scripts

--- pro-ctcae/core/src/main/db/migrate/001_elaboration_1/026_alter_partcipants.groovy
+++
@@ -1,9 +0,0 @@
-class AlterParticipants extends edu.northwestern.bioinformatics.bering.Migration {
-  void up() {
-    execute('delete from participants')
-    addColumn('participants', 'mrn_identifier', 'string', nullable: false);
-  }
-
-  void down() {
-  }
-}
