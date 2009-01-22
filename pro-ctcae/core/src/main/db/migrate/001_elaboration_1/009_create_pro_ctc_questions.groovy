d2f98374b9a80a791e33bbdb068e54188442e254
kumarvi <kumarvi@ncias-d1508-v.nci.nih.gov>
2009-01-22 16:05:14 +0000

    PRKC-124 -system must preserve the reorderning

--- pro-ctcae/core/src/main/db/migrate/001_elaboration_1/009_create_pro_ctc_questions.groovy
+++ pro-ctcae/core/src/main/db/migrate/001_elaboration_1/009_create_pro_ctc_questions.groovy
@@ -1,15 +1,15 @@
-class CreateProCtc extends edu.northwestern.bioinformatics.bering.Migration {
-	void up() {
-		createTable("PRO_CTC_QUESTIONS") {t ->
-			t.addColumn('question_text', 'string', nullable: false)
-			t.addColumn('pro_ctc_term_id', 'integer', nullable: false)
-			t.addColumn('question_type', 'string', nullable: true)
-			t.addColumn('display_order', 'integer', nullable: true)
-	}
-		execute('ALTER TABLE PRO_CTC_QUESTIONS ADD CONSTRAINT fk_pro_ctc_ques_term FOREIGN KEY (pro_ctc_term_id) REFERENCES PRO_CTC_TERMS')
-	}
+class CreateProCtcQuestions extends edu.northwestern.bioinformatics.bering.Migration {
+  void up() {
+    createTable("PRO_CTC_QUESTIONS") {t ->
+      t.addColumn('question_text', 'string', nullable: false)
+      t.addColumn('pro_ctc_term_id', 'integer', nullable: false)
+      t.addColumn('question_type', 'string', nullable: true)
+      t.addColumn('display_order', 'integer', nullable: true)
+    }
+    execute('ALTER TABLE PRO_CTC_QUESTIONS ADD CONSTRAINT fk_pro_ctc_ques_term FOREIGN KEY (pro_ctc_term_id) REFERENCES PRO_CTC_TERMS')
+  }
 
-	void down() {
-		dropTable("PRO_CTC_QUESTIONS")
-	}
+  void down() {
+    dropTable("PRO_CTC_QUESTIONS")
+  }
 }
