1ac472d906d7d140d3712919a605d8ada99e5891
kumarvi <kumarvi@ncias-d1508-v.nci.nih.gov>
2009-01-30 19:08:27 +0000

    updating the test case

--- pro-ctcae/web/src/test/java/gov/nih/nci/ctcae/selenium/form/EditFormSeleniumTest.java
+++ pro-ctcae/web/src/test/java/gov/nih/nci/ctcae/selenium/form/EditFormSeleniumTest.java
@@ -4,23 +4,26 @@ import gov.nih.nci.ctcae.selenium.AbstractSeleniumTestCase;
 
 /**
  * @author Mehul Gulati
- * Date: Jan 29, 2009
+ *         Date: Jan 29, 2009
  */
 public class EditFormSeleniumTest extends AbstractSeleniumTestCase {
-    	public void testNew() throws Exception {
-	        String formTitle = randomString();
-            createForm(formTitle);
+
+    public void testEditForm() throws Exception {
+        String formTitle = randomString();
+        createForm(formTitle);
 
         selenium.open("/ctcae/pages/form/manageForm");
         selenium.setSpeed("1000");
         typeAutosuggest("study-input", "p", "study-choices");
-		selenium.click("link=Edit");
-		selenium.waitForPageToLoad("30000");
-		selenium.click("crfTitle");
-		selenium.type("value", "abc");
-		selenium.click("//a[@id='proCtcTerm_-2']/img");
-		selenium.click("flow-next");
-		selenium.waitForPageToLoad("30000");
+        selenium.click("link=Edit");
+        selenium.waitForPageToLoad("30000");
+        selenium.click("crfTitle");
+        selenium.type("value", "abc");
+        selenium.type("crf.title", "abc");
+
+        selenium.click("//a[@id='proCtcTerm_-2']/img");
+        selenium.click("flow-next");
+        selenium.waitForPageToLoad("30000");
         assertTrue(selenium.isTextPresent("The Form was saved successfully"));
         assertTrue(selenium.isTextPresent("abc"));
 
