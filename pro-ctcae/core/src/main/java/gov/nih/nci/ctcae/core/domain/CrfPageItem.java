5dab444a14283635b6752884dd3f745f096d1352
gulatim <gulatim@ncias-d1508-v.nci.nih.gov>
2009-02-03 15:47:43 +0000

    Added Javadoc

--- pro-ctcae/core/src/main/java/gov/nih/nci/ctcae/core/domain/CrfPageItem.java
+++ pro-ctcae/core/src/main/java/gov/nih/nci/ctcae/core/domain/CrfPageItem.java
@@ -11,7 +11,10 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+// TODO: Auto-generated Javadoc
 /**
+ * The Class CrfPageItem.
+ * 
  * @author Harsh Agarwal
  * @created Oct 13, 2008
  */
@@ -22,44 +25,62 @@ import java.util.Set;
 @GenericGenerator(name = "id-generator", strategy = "native", parameters = {@Parameter(name = "sequence", value = "seq_crf_page_items_id")})
 public class CrfPageItem extends BasePersistable {
 
+    /** The Constant INITIAL_ORDER. */
     public static final Integer INITIAL_ORDER = 1;
 
+    /** The id. */
     @Id
     @GeneratedValue(generator = "id-generator")
     @Column(name = "id")
     private Integer id;
 
+    /** The display order. */
     @Column(name = "display_order", nullable = false)
     private Integer displayOrder = 0;
 
+    /** The crf page. */
     @JoinColumn(name = "crf_page_id", referencedColumnName = "id", nullable = false)
     @ManyToOne
     private CRFPage crfPage;
 
+    /** The pro ctc question. */
     @JoinColumn(name = "pro_ctc_question_id", referencedColumnName = "id")
     @ManyToOne
     private ProCtcQuestion proCtcQuestion;
 
+    /** The response required. */
     @Column(name = "response_required")
     private Boolean responseRequired = Boolean.FALSE;
 
+    /** The instructions. */
     @Column(name = "instructions")
     private String instructions;
 
+    /** The DEFAUL t_ allignment. */
     public static CrfItemAllignment DEFAULT_ALLIGNMENT = CrfItemAllignment.HORIZONTAL;
 
+    /** The crf item allignment. */
     @Enumerated(value = EnumType.STRING)
     @Column(name = "allignment")
     private CrfItemAllignment crfItemAllignment = DEFAULT_ALLIGNMENT;
 
+    /** The crf page item display rules. */
     @OneToMany(mappedBy = "crfPageItem", fetch = FetchType.LAZY)
     @Cascade(value = {CascadeType.ALL, CascadeType.DELETE_ORPHAN})
     private List<CrfPageItemDisplayRule> crfPageItemDisplayRules = new ArrayList<CrfPageItemDisplayRule>();
 
 
+    /**
+     * Instantiates a new crf page item.
+     */
     public CrfPageItem() {
     }
 
+    /**
+     * Instantiates a new crf page item.
+     * 
+     * @param proCtcQuestion the pro ctc question
+     */
     public CrfPageItem(ProCtcQuestion proCtcQuestion) {
         super();
         this.proCtcQuestion = proCtcQuestion;
@@ -71,63 +92,134 @@ public class CrfPageItem extends BasePersistable {
     }
 
 
+    /**
+     * Gets the response required.
+     * 
+     * @return the response required
+     */
     public Boolean getResponseRequired() {
         return responseRequired;
     }
 
+    /**
+     * Sets the response required.
+     * 
+     * @param responseRequired the new response required
+     */
     public void setResponseRequired(final Boolean responseRequired) {
         this.responseRequired = responseRequired;
     }
 
+    /**
+     * Gets the instructions.
+     * 
+     * @return the instructions
+     */
     public String getInstructions() {
         return instructions;
     }
 
+    /**
+     * Sets the instructions.
+     * 
+     * @param instructions the new instructions
+     */
     public void setInstructions(final String instructions) {
         this.instructions = instructions;
     }
 
+    /**
+     * Gets the crf item allignment.
+     * 
+     * @return the crf item allignment
+     */
     public CrfItemAllignment getCrfItemAllignment() {
         return crfItemAllignment;
     }
 
+    /**
+     * Sets the crf item allignment.
+     * 
+     * @param crfItemAllignment the new crf item allignment
+     */
     public void setCrfItemAllignment(final CrfItemAllignment crfItemAllignment) {
         this.crfItemAllignment = crfItemAllignment;
     }
 
+    /* (non-Javadoc)
+     * @see gov.nih.nci.ctcae.core.domain.Persistable#getId()
+     */
     public Integer getId() {
         return id;
     }
 
+    /* (non-Javadoc)
+     * @see gov.nih.nci.ctcae.core.domain.Persistable#setId(java.lang.Integer)
+     */
     public void setId(Integer id) {
         this.id = id;
     }
 
+    /**
+     * Gets the display order.
+     * 
+     * @return the display order
+     */
     public Integer getDisplayOrder() {
         return displayOrder;
     }
 
+    /**
+     * Sets the display order.
+     * 
+     * @param displayOrder the new display order
+     */
     public void setDisplayOrder(Integer displayOrder) {
         this.displayOrder = displayOrder;
     }
 
 
+    /**
+     * Gets the crf page.
+     * 
+     * @return the crf page
+     */
     public CRFPage getCrfPage() {
         return crfPage;
     }
 
+    /**
+     * Sets the crf page.
+     * 
+     * @param crfPage the new crf page
+     */
     public void setCrfPage(final CRFPage crfPage) {
         this.crfPage = crfPage;
     }
 
+    /**
+     * Gets the pro ctc question.
+     * 
+     * @return the pro ctc question
+     */
     public ProCtcQuestion getProCtcQuestion() {
         return proCtcQuestion;
     }
 
+    /**
+     * Sets the pro ctc question.
+     * 
+     * @param proCtcQuestion the new pro ctc question
+     */
     public void setProCtcQuestion(ProCtcQuestion proCtcQuestion) {
         this.proCtcQuestion = proCtcQuestion;
     }
 
+    /**
+     * Gets the copy.
+     * 
+     * @return the copy
+     */
     public CrfPageItem getCopy() {
         CrfPageItem copiedCrfPageItem = new CrfPageItem();
         copiedCrfPageItem.setInstructions(instructions);
@@ -141,6 +233,9 @@ public class CrfPageItem extends BasePersistable {
         return copiedCrfPageItem;
     }
 
+    /* (non-Javadoc)
+     * @see java.lang.Object#equals(java.lang.Object)
+     */
     @Override
     public boolean equals(final Object o) {
         if (this == o) return true;
@@ -162,6 +257,9 @@ public class CrfPageItem extends BasePersistable {
         return true;
     }
 
+    /* (non-Javadoc)
+     * @see java.lang.Object#hashCode()
+     */
     @Override
     public int hashCode() {
         int result = displayOrder != null ? displayOrder.hashCode() : 0;
@@ -174,11 +272,21 @@ public class CrfPageItem extends BasePersistable {
     }
 
 
+    /**
+     * Gets the crf page item display rules.
+     * 
+     * @return the crf page item display rules
+     */
     public List<CrfPageItemDisplayRule> getCrfPageItemDisplayRules() {
         return crfPageItemDisplayRules;
     }
 
 
+    /**
+     * Removes the crf page item display rules by pro ctc valid value ids.
+     * 
+     * @param objectsIdsToRemove the objects ids to remove
+     */
     public void removeCrfPageItemDisplayRulesByProCtcValidValueIds(final String objectsIdsToRemove) {
         Set<String> ids = org.springframework.util.StringUtils.commaDelimitedListToSet(objectsIdsToRemove);
         final Set<Integer> proCtcValidValues = new HashSet();
@@ -189,6 +297,11 @@ public class CrfPageItem extends BasePersistable {
 
     }
 
+    /**
+     * Removes the crf page item display rules by ids.
+     * 
+     * @param proCtcValidValues the pro ctc valid values
+     */
     public void removeCrfPageItemDisplayRulesByIds(final Set<Integer> proCtcValidValues) {
         List<CrfPageItemDisplayRule> crfPageItemDisplayRulesToRemove = new ArrayList<CrfPageItemDisplayRule>();
         for (Integer id : proCtcValidValues) {
@@ -204,6 +317,13 @@ public class CrfPageItem extends BasePersistable {
 
     }
 
+    /**
+     * Gets the crf display rule by pro ctc valid value id.
+     * 
+     * @param id the id
+     * 
+     * @return the crf display rule by pro ctc valid value id
+     */
     private CrfPageItemDisplayRule getCrfDisplayRuleByProCtcValidValueId(final Integer id) {
         for (CrfPageItemDisplayRule crfPageItemDisplayRule : getCrfPageItemDisplayRules()) {
             if (crfPageItemDisplayRule.getProCtcValidValue().getId().equals(id)) {
@@ -213,11 +333,23 @@ public class CrfPageItem extends BasePersistable {
         return null;
     }
 
+    /**
+     * Removes the crf page item display rule.
+     * 
+     * @param crfPageItemDisplayRule the crf page item display rule
+     */
     private void removeCrfPageItemDisplayRule(final CrfPageItemDisplayRule crfPageItemDisplayRule) {
 
         getCrfPageItemDisplayRules().remove(crfPageItemDisplayRule);
     }
 
+    /**
+     * Adds the crf page item display rules.
+     * 
+     * @param crfPageItemDisplayRule the crf page item display rule
+     * 
+     * @return true, if successful
+     */
     public boolean addCrfPageItemDisplayRules(CrfPageItemDisplayRule crfPageItemDisplayRule) {
         if (crfPageItemDisplayRule != null) {
             CrfPageItemDisplayRule anotherCrfPageItemDisplayRule = getCrfDisplayRuleByProCtcValidValueId(crfPageItemDisplayRule.getProCtcValidValue().getId());
@@ -233,6 +365,13 @@ public class CrfPageItem extends BasePersistable {
     }
 
 
+    /**
+     * Adds the crf page item display rules.
+     * 
+     * @param proCtcValidValues the pro ctc valid values
+     * 
+     * @return the list< crf page item display rule>
+     */
     public List<CrfPageItemDisplayRule> addCrfPageItemDisplayRules(final List<ProCtcValidValue> proCtcValidValues) {
         final List<CrfPageItemDisplayRule> addedCrfPageItemDisplayRules = new ArrayList<CrfPageItemDisplayRule>();
         for (ProCtcValidValue proCtcValidValue : proCtcValidValues) {
@@ -247,6 +386,9 @@ public class CrfPageItem extends BasePersistable {
         return addedCrfPageItemDisplayRules;
     }
 
+    /**
+     * Reset all properties to default.
+     */
     public void resetAllPropertiesToDefault() {
         this.setCrfItemAllignment(CrfPageItem.DEFAULT_ALLIGNMENT);
         this.setInstructions(null);
--- pro-ctcae/web/src/main/java/gov/nih/nci/ctcae/web/form/AbstractCrfController.java
+++ pro-ctcae/web/src/main/java/gov/nih/nci/ctcae/web/form/AbstractCrfController.java
@@ -9,18 +9,40 @@ import org.springframework.web.servlet.mvc.AbstractController;
 import java.util.HashMap;
 import java.util.Map;
 
+// TODO: Auto-generated Javadoc
 /**
+ * The Class AbstractCrfController.
+ * 
  * @author Vinay Kumar
  * @crated Dec 31, 2008
  */
 public abstract class AbstractCrfController extends AbstractController {
+    
+    /** The pro ctc question repository. */
     protected ProCtcQuestionRepository proCtcQuestionRepository;
+    
+    /** The Constant PRO_CTC_TERM_COMPONENT. */
     public static final String PRO_CTC_TERM_COMPONENT = "proCtcTerm";
+    
+    /** The Constant CRF_PAGE_COMPONENT. */
     public static final String CRF_PAGE_COMPONENT = "crfPage";
+    
+    /** The Constant PRO_CTC_QUESTIONS_COMPONENT. */
     public static final String PRO_CTC_QUESTIONS_COMPONENT = "proCtcQuestion";
-
+   
+   /** The Constant CTC_CATEGORY_COMPONENT. */
+   public static final String CTC_CATEGORY_COMPONENT = "ctcCategory";
+    
+    /** The pro ctc term repository. */
     protected ProCtcTermRepository proCtcTermRepository;
 
+    /**
+     * Reference data.
+     * 
+     * @param command the command
+     * 
+     * @return the map< string, object>
+     */
     public Map<String, Object> referenceData(CreateFormCommand command) {
         Map<String, Object> map = new HashMap<String, Object>();
         map.put("responseRequired", ListValues.getResponseRequired());
@@ -33,11 +55,21 @@ public abstract class AbstractCrfController extends AbstractController {
     }
 
 
+    /**
+     * Sets the pro ctc term repository.
+     * 
+     * @param proCtcTermRepository the new pro ctc term repository
+     */
     @Required
     public void setProCtcTermRepository(ProCtcTermRepository proCtcTermRepository) {
         this.proCtcTermRepository = proCtcTermRepository;
     }
 
+    /**
+     * Sets the pro ctc question repository.
+     * 
+     * @param proCtcQuestionRepository the new pro ctc question repository
+     */
     @Required
     public void setProCtcQuestionRepository(ProCtcQuestionRepository proCtcQuestionRepository) {
         this.proCtcQuestionRepository = proCtcQuestionRepository;
