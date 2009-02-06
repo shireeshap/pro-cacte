7886c823b4d36ac879ac9fb4ce4b5cd6b67a0f49
kumarvi <kumarvi@ncias-d1508-v.nci.nih.gov>
2009-02-06 16:02:45 +0000

    updating test cases script

--- pro-ctcae/core/src/test/java/gov/nih/nci/ctcae/core/loader/CsvImporterIntegrationTest.java
+++ pro-ctcae/core/src/test/java/gov/nih/nci/ctcae/core/loader/CsvImporterIntegrationTest.java
@@ -1,4 +1,4 @@
-package gov.nih.nci.ctcae.core.CsvLoader;
+package gov.nih.nci.ctcae.core.loader;
 
 import gov.nih.nci.ctcae.core.AbstractHibernateIntegrationTestCase;
 import gov.nih.nci.ctcae.core.csv.loader.CsvImporter;
