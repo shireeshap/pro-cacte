accac573c8417af3888296e7f00e42fd5b3e97fe
gulatim <gulatim@ncias-d1508-v.nci.nih.gov>
2009-02-16 20:48:34 +0000

    crf_cylces table

---
+++ pro-ctcae/core/src/main/db/migrate/002_elaboration_1/019_create_crf_cycles.groovy
@@ -0,0 +1,15 @@
+class CreateCrfCalendars extends edu.northwestern.bioinformatics.bering.Migration {
+	void up() {
+		createTable("crf_cycles") {t ->
+			t.addColumn('crf_id', 'integer', nullable: false)
+			t.addColumn('cycle_length', 'integer', nullable: false)
+			t.addColumn('cycle_days', 'string', nullable: true)
+			t.addColumn('repeat_times', 'integer', nullable: true)
+			}
+
+
+ 	}
+ 		void down() {
+		dropTable("crf_cycles")
+	}
+}
