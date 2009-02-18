f216ff5ac0bede5ddafa79bcf7369d139c66aefe
kumarvi <kumarvi@ncias-d1508-v.nci.nih.gov>
2009-02-18 21:12:26 +0000

    updating groovy scripts

---
+++ pro-ctcae/core/src/main/db/migrate/002_elaboration_1/015_add_sp_clinical_staff.groovy
@@ -0,0 +1,29 @@
+class AddStudyParticipantClinicalStaff extends edu.northwestern.bioinformatics.bering.Migration {
+    void up() {
+
+//        createTable("STUDY_PARTICIPANT_CLINICAL_STAFFS") {t ->
+//
+//            t.addVersionColumn()
+//
+//            t.addColumn('study_site_clinical_staff_id', 'integer', nullable: false)
+//            t.addColumn('sp_assignment_id', 'integer', nullable: false)
+//
+//        }
+//
+//
+//        execute('ALTER TABLE STUDY_PARTICIPANT_CLINICAL_STAFFS ADD CONSTRAINT fk_ss_cls_site_clinical_staff FOREIGN KEY (study_site_clinical_staff_id) REFERENCES STUDY_SITE_CLINICAL_STAFFS')
+//        execute('ALTER TABLE STUDY_PARTICIPANT_CLINICAL_STAFFS ADD CONSTRAINT fk_ss_cls_study_site FOREIGN KEY (sp_assignment_id) REFERENCES STUDY_PARTICIPANT_ASSIGNMENTS')
+
+
+    }
+
+
+    void down() {
+        dropTable("STUDY_PARTICIPANT_CLINICAL_STAFFS")
+
+
+
+    }
+
+
+}
