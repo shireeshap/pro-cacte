04db7b9856e475005b377862c736b9a4b7f5b3c4
kumarvi <kumarvi@ncias-d1508-v.nci.nih.gov>
2009-03-18 17:40:11 +0000

    fixing test case

--- pro-ctcae/core/src/test/java/gov/nih/nci/ctcae/core/security/SiteCRAMethodAuthorizationIntegrationTest.java
+++ pro-ctcae/core/src/test/java/gov/nih/nci/ctcae/core/security/SiteCRAMethodAuthorizationIntegrationTest.java
@@ -67,7 +67,6 @@ public class SiteCRAMethodAuthorizationIntegrationTest extends MethodAuthorizati
         List<String> allowedMethods = allowedMethodsMap.get(ClinicalStaffRepository.class);
 
         allowedMethods.add(SEARCH_CLINICAL_STAFF_BY_SS_METHOD);
-        allowedMethods.add(FIND_BY_ID_METHOD);
 
         authorizeAndUnAuthorizeMethods(organizationClinicalStaffRepository, OrganizationClinicalStaffRepository.class, allowedMethods);
 
