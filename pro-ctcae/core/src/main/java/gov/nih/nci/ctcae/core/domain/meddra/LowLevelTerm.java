e4a56a83c996410bb7148c3f507fc11ffeda3f94
gulatim <gulatim@ncias-d1508-v.nci.nih.gov>
2009-05-20 16:44:13 +0000

    meddraImport

---
+++ pro-ctcae/core/src/main/java/gov/nih/nci/ctcae/core/domain/meddra/LowLevelTerm.java
@@ -0,0 +1,37 @@
+package gov.nih.nci.ctcae.core.domain.meddra;
+
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+
+import org.hibernate.annotations.Cascade;
+import org.hibernate.annotations.CascadeType;
+
+@Entity
+@Table(name = "meddra_llt")
+public class LowLevelTerm extends AbstractMeddraDomainObject {
+
+    private PreferredTerm preferredTerm;
+
+    @ManyToOne(fetch=FetchType.LAZY)
+    @JoinColumn(name = "meddra_pt_id")
+    @Cascade(value = { CascadeType.LOCK })
+    public PreferredTerm getPreferredTerm(){
+    	return preferredTerm;
+    }
+
+    public void setPreferredTerm(PreferredTerm preferredTerm){
+    	this.preferredTerm = preferredTerm;
+    }
+
+    @Transient
+    public String getFullName() {
+        return getMeddraCode() + " - " + getMeddraTerm();
+    }
+
+
+}
