e9c181abfa52b98513550c025830f8d1d532c788
gulatim <gulatim@ncias-d1508-v.nci.nih.gov>
2009-06-10 18:27:30 +0000

    Test cases fixes

--- pro-ctcae/web/src/test/java/gov/nih/nci/ctcae/web/participant/ParticipantInboxControllerTest.java
+++ pro-ctcae/web/src/test/java/gov/nih/nci/ctcae/web/participant/ParticipantInboxControllerTest.java
@@ -1,11 +1,16 @@
 package gov.nih.nci.ctcae.web.participant;
 
 import gov.nih.nci.ctcae.core.helper.Fixture;
+import gov.nih.nci.ctcae.core.helper.ParticipantTestHelper;
+import gov.nih.nci.ctcae.core.helper.TestDataManager;
+import gov.nih.nci.ctcae.core.helper.ClinicalStaffTestHelper;
 import gov.nih.nci.ctcae.core.domain.Participant;
 import gov.nih.nci.ctcae.core.domain.User;
 import gov.nih.nci.ctcae.core.query.ParticipantQuery;
 import gov.nih.nci.ctcae.core.repository.ParticipantRepository;
+import gov.nih.nci.ctcae.core.exception.CtcAeSystemException;
 import gov.nih.nci.ctcae.web.WebTestCase;
+import gov.nih.nci.ctcae.web.AbstractWebTestCase;
 import static org.easymock.EasyMock.expect;
 import static org.easymock.EasyMock.isA;
 import org.springframework.security.context.SecurityContextHolder;
@@ -18,37 +23,25 @@ import java.util.List;
  * @author Vinay Kumar
  * @crated Nov 4, 2008
  */
-public class ParticipantInboxControllerTest extends WebTestCase {
-    private ParticipantInboxController controller;
-    private ParticipantRepository participantRepository;
+public class ParticipantInboxControllerTest extends AbstractWebTestCase {
     Participant participant;
 
-
-    @Override
-    protected void setUp() throws Exception {
-        super.setUp();
-        controller = new ParticipantInboxController();
-        participantRepository = registerMockFor(ParticipantRepository.class);
-
+    public void testFormBackingObject() throws Exception {
+        ParticipantInboxController controller = new ParticipantInboxController();
+        request.setMethod("GET");
         controller.setParticipantRepository(participantRepository);
 
-        participant = Fixture.createParticipant("test", "test", "test");
-        participant.setId(1);
-    }
+        login(ParticipantTestHelper.getDefaultParticipant().getUser().getUsername());
+        controller.handleRequest(request, response);
+        assertEquals(gov.nih.nci.ctcae.core.domain.Participant.class, controller.getCommandClass());
 
+        login(ClinicalStaffTestHelper.getDefaultClinicalStaff().getUser().getUsername());
+        try {
+            controller.handleRequest(request, response);
+            fail("Not a valid participant. Expecting error");
+        } catch (CtcAeSystemException e) {
 
-    public void testFormBackingObject() throws Exception {
-        List<Participant> participants = new ArrayList<Participant>();
-        participants.add(participant);
-        User user = new User();
-        user.setUsername("test");
-        SecurityContextHolder.getContext().setAuthentication(new UsernamePasswordAuthenticationToken(user, "test"));
-        expect(participantRepository.find(isA(ParticipantQuery.class))).andReturn(participants);
-        replayMocks();
+        }
 
-        Participant p = (Participant) controller.formBackingObject(request);
-        verifyMocks();
-
-        assertEquals(participant, p);
     }
 }
