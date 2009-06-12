ecb22208c6c6c4e768db88d1f862333bd99953ca
gulatim <gulatim@ncias-d1508-v.nci.nih.gov>
2009-06-12 15:17:54 +0000

    Test cases fixes

--- pro-ctcae/web/src/test/java/gov/nih/nci/ctcae/web/participant/ScheduleCrfTabTest.java
+++ pro-ctcae/web/src/test/java/gov/nih/nci/ctcae/web/participant/ScheduleCrfTabTest.java
@@ -1,6 +1,17 @@
 package gov.nih.nci.ctcae.web.participant;
 
 import gov.nih.nci.ctcae.web.WebTestCase;
+import gov.nih.nci.ctcae.core.domain.Privilege;
+import gov.nih.nci.ctcae.core.domain.StudyParticipantAssignment;
+import gov.nih.nci.ctcae.core.domain.StudyParticipantCrf;
+import gov.nih.nci.ctcae.core.domain.CRF;
+import gov.nih.nci.ctcae.core.repository.CRFRepository;
+import org.springframework.web.servlet.ModelAndView;
+import static org.easymock.EasyMock.expect;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author Harsh Agarwal
@@ -17,8 +28,38 @@ public class ScheduleCrfTabTest extends WebTestCase {
 
     }
 
-    public void testConstructor (){
+    public void testConstructor() {
         assertNotNull(tab);
     }
+
+    public void testTab() {
+        assertEquals(Privilege.PRIVILEGE_PARTICIPANT_SCHEDULE_CRF, tab.getRequiredPrivilege());
+        CRFRepository crfRepository = registerMockFor(CRFRepository.class);
+        StudyParticipantAssignment a = new StudyParticipantAssignment();
+        List<StudyParticipantCrf> l = new ArrayList<StudyParticipantCrf>();
+        StudyParticipantCrf s = new StudyParticipantCrf();
+        CRF c = new CRF();
+        c.setId(1);
+        s.setCrf(c);
+        l.add(s);
+        a.addStudyParticipantCrf(s);
+        StudyParticipantCommand command = new StudyParticipantCommand();
+        command.setStudyParticipantAssignment(a);
+
+        tab.setCrfRepository(crfRepository);
+        expect(crfRepository.findById(1)).andReturn(c);
+        replayMocks();
+        tab.onDisplay(request, command);
+        verifyMocks();
+
+        Map<String, Object> m =  tab.referenceData(command);
+        assertNotNull(m.get("repetitionunits"));
+        assertNotNull(m.get("duedateunits"));
+        assertNotNull(m.get("repeatuntilunits"));
+        assertNotNull(m.get("cyclelengthunits"));
+
+        
+    }
+
 
 }
