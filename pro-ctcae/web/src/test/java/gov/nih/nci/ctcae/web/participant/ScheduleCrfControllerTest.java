fb046848268ff86f067c67cf09a661b24a294717
gulatim <gulatim@ncias-d1508-v.nci.nih.gov>
2009-06-12 15:23:32 +0000

    Test cases fixes

--- pro-ctcae/web/src/test/java/gov/nih/nci/ctcae/web/participant/ScheduleCrfControllerTest.java
+++ pro-ctcae/web/src/test/java/gov/nih/nci/ctcae/web/participant/ScheduleCrfControllerTest.java
@@ -1,8 +1,14 @@
 package gov.nih.nci.ctcae.web.participant;
 
 import gov.nih.nci.ctcae.core.repository.StudyParticipantAssignmentRepository;
+import gov.nih.nci.ctcae.core.domain.StudyParticipantAssignment;
+import gov.nih.nci.ctcae.core.domain.StudyParticipantCrf;
 import gov.nih.nci.ctcae.web.WebTestCase;
 import org.springframework.validation.BindException;
+import org.springframework.web.servlet.ModelAndView;
+import static org.easymock.EasyMock.expect;
+
+import java.util.HashMap;
 
 /**
  * @author Harsh Agarwal
@@ -23,17 +29,23 @@ public class ScheduleCrfControllerTest extends WebTestCase {
         errors = registerMockFor(BindException.class);
         controller.setStudyParticipantAssignmentRepository(studyParticipantAssignmentRepository);
         command = new StudyParticipantCommand();
+        StudyParticipantAssignment a = new StudyParticipantAssignment();
+        a.setId(1);
+        command.setStudyParticipantAssignment(a);
+        StudyParticipantCrf s = new StudyParticipantCrf();
+        a.addStudyParticipantCrf(s);
     }
 
     public void testProcessFinish() throws Exception {
 
-//        expect(studyParticipantAssignmentRepository.save(command.getStudyParticipantAssignment())).andReturn(null);
-//        expect(errors.getModel()).andReturn(new HashMap());
-//        replayMocks();
-//        ModelAndView modelAndView = controller.processFinish(request, response, command, errors);
-//        verifyMocks();
-//
-//        assertEquals("participant/confirmschedule", modelAndView.getViewName());
+        expect(studyParticipantAssignmentRepository.save(command.getStudyParticipantAssignment())).andReturn(null);
+        expect(studyParticipantAssignmentRepository.findById(1)).andReturn(command.getStudyParticipantAssignment());
+        expect(errors.getModel()).andReturn(new HashMap());
+        replayMocks();
+        ModelAndView modelAndView = controller.processFinish(request, response, command, errors);
+        verifyMocks();
+
+        assertEquals("participant/confirmschedule", modelAndView.getViewName());
 
 
     }
