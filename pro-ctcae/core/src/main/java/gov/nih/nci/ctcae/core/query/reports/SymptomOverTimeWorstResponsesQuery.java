28b6c7d4ee24a8a7a906ab7d63352a16fa218438
gulatim <gulatim@ncias-d1508-v.nci.nih.gov>
2009-06-30 13:40:55 +0000

    New rules

--- pro-ctcae/core/src/main/java/gov/nih/nci/ctcae/core/query/reports/SymptomOverTimeWorstResponsesQuery.java
+++ pro-ctcae/core/src/main/java/gov/nih/nci/ctcae/core/query/reports/SymptomOverTimeWorstResponsesQuery.java
@@ -17,18 +17,18 @@ public class SymptomOverTimeWorstResponsesQuery extends AbstractReportQuery {
         String selectGroup = "'Week ' || spci.studyParticipantCrfSchedule.weekInYear ";
         String middle = "from StudyParticipantCrfItem spci group by spci.proCtcValidValue.displayOrder,spci.crfPageItem.proCtcQuestion.proCtcQuestionType,spci.studyParticipantCrfSchedule.studyParticipantCrf.studyParticipantAssignment.participant.id, ";
         String groupBy = "spci.studyParticipantCrfSchedule.weekInYear ";
-        String orderBy = "order by spci.studyParticipantCrfSchedule.weekInYear, ";
-        String orderBy2 = "spci.crfPageItem.proCtcQuestion.proCtcQuestionType ";
+        String orderBy = "order by spci.crfPageItem.proCtcQuestion.proCtcQuestionType, ";
+        String orderBy2 = "spci.studyParticipantCrfSchedule.weekInYear ";
 
         if (group.equals("month")) {
             selectGroup = "'Month ' || spci.studyParticipantCrfSchedule.monthInYear ";
             groupBy = "spci.studyParticipantCrfSchedule.monthInYear ";
-            orderBy = "order by spci.studyParticipantCrfSchedule.monthInYear, ";
+            orderBy2 = "spci.studyParticipantCrfSchedule.monthInYear ";
         }
         if (group.equals("cycle")) {
             selectGroup = "'Cycle ' || spci.studyParticipantCrfSchedule.cycleNumber ";
             groupBy = "spci.studyParticipantCrfSchedule.cycleNumber ";
-            orderBy = "order by spci.studyParticipantCrfSchedule.cycleNumber, ";
+            orderBy2 = "spci.studyParticipantCrfSchedule.cycleNumber ";
         }
 
         query.append(append).append(selectGroup).append(middle).append(groupBy).append(orderBy).append(orderBy2);
