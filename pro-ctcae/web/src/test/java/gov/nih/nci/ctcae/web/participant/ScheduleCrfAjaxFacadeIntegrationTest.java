aa5c2ffe896da4010bd6d810746fdef4aceeced2
gulatim <gulatim@ncias-d1508-v.nci.nih.gov>
2009-07-23 14:00:15 +0000

    Test case fix

--- pro-ctcae/web/src/test/java/gov/nih/nci/ctcae/web/participant/ScheduleCrfAjaxFacadeIntegrationTest.java
+++ pro-ctcae/web/src/test/java/gov/nih/nci/ctcae/web/participant/ScheduleCrfAjaxFacadeIntegrationTest.java
@@ -1,9 +1,13 @@
 package gov.nih.nci.ctcae.web.participant;
 
 import gov.nih.nci.ctcae.core.domain.Participant;
+import gov.nih.nci.ctcae.core.domain.Study;
 import gov.nih.nci.ctcae.core.helper.Fixture;
 import gov.nih.nci.ctcae.core.helper.StudyTestHelper;
+import gov.nih.nci.ctcae.core.helper.ParticipantTestHelper;
 import gov.nih.nci.ctcae.web.AbstractWebIntegrationTestCase;
+
+import java.util.ArrayList;
 
 
 /**
@@ -13,34 +17,26 @@ import gov.nih.nci.ctcae.web.AbstractWebIntegrationTestCase;
 public class ScheduleCrfAjaxFacadeIntegrationTest extends AbstractWebIntegrationTestCase {
 
     private ScheduleCrfAjaxFacade scheduleCrfAjaxFacade;
-    private Participant participant;
-
-    @Override
-    protected void onSetUpInTransaction() throws Exception {
-        super.onSetUpInTransaction();
-
-
-        participant = Fixture.createParticipantWithStudyAssignment("Mehul", "Gulati", "1234", StudyTestHelper.getDefaultStudy().getLeadStudySite());
-        participant = participantRepository.save(participant);
-    }
 
     public void testSearchParticipantForStudy() {
+        Study study = StudyTestHelper.getDefaultStudy();
 
-//        ArrayList<Participant> participants = (ArrayList<Participant>) scheduleCrfAjaxFacade.matchParticipants("gu", defaultStudy.getId());
-//        assertEquals(1, participants.size());
-//
-//        participants = (ArrayList<Participant>) scheduleCrfAjaxFacade.matchParticipants("abc", defaultStudy.getId());
-//        assertEquals(0, participants.size());
+        ArrayList<Participant> participants = (ArrayList<Participant>) scheduleCrfAjaxFacade.matchParticipants("john", study.getId());
+        assertEquals(1, participants.size());
+
+        participants = (ArrayList<Participant>) scheduleCrfAjaxFacade.matchParticipants("abc", study.getId());
+        assertEquals(0, participants.size());
 
     }
 
     public void testSearchStudyForParticipant() {
+        Participant participant = ParticipantTestHelper.getDefaultParticipant();
 
-//        ArrayList<Study> studies = (ArrayList<Study>) scheduleCrfAjaxFacade.matchStudies("gu", participant.getId());
-//        assertEquals(1, studies.size());
-//
-//        studies = (ArrayList<Study>) scheduleCrfAjaxFacade.matchStudies("xyzz", participant.getId());
-//        assertEquals(0, studies.size());
+        ArrayList<Study> studies = (ArrayList<Study>) scheduleCrfAjaxFacade.matchStudies("Collection", participant.getId());
+        assertEquals(1, studies.size());
+
+        studies = (ArrayList<Study>) scheduleCrfAjaxFacade.matchStudies("xyzz", participant.getId());
+        assertEquals(0, studies.size());
     }
 
     public ScheduleCrfAjaxFacade getScheduleCrfAjaxFacade() {
