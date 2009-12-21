dc36be8503708de192b99f35a1a7773bc82306e1
hagarwal <hagarwal@ncias-d1508-v.nci.nih.gov>
2009-12-21 22:53:51 +0000

    BDA Changes

--- pro-ctcae/all.bat
+++ pro-ctcae/all.bat
@@ -1,8 +1,9 @@
 set CATALINA_HOME=C:\Users\Harsh\proctcae\Tomcat_5_5_26
-call ant publish-all  -Dskip.test=true -Doffline=true
-
+call ant publish-all  -Dskip.test=true 
+pause
 cd web
-call ant -f ivy-build.xml deploy-proctcae  -Doffline=true
+call ant -f ivy-build.xml deploy-proctcae 
+pause
 cd ..
 cd C:\Users\Harsh\proctcae\Tomcat_5_5_26\bin
 startup.bat
--- pro-ctcae/web.bat
+++ pro-ctcae/web.bat
@@ -1,6 +1,6 @@
 cd web
 set CATALINA_HOME=C:\Users\Harsh\proctcae\Tomcat_5_5_26
-call ant -f ivy-build.xml deploy-proctcae  -Doffline=true
+call ant -f ivy-build.xml deploy-proctcae -Doffline=true
 
 cd C:\Users\Harsh\proctcae\Tomcat_5_5_26\bin
 
