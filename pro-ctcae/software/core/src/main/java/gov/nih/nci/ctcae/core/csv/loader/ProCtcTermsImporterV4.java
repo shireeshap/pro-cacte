b492c673e5fb889b2e907ccc508b38ede567fe72
hagarwal <hagarwal@ncias-d1508-v.nci.nih.gov>
2010-02-17 21:27:34 +0000

    Load New ProCtcTerms

--- pro-ctcae/software/core/src/main/java/gov/nih/nci/ctcae/core/csv/loader/ProCtcTermsImporterV4.java
+++ pro-ctcae/software/core/src/main/java/gov/nih/nci/ctcae/core/csv/loader/ProCtcTermsImporterV4.java
@@ -11,6 +11,9 @@ import org.springframework.core.io.ClassPathResource;
 
 import java.io.*;
 import java.util.*;
+import java.nio.charset.Charset;
+
+import com.csvreader.CsvReader;
 
 
 /**
@@ -20,30 +23,30 @@ import java.util.*;
 
 public class ProCtcTermsImporterV4 {
     private CtcTermRepository ctcTermRepository;
+    private static final String QUESTION_TEXT = "PRO-CTCAE Wording";
+    private static final String PRO_CTC_TERM = "PRO-CTCAE Term";
+    private static final String CORE_ITEM = "Core Item";
+    private static final String QUESTION_TYPE = "Attribute";
+    private static final String PRO_CTC_VALID_VALUES = "Response Options";
+    private static final String CTC_TERM = "CTCAE v4 Term";
+    private static final String CATEGORY = "CTCAE v4 System Organ Class (SOC)";
+
 
     public ProCtc loadProCtcTerms(boolean fromTestCase) throws IOException {
-        InputStream xls;
-        xls = new ClassPathResource("ProCtcTerms_V4.xls").getURL().openStream();
-        HSSFWorkbook wb = new HSSFWorkbook(xls);
-        HSSFSheet sheet = wb.getSheetAt(0);
-        int rowIndex = 2;
-        short cellIndex = 0;
-        HSSFRow row;
+        ClassPathResource classPathResource = new ClassPathResource("ProCtcTerms_V4.csv");
         HashMap<String, List<CsvLine>> hm = new HashMap<String, List<CsvLine>>();
-        HashMap<String, ProCtcQuestion> firstQuestions = new HashMap<String, ProCtcQuestion>();
-        while (true) {
-            row = sheet.getRow(rowIndex);
-            if (row == null) {
-                break;
-            }
-            String question = row.getCell(cellIndex).getRichStringCellValue().getString();
-            String proCtcTerm = row.getCell(++cellIndex).getRichStringCellValue().getString();
-            String core = row.getCell(++cellIndex) == null ? "" : row.getCell(cellIndex).getRichStringCellValue().getString();
-            String attribute = row.getCell(++cellIndex).getRichStringCellValue().getString();
-            String validValues = row.getCell(++cellIndex).getRichStringCellValue().getString();
-            String ctcTerm = row.getCell(++cellIndex).getRichStringCellValue().getString();
-            String ctcCategory = row.getCell(++cellIndex).getRichStringCellValue().getString();
+        CsvReader reader = new CsvReader(classPathResource.getInputStream(), Charset.forName("ISO-8859-1"));
+        reader.readHeaders();
+        while (reader.readRecord()) {
             CsvLine csvLine = new CsvLine();
+
+            String question = reader.get(QUESTION_TEXT);
+            String proCtcTerm = reader.get(PRO_CTC_TERM);
+            String core = reader.get(CORE_ITEM);
+            String attribute = reader.get(QUESTION_TYPE);
+            String validValues = reader.get(PRO_CTC_VALID_VALUES);
+            String ctcTerm = reader.get(CTC_TERM);
+            String ctcCategory = reader.get(CATEGORY);
 
             String displayOrder = attribute.substring(0, attribute.indexOf('-'));
             String questionType = attribute.substring(attribute.indexOf('-') + 1);
@@ -66,8 +69,6 @@ public class ProCtcTermsImporterV4 {
             }
             System.out.println(csvLine);
 
-            cellIndex = 0;
-            rowIndex++;
         }
 
         ProCtc proCtc = new ProCtc();
