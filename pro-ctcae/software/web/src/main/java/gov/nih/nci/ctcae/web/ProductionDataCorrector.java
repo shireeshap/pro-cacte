552bba13ff3891d7f4c2f3990ad63272857f2c01
suneela <suneela@ncias-d1508-v.nci.nih.gov>
2011-01-24 17:18:40 +0000

    fix in production due dates

--- pro-ctcae/software/web/src/main/java/gov/nih/nci/ctcae/web/ProductionDataCorrector.java
+++ pro-ctcae/software/web/src/main/java/gov/nih/nci/ctcae/web/ProductionDataCorrector.java
@@ -1,5 +1,6 @@
 package gov.nih.nci.ctcae.web;
 
+import org.hibernate.Query;
 import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
 import org.springframework.orm.hibernate3.HibernateCallback;
 import org.springframework.orm.hibernate3.HibernateTemplate;
@@ -21,8 +22,16 @@ import java.sql.SQLException;
         public void correctDueDateOfScheduleInProdDb(){
             getHibernateTemplate().execute(new HibernateCallback(){
                 public Object doInHibernate(Session session) throws HibernateException, SQLException {
-                    session.createSQLQuery("update sp_crf_schedules scs1 set due_date = (select (scs.start_date+cast(COALESCE(ccd.due_date_amount,'1') as int)) from sp_crf_schedules scs JOIN study_participant_crfs spc ON spc.id = scs.study_participant_crf_id JOIN study_participant_assignments spa ON spa.id=spc.study_participant_id JOIN crfs c ON c.id=spc.crf_id JOIN form_arm_schedules fas ON c.id=fas.form_id JOIN crf_cycle_definitions ccd ON fas.id = ccd.form_arm_schedules_id where scs1.id= scs.id and scs.baseline='false' and fas.arm_id=spa.arm_id and scs.due_date <> (scs.start_date+cast(COALESCE(ccd.due_date_amount,'1') as int)) ) where scs1.id in ( select scs.id from sp_crf_schedules scs JOIN study_participant_crfs spc ON spc.id = scs.study_participant_crf_id JOIN study_participant_assignments spa ON spa.id=spc.study_participant_id JOIN crfs c ON c.id=spc.crf_id JOIN form_arm_schedules fas ON c.id=fas.form_id JOIN crf_cycle_definitions ccd ON fas.id = ccd.form_arm_schedules_id where scs.baseline='false' and fas.arm_id=spa.arm_id and scs.due_date <> (scs.start_date+cast(COALESCE(ccd.due_date_amount,'1') as int)) );");
-                    session.createSQLQuery("update sp_crf_schedules scs set status='SCHEDULED' where status='PASTDUE' and due_date>=now();");
+                    int numberOfResults = 0;
+
+                    Query query = session.createSQLQuery("update sp_crf_schedules scs1 set due_date = (select (scs.start_date+cast(COALESCE(ccd.due_date_amount,'1') as int)) from sp_crf_schedules scs JOIN study_participant_crfs spc ON spc.id = scs.study_participant_crf_id JOIN study_participant_assignments spa ON spa.id=spc.study_participant_id JOIN crfs c ON c.id=spc.crf_id JOIN form_arm_schedules fas ON c.id=fas.form_id JOIN crf_cycle_definitions ccd ON fas.id = ccd.form_arm_schedules_id where scs1.id= scs.id and scs.baseline='false' and fas.arm_id=spa.arm_id and scs.due_date <> (scs.start_date+cast(COALESCE(ccd.due_date_amount,'1') as int)) ) where scs1.id in ( select scs.id from sp_crf_schedules scs JOIN study_participant_crfs spc ON spc.id = scs.study_participant_crf_id JOIN study_participant_assignments spa ON spa.id=spc.study_participant_id JOIN crfs c ON c.id=spc.crf_id JOIN form_arm_schedules fas ON c.id=fas.form_id JOIN crf_cycle_definitions ccd ON fas.id = ccd.form_arm_schedules_id where scs.baseline='false' and fas.arm_id=spa.arm_id and scs.due_date <> (scs.start_date+cast(COALESCE(ccd.due_date_amount,'1') as int)) );");
+                     numberOfResults = query.executeUpdate();
+                    //System.out.println(numberOfResults);
+                    query = session.createSQLQuery("update sp_crf_schedules scs set status='SCHEDULED' where status='PASTDUE' and due_date>=now();");
+                    numberOfResults = query.executeUpdate();
+                    //System.out.println(numberOfResults);
+                    //session.flush();
+                    
                     return null;  //To change body of implemented methods use File | Settings | File Templates.
                 }
             });
