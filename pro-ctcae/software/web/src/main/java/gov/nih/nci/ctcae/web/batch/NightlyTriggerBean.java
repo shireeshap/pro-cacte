4575670b3af86105feae83663d3bcd0f60cdd4aa
suneela <suneela@ncias-d1508-v.nci.nih.gov>
2011-03-09 20:52:47 +0000

    PRKC-754,755-----
    Added by Suneel Allareddy
    
    Test Cases added for UpdateSchedule,getDueDateForFormSchedule and

--- pro-ctcae/software/web/src/main/java/gov/nih/nci/ctcae/web/batch/NightlyTriggerBean.java
+++ pro-ctcae/software/web/src/main/java/gov/nih/nci/ctcae/web/batch/NightlyTriggerBean.java
@@ -9,8 +9,8 @@ public class NightlyTriggerBean extends SimpleTriggerBean {
     public NightlyTriggerBean() {
         setStartDelay(0);
         Calendar calendar = Calendar.getInstance();
-        calendar.set(Calendar.HOUR, 23);
-        calendar.set(Calendar.MINUTE, 30);
+        calendar.set(Calendar.HOUR, 16);
+        calendar.set(Calendar.MINUTE, 05);
         calendar.set(Calendar.SECOND, 0);
         calendar.set(Calendar.MILLISECOND, 0);
 
