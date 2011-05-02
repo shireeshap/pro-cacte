e88ced995991695abad982c44d61771b793caebe
vinayg <vinayg@ncias-d1508-v.nci.nih.gov>
2011-05-02 15:14:27 +0000

    PRKC-879: Dev work for multi language support. Will drop columns in next script (backup measure)

--- pro-ctcae/software/core/src/main/db/migrate/004_elaboration_1/030_drop_term_constraints.groovy
+++ pro-ctcae/software/core/src/main/db/migrate/004_elaboration_1/030_drop_term_constraints.groovy
@@ -4,6 +4,12 @@ class StudyParticipantReportingModeHistory extends edu.northwestern.bioinformati
         
      	execute('ALTER TABLE pro_ctc_terms ALTER COLUMN term DROP NOT NULL')
      	execute('ALTER TABLE ctc_terms ALTER COLUMN term DROP NOT NULL')
+     	execute('ALTER TABLE meddra_llt ALTER COLUMN meddra_term DROP NOT NULL')
+     	execute('ALTER TABLE pro_ctc_questions ALTER COLUMN question_text DROP NOT NULL')
+     	execute('ALTER TABLE meddra_questions ALTER COLUMN question_text DROP NOT NULL')
+     	execute('ALTER TABLE meddra_valid_values ALTER COLUMN value DROP NOT NULL')
+     	execute('ALTER TABLE pro_ctc_valid_values ALTER COLUMN value DROP NOT NULL')
+
     }
     
 
