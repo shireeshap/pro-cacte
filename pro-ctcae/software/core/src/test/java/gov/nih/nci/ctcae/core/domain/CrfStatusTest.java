32bb36a770b9cbabfc81b67d8b0c1c97062d4581
kogantir <kogantir@ncias-d1508-v.nci.nih.gov>
2011-07-11 17:23:43 +0000

    PRKC-974 : Fixed

--- pro-ctcae/software/core/src/test/java/gov/nih/nci/ctcae/core/domain/CrfStatusTest.java
+++ pro-ctcae/software/core/src/test/java/gov/nih/nci/ctcae/core/domain/CrfStatusTest.java
@@ -11,14 +11,14 @@ public class CrfStatusTest extends TestCase {
         CrfStatus status = CrfStatus.DRAFT;
         assertEquals("Draft", status.toString());
         status = CrfStatus.RELEASED;
-        assertEquals("Released", status.toString());
+        assertEquals("Final", status.toString());
         status = CrfStatus.INPROGRESS;
         assertEquals("In-progress", status.toString());
     }
 
     public void testGetByCode() {
         assertEquals(CrfStatus.DRAFT, CrfStatus.getByCode("Draft"));
-        assertEquals(CrfStatus.RELEASED, CrfStatus.getByCode("Released"));
+        assertEquals(CrfStatus.RELEASED, CrfStatus.getByCode("Final"));
         assertEquals(CrfStatus.INPROGRESS, CrfStatus.getByCode("In-progress"));
 
     }
