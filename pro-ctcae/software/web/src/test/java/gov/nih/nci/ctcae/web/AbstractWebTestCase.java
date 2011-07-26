ae6ba497eb258393145a8de771bd7af468285145
gangoliv <gangoliv@ncias-d1508-v.nci.nih.gov>
2011-07-26 19:17:09 +0000

    PRKC-971: fixing test cases

--- pro-ctcae/software/web/src/test/java/gov/nih/nci/ctcae/web/AbstractWebTestCase.java
+++ pro-ctcae/software/web/src/test/java/gov/nih/nci/ctcae/web/AbstractWebTestCase.java
@@ -1,7 +1,15 @@
 package gov.nih.nci.ctcae.web;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import gov.nih.nci.ctcae.core.helper.TestDataManager;
 import gov.nih.nci.ctcae.web.reports.graphical.ReportResultsHelper;
+
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.mock.web.MockHttpSession;
@@ -17,6 +25,26 @@ public abstract class AbstractWebTestCase extends TestDataManager {
     protected MockHttpSession session;
     protected ControllerTools controllerTools;
 
+    @Override
+    protected String[] getConfigLocations() {
+        String[] configLocations = super.getConfigLocations();
+        Resource r = new FileSystemResource("");
+        String path = null;
+		try {
+			path = r.getFile().getCanonicalPath();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        List<String> list = new ArrayList<String>(Arrays.asList(configLocations));
+        list.add("classpath*:gov/nih/nci/ctcae/web/applicationContext-web-dwr.xml");
+        list.add("classpath*:gov/nih/nci/ctcae/web/applicationContext-web-security.xml");
+        list.add("classpath*:gov/nih/nci/ctcae/web/applicationContext-web-common.xml");
+        list.add("classpath*:gov/nih/nci/ctcae/web/applicationContext-validator.xml");
+//        list.add(String.format("file:" + path + "/web/src/main/webapp/WEB-INF/spring-servlet.xml"));
+        return list.toArray(new String[]{});
+    }
+    
     @Override
     protected void onSetUpInTransaction() throws Exception {
         super.onSetUpInTransaction();
