e375dfc313b2cf76faa47f882aadae214b63e8f9
gangoliv <gangoliv@ncias-d1508-v.nci.nih.gov>
2011-07-29 19:27:29 +0000

    PRKC-971: added logging to solve the mystery of invalid setup.

--- pro-ctcae/software/core/src/test/java/gov/nih/nci/ctcae/core/helper/CrfTestHelper.java
+++ pro-ctcae/software/core/src/test/java/gov/nih/nci/ctcae/core/helper/CrfTestHelper.java
@@ -65,6 +65,7 @@ public class CrfTestHelper {
     }
 
     public static void createIVRSTestForm(Study study,TestDataManager localTestDataManager) throws Exception {
+        ParticipantTestHelper.createIVRSParticipant("ivrs", "participant", "007", study.getLeadStudySite(), 0,"1201201200",1234);
         if(myTestDataManager == null)
              localTestDataManager.commitAndStartNewTransaction();
         else
@@ -81,7 +82,6 @@ public class CrfTestHelper {
         crf = crfRepository.save(crf);
         crf.setEffectiveStartDate(DateUtils.addDaysToDate(new Date(), 2));
         crf = crfRepository.updateStatusToReleased(crf);
-        ParticipantTestHelper.createIVRSParticipant("ivrs", "participant", "007", study.getLeadStudySite(), 0,"1201201200",1234);
     }
 
     private static void createSecondaryForms() throws ParseException {
