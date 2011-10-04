40386459f6c75c74df9c4cc8d8c3896820754c11
kogantir <kogantir@ncias-d1508-v.nci.nih.gov>
2011-10-04 16:49:30 +0000

    PRKC-1296 : (gaurav)Fixed test case. Added % sign for short title search.

--- pro-ctcae/software/core/src/test/java/gov/nih/nci/ctcae/core/query/StudyQueryTest.java
+++ pro-ctcae/software/core/src/test/java/gov/nih/nci/ctcae/core/query/StudyQueryTest.java
@@ -52,11 +52,11 @@ public class StudyQueryTest extends TestCase {
     public void testFilterByLastName() throws Exception {
         StudyQuery studyQuery = new StudyQuery();
         studyQuery.filterStudiesByShortTitle("dow");
-        assertEquals("Select study from Study study WHERE study.id in (:objectIds ) AND lower(study.shortTitle) LIKE :shortTitle order by study.shortTitle",
+        assertEquals("Select study from Study study WHERE lower(study.shortTitle) LIKE :shortTitle) AND study.id in (:objectIds ) order by study.shortTitle",
                 studyQuery.getQueryString());
         assertEquals("wrong number of parameters", studyQuery.getParameterMap().size(), 1);
         assertTrue("missing parameter name", studyQuery.getParameterMap().containsKey("shortTitle"));
-        assertEquals("wrong parameter value", studyQuery.getParameterMap().get("shortTitle"), "dow");
+        assertEquals("wrong parameter value", studyQuery.getParameterMap().get("shortTitle"), "%dow%");
     }
 
     public void testFilterBySite() throws Exception {
