0df2dde5bcdab5935dc60e50f3025f202d2ad1b9
gulatim3 <gulatim3@ncias-d1508-v.nci.nih.gov>
2011-10-06 19:35:53 +0000

    PRKC-1329 PRKC-1330

--- pro-ctcae/software/web/src/main/java/gov/nih/nci/ctcae/web/participant/PrintSchedulePdfView.java
+++ pro-ctcae/software/web/src/main/java/gov/nih/nci/ctcae/web/participant/PrintSchedulePdfView.java
@@ -2,6 +2,7 @@ package gov.nih.nci.ctcae.web.participant;
 
 import com.lowagie.text.*;
 import com.lowagie.text.Font;
+import com.lowagie.text.pdf.PdfCell;
 import com.lowagie.text.pdf.PdfPCell;
 import com.lowagie.text.pdf.PdfPTable;
 import com.lowagie.text.pdf.PdfWriter;
@@ -9,8 +10,10 @@ import gov.nih.nci.ctcae.commons.utils.DateUtils;
 import gov.nih.nci.ctcae.constants.SupportedLanguageEnum;
 import gov.nih.nci.ctcae.core.domain.*;
 import gov.nih.nci.ctcae.core.repository.StudyParticipantCrfScheduleRepository;
+import org.jfree.ui.Align;
 import org.springframework.web.servlet.view.document.AbstractPdfView;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.awt.*;
@@ -42,14 +45,84 @@ public class PrintSchedulePdfView extends AbstractPdfView {
         ArrayList<ProCtcQuestion> participantAddedProctcQuestions;
         Map<String, ArrayList<MeddraQuestion>> participantAddedMeddraSymptomMap = new LinkedHashMap();
         ArrayList<MeddraQuestion> participantAddedMeddraQuestions;
-        document.add(new Paragraph("Study: " + study.getDisplayName()));
-        document.add(new Paragraph("Form: " + studyParticipantCrfSchedule.getStudyParticipantCrf().getCrf().getTitle()));
-        document.add(new Paragraph("Study site: " + studySite.getDisplayName()));
-        document.add(new Paragraph("Participant: " + participant.getDisplayName() + " [" + participant.getAssignedIdentifier() + "]"));
-        document.add(new Paragraph("Start date: " + DateUtils.format(studyParticipantCrfSchedule.getStartDate())));
-        document.add(new Paragraph("Recall period: " + studyParticipantCrfSchedule.getStudyParticipantCrf().getCrf().getRecallPeriod()));
-        document.add(new Paragraph(" "));
-        document.add(new Paragraph(" "));
+//        document.add(new Paragraph("Study: " + study.getDisplayName()));
+//        document.add(new Paragraph("Survey: " + studyParticipantCrfSchedule.getStudyParticipantCrf().getCrf().getTitle()));
+//        document.add(new Paragraph("Study site: " + studySite.getDisplayName()));
+//        document.add(new Paragraph("Participant: " + participant.getDisplayName() + " [" + participant.getAssignedIdentifier() + "]"));
+//        document.add(new Paragraph("Survey start date: " + DateUtils.format(studyParticipantCrfSchedule.getStartDate())));
+//        document.add(new Paragraph("Survey due date: " + DateUtils.format(studyParticipantCrfSchedule.getDueDate())));
+//        document.add(new Paragraph(" "));
+//        document.add(new Paragraph(" "));
+        Table table1 = new Table(2, 8);//2 Columns and 8 line
+        table1.setWidth(100);
+        table1.setWidths(new int[]{20, 80});
+        table1.setBorderWidth(0);
+        Cell c1 = new Cell(new Paragraph("Study:"));
+        c1.setBorderWidth(0);
+        table1.addCell(c1);
+
+        Cell c2 = new Cell(new Paragraph("" + study.getDisplayName()));
+        c2.setBorderWidth(0);
+        table1.addCell(c2);
+
+        Cell c3 = new Cell(new Paragraph("Survey:"));
+        c3.setBorderWidth(0);
+        table1.addCell(c3);
+
+        Cell c4 = new Cell(new Paragraph("" + studyParticipantCrfSchedule.getStudyParticipantCrf().getCrf().getTitle()));
+        c4.setBorderWidth(0);
+        table1.addCell(c4);
+
+        Cell c5 = new Cell(new Paragraph("Study site:"));
+        c5.setBorderWidth(0);
+        table1.addCell(c5);
+
+        Cell c6 = new Cell(new Paragraph("" + studySite.getDisplayName()));
+        c6.setBorderWidth(0);
+        table1.addCell(c6);
+
+        Cell c7 = new Cell(new Paragraph("Participant:"));
+        c7.setBorderWidth(0);
+        table1.addCell(c7);
+
+        Cell c8 = new Cell(new Paragraph("" + participant.getDisplayName() + " [" + participant.getAssignedIdentifier() + "]"));
+        c8.setBorderWidth(0);
+        table1.addCell(c8);
+
+        Cell c9 = new Cell(new Paragraph("Survey start date:"));
+        c9.setBorderWidth(0);
+        table1.addCell(c9);
+
+        Cell c10 = new Cell(new Paragraph("" + DateUtils.format(studyParticipantCrfSchedule.getStartDate())));
+        c10.setBorderWidth(0);
+        table1.addCell(c10);
+
+        Cell c11 = new Cell(new Paragraph("Survey due date:"));
+        c11.setBorderWidth(0);
+        table1.addCell(c11);
+
+        Cell c12 = new Cell(new Paragraph("" + DateUtils.format(studyParticipantCrfSchedule.getDueDate())));
+        c12.setBorderWidth(0);
+        table1.addCell(c12);
+
+        Cell c13 = new Cell(new Paragraph("\n"));
+        c13.setBorderWidth(0);
+        table1.addCell(c13);
+
+        Cell c14 = new Cell(new Paragraph("\n"));
+        c14.setBorderWidth(0);
+        table1.addCell(c14);
+        document.add(table1);
+
+        HeaderFooter footer = new HeaderFooter(new Phrase("[ CONFIDENIAL ]                         ", new Font(Font.COURIER, 12, Font.NORMAL)), true);
+        document.setFooter(footer);
+        Phrase p = new Phrase();
+        p.clear();
+        p.add(table1);
+        HeaderFooter header = new HeaderFooter(p, false);
+        document.setHeader(header);
+
+
         studyParticipantCrfSchedule.addParticipantAddedQuestions();
         if (studyParticipantCrfSchedule.getStudyParticipantCrfScheduleAddedQuestions().size() > 0) {
             for (StudyParticipantCrfScheduleAddedQuestion studyParticipantCrfScheduleAddedQuestion : studyParticipantCrfSchedule.getStudyParticipantCrfScheduleAddedQuestions()) {
@@ -95,7 +168,7 @@ public class PrintSchedulePdfView extends AbstractPdfView {
         Font f1 = FontFactory.getFont("Arial", 13f);
 
         for (ProCtcTerm proCtcTerm : symptomMap.keySet()) {
-            PdfPTable table = new PdfPTable(new float[]{2.5f, 1f, 1f, 1f, 1f, 1f});
+            PdfPTable table = new PdfPTable(new float[]{1f, 1f, 1f, 1f, 1f, 1f, 1f});
             table.setWidthPercentage(98);
             PdfPCell cell;
             if (language.equals("en")) {
@@ -104,7 +177,7 @@ public class PrintSchedulePdfView extends AbstractPdfView {
                 cell = new PdfPCell(new Paragraph(proCtcTerm.getProCtcTermVocab().getTermSpanish()));
             }
             cell.setBackgroundColor(Color.lightGray);
-            cell.setColspan(6);
+            cell.setColspan(8);
             table.addCell(cell);
 //            BaseFont bf;
 //            bf = BaseFont.createFont("ZapfDingbats", "ZapfDingbats", true);
@@ -117,6 +190,7 @@ public class PrintSchedulePdfView extends AbstractPdfView {
                 } else {
                     cell2 = new PdfPCell(new Paragraph(proCtcQuestion.getQuestionText(SupportedLanguageEnum.SPANISH), f));
                 }
+                cell2.setColspan(8);
                 table.addCell(cell2);
                 int i = 0;
                 for (ProCtcValidValue proCtcValidValue : proCtcQuestion.getValidValues()) {
@@ -132,7 +206,7 @@ public class PrintSchedulePdfView extends AbstractPdfView {
                     cell3.setBorderWidth(0.0f);
 
                 }
-                for (int j = 0; j < 5 - i; j++) {
+                for (int j = 0; j < 7 - i; j++) {
                     PdfPCell cell4 = new PdfPCell(new Paragraph(""));
                     table.addCell(cell4);
                     cell4.setBorderWidth(0.0f);
@@ -148,7 +222,7 @@ public class PrintSchedulePdfView extends AbstractPdfView {
 
         if (participantAddedProctcSymptomMap.keySet().size() > 0) {
             for (ProCtcTerm proCtcTerm : participantAddedProctcSymptomMap.keySet()) {
-                PdfPTable table = new PdfPTable(new float[]{2.5f, 1f, 1f, 1f, 1f, 1f});
+                PdfPTable table = new PdfPTable(new float[]{1f, 1f, 1f, 1f, 1f, 1f, 1f});
                 table.setWidthPercentage(98);
                 PdfPCell cell;
                 if (language.equals("en")) {
@@ -157,7 +231,7 @@ public class PrintSchedulePdfView extends AbstractPdfView {
                     cell = new PdfPCell(new Paragraph(proCtcTerm.getProCtcTermVocab().getTermSpanish()));
                 }
                 cell.setBackgroundColor(Color.lightGray);
-                cell.setColspan(6);
+                cell.setColspan(8);
                 table.addCell(cell);
 
                 for (ProCtcQuestion proCtcQuestion : participantAddedProctcSymptomMap.get(proCtcTerm)) {
@@ -167,7 +241,7 @@ public class PrintSchedulePdfView extends AbstractPdfView {
                     } else {
                         cell2 = new PdfPCell(new Paragraph(proCtcQuestion.getQuestionText(SupportedLanguageEnum.SPANISH), f));
                     }
-
+                    cell2.setColspan(8);
                     table.addCell(cell2);
                     int i = 0;
                     for (ProCtcValidValue proCtcValidValue : proCtcQuestion.getValidValues()) {
@@ -184,7 +258,7 @@ public class PrintSchedulePdfView extends AbstractPdfView {
                         cell3.setBorderWidth(0.0f);
 
                     }
-                    for (int j = 0; j < 5 - i; j++) {
+                    for (int j = 0; j < 8 - i; j++) {
                         PdfPCell cell4 = new PdfPCell(new Paragraph(""));
                         table.addCell(cell4);
                         cell4.setBorderWidth(0.0f);
@@ -199,11 +273,11 @@ public class PrintSchedulePdfView extends AbstractPdfView {
 
         if (participantAddedMeddraSymptomMap.keySet().size() > 0) {
             for (String meddraTerm : participantAddedMeddraSymptomMap.keySet()) {
-                PdfPTable table = new PdfPTable(new float[]{2.5f, 1f, 1f, 1f, 1f, 1f});
+                PdfPTable table = new PdfPTable(new float[]{1f, 1f, 1f, 1f, 1f, 1f, 1f});
                 table.setWidthPercentage(98);
                 PdfPCell cell = new PdfPCell(new Paragraph(meddraTerm));
                 cell.setBackgroundColor(Color.lightGray);
-                cell.setColspan(6);
+                cell.setColspan(8);
                 table.addCell(cell);
 
                 for (MeddraQuestion meddraQuestion : participantAddedMeddraSymptomMap.get(meddraTerm)) {
@@ -213,6 +287,7 @@ public class PrintSchedulePdfView extends AbstractPdfView {
                     } else {
                         cell2 = new PdfPCell(new Paragraph(meddraQuestion.getQuestionText(SupportedLanguageEnum.SPANISH), f));
                     }
+                    cell2.setColspan(8);
                     table.addCell(cell2);
                     int i = 0;
                     for (MeddraValidValue meddraValidValue : meddraQuestion.getValidValues()) {
@@ -228,7 +303,7 @@ public class PrintSchedulePdfView extends AbstractPdfView {
                         cell3.setBorderWidth(0.0f);
 
                     }
-                    for (int j = 0; j < 5 - i; j++) {
+                    for (int j = 0; j < 8 - i; j++) {
                         PdfPCell cell4 = new PdfPCell(new Paragraph(""));
                         table.addCell(cell4);
                         cell4.setBorderWidth(0.0f);
@@ -240,8 +315,29 @@ public class PrintSchedulePdfView extends AbstractPdfView {
                 document.add(new Paragraph(" "));
             }
         }
+        PdfPTable table2 = new PdfPTable(new float[]{1f});
+        table2.setWidthPercentage(98);
+        PdfPCell cell = new PdfPCell(new Paragraph("Notes"));
+        cell.setBackgroundColor(Color.lightGray);
+        cell.setColspan(1);
+        table2.addCell(cell);
 
+        PdfPCell cell1 = new PdfPCell(new Paragraph("\n"));
+        cell1.setBackgroundColor(Color.WHITE);
+        cell1.setColspan(1);
+        table2.addCell(cell1);
 
+        PdfPCell cell2 = new PdfPCell(new Paragraph("\n"));
+        cell2.setBackgroundColor(Color.WHITE);
+        cell2.setColspan(1);
+        table2.addCell(cell2);
+
+        PdfPCell cell3 = new PdfPCell(new Paragraph("\n"));
+        cell3.setBackgroundColor(Color.WHITE);
+        cell3.setColspan(1);
+        table2.addCell(cell3);
+        document.add(table2);
+        document.add(new Paragraph(" "));
     }
 
     static String cst(char c) {
