5be0341857dc9dc64de2c8db507d92ea3a5031b8
gangoliv <gangoliv@ncias-d1508-v.nci.nih.gov>
2011-11-09 18:39:32 +0000

    PRKC-1406: Implemented the blackout feature.

--- pro-ctcae/software/web/src/main/java/gov/nih/nci/ctcae/web/ivrs/callout/IvrsMessageListener.java
+++ pro-ctcae/software/web/src/main/java/gov/nih/nci/ctcae/web/ivrs/callout/IvrsMessageListener.java
@@ -4,12 +4,15 @@ import gov.nih.nci.cabig.ctms.audit.domain.DataAuditInfo;
 import gov.nih.nci.ctcae.core.domain.CrfStatus;
 import gov.nih.nci.ctcae.core.domain.IvrsCallStatus;
 import gov.nih.nci.ctcae.core.domain.IvrsSchedule;
+import gov.nih.nci.ctcae.core.domain.StudyParticipantAssignment;
 import gov.nih.nci.ctcae.core.repository.IvrsScheduleRepository;
 
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.Properties;
+import java.util.TimeZone;
 
 import javax.jms.JMSException;
 import javax.jms.Message;
@@ -41,6 +44,10 @@ public class IvrsMessageListener implements MessageListener, ApplicationContextA
 	public static final String IVRS_CALLER_ID = "ivrs.callerid";
 	public static final int port = 5038;
 	
+	//values like 21:00 and 04:59
+	public static final String BLACKOUT_START = "ivrs.blackout.start";
+	public static final String BLACKOUT_END = "ivrs.blackout.end";
+
 	protected static final Log logger = LogFactory.getLog(IvrsMessageListener.class);
     
 	private IvrsScheduleRepository ivrsScheduleRepository;
@@ -66,12 +73,18 @@ public class IvrsMessageListener implements MessageListener, ApplicationContextA
 		IvrsSchedule ivrsSchedule = null;
 		ivrsScheduleRepository = (IvrsScheduleRepository) applicationContext.getBean("ivrsScheduleRepository");
 		//ManagerResponse originateResponse;
+		
 		try {
 			ObjectMessage msg = (ObjectMessage) message;;
 			callAction = (CallAction)msg.getObject();
 			Integer ivrsScheduleId = callAction.getIvrsScheduleId();
 			ivrsSchedule = ivrsScheduleRepository.findById(ivrsScheduleId);
 	        
+			if(isCurrentTimeInBlackoutRange(ivrsSchedule) || 
+					isCallASpilloverFromYesterdayOrEarlier(ivrsSchedule)){
+				return;
+			}
+			
 			DataAuditInfo auditInfo = new DataAuditInfo("admin", "localhost", new Date(), "127.0.0.0");
 	        DataAuditInfo.setLocal(auditInfo);
 			
@@ -153,6 +166,70 @@ public class IvrsMessageListener implements MessageListener, ApplicationContextA
 	}
 
     
+	/**
+	 * Checks if the call is a spillover from yesterday or earlier. This can happen in case of an outage. 
+	 * If call was originally scheduled for yesterday or earlier then do not make it.
+	 * Note that the callCount is not reverted in this case.
+	 *
+	 * @param spa the spa
+	 * @return true, if is call a spillover from yesterday or earlier
+	 */
+	private boolean isCallASpilloverFromYesterdayOrEarlier(IvrsSchedule ivrsSchedule ) {
+		boolean returnBool = false;
+		
+		Calendar cal = Calendar.getInstance();
+		int today = cal.get(Calendar.DAY_OF_MONTH);
+		
+		cal.setTime(ivrsSchedule.getNextCallTime());
+		int scheduled = cal.get(Calendar.DAY_OF_MONTH);
+		if(scheduled != today){
+			logger.error("Aborting call for ivrsSchedule.id="+ ivrsSchedule.getId() + ". Reason: Call not originally scheduled for today.");
+			returnBool = true;
+		}
+		return returnBool;
+	}
+
+	/**
+	 * Checks if is current time in the Participant's timezone is in blackout range.
+	 *
+	 * @return true, if is current time in blackout range
+	 */
+	private boolean isCurrentTimeInBlackoutRange(IvrsSchedule ivrsSchedule) {
+		
+		StudyParticipantAssignment spa = ivrsSchedule.getStudyParticipantAssignment();
+		boolean returnBool = false;
+		String blackoutStartTime = properties.getProperty(BLACKOUT_START);
+    	String blackoutEndTime = properties.getProperty(BLACKOUT_END);
+    	
+    	String [] sTokens = blackoutStartTime.split(":");
+    	int hhStart, mmStart;
+    	hhStart = Integer.valueOf(sTokens[0]);
+    	mmStart = Integer.valueOf(sTokens[1]);
+    	
+    	String [] eTokens = blackoutEndTime.split(":");
+    	int hhEnd, mmEnd;
+    	hhEnd = Integer.valueOf(eTokens[0]);
+    	mmEnd = Integer.valueOf(eTokens[1]);
+    	
+    	Calendar cal = Calendar.getInstance(TimeZone.getTimeZone(spa.getCallTimeZone()));
+		if(cal.get(Calendar.HOUR_OF_DAY) > hhStart || cal.get(Calendar.HOUR_OF_DAY) < hhEnd){
+			logger.error("Aborting call for ivrsSchedule.id="+ ivrsSchedule.getId() + ". Reason: Call hour in blackout range.");
+			returnBool = true;
+		} else if(cal.get(Calendar.HOUR_OF_DAY) == hhStart){
+			if(cal.get(Calendar.MINUTE) >= mmStart){
+				logger.error("Aborting call for ivrsSchedule.id="+ ivrsSchedule.getId() + ". Reason: Call min in blackout range.");
+				returnBool = true;
+			}
+		} else if(cal.get(Calendar.HOUR_OF_DAY) == hhEnd){
+			if(cal.get(Calendar.MINUTE) <= mmEnd){
+				logger.error("Aborting call for ivrsSchedule.id="+ ivrsSchedule.getId() +". Reason:Call min in blackout range.");
+				returnBool = true;
+			}
+		}
+		
+		return returnBool;
+	}
+
 	/**
 	 * Mark schedule as failed and reset the callcount as the call never went through.
 	 *
