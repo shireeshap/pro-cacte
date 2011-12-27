beb98b65f2946d316ce28844ad8924d8a94e093b
gangoliv <gangoliv@ncias-d1508-v.nci.nih.gov>
2011-12-27 16:21:31 +0000

    Organized Imports

--- pro-ctcae/software/core/src/main/java/gov/nih/nci/ctcae/core/domain/Organization.java
+++ pro-ctcae/software/core/src/main/java/gov/nih/nci/ctcae/core/domain/Organization.java
@@ -1,10 +1,15 @@
 package gov.nih.nci.ctcae.core.domain;
 
 import gov.nih.nci.ctcae.core.validation.annotation.UniqueNciIdentifierForOrganization;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.annotations.Parameter;
-
-import javax.persistence.*;
 
 //
 /**
--- pro-ctcae/software/core/src/main/java/gov/nih/nci/ctcae/core/domain/OrganizationClinicalStaff.java
+++ pro-ctcae/software/core/src/main/java/gov/nih/nci/ctcae/core/domain/OrganizationClinicalStaff.java
@@ -1,12 +1,22 @@
 package gov.nih.nci.ctcae.core.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+
 import org.hibernate.annotations.Cascade;
 import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.annotations.Parameter;
-
-import javax.persistence.*;
-import java.util.ArrayList;
-import java.util.List;
 
 //
 /**
