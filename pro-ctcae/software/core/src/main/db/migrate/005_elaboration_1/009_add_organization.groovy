a780e73c568141bb87dd67c8bf6a18f9b895f4df
gangoliv <gangoliv@ncias-d1508-v.nci.nih.gov>
2012-07-31 17:21:51 +0000

    PRKC-1685: MA185 was being inserted twice via 009 and 013.

--- pro-ctcae/software/core/src/main/db/migrate/005_elaboration_1/009_add_organization.groovy
+++ pro-ctcae/software/core/src/main/db/migrate/005_elaboration_1/009_add_organization.groovy
@@ -1,6 +1,6 @@
  class AddOrganization extends edu.northwestern.bioinformatics.bering.Migration {
 	void up() {
-        insert('organizations', [id: 108017, nci_institute_code: "MA185", name: "Dana-Farber/Brigham and Women's Cancer Center at South Shore"], primaryKey: false)
+        //insert('organizations', [id: 108017, nci_institute_code: "MA185", name: "Dana-Farber/Brigham and Women's Cancer Center at South Shore"], primaryKey: false)
         }
         void down() {
 
