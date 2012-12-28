ff074a172a521ce25668fde39a2ac30614724c95
sanea <sanea@ncias-d1508-v.nci.nih.gov>
2012-12-28 20:12:50 +0000

    PRKC-1798: Make a user_role available to the logged in user, only if that user_role is Active on atleast one of the assigned studies

--- pro-ctcae/software/core/src/main/java/gov/nih/nci/ctcae/core/query/UserQuery.java
+++ pro-ctcae/software/core/src/main/java/gov/nih/nci/ctcae/core/query/UserQuery.java
@@ -1,6 +1,7 @@
 package gov.nih.nci.ctcae.core.query;
 
 import gov.nih.nci.ctcae.core.domain.Role;
+import gov.nih.nci.ctcae.core.domain.UserRole;
 
 //
 
@@ -16,8 +17,10 @@ public class UserQuery extends AbstractQuery {
     private static String queryString = "SELECT user from User user order by user.id";
     private static String queryString1 = "SELECT un from UserNotification un";
     private static String queryString2 = "SELECT count(distinct un) from UserNotification un";
+    private static String queryString3 = "SELECT count(*) from StudyOrganizationClinicalStaff socs";
     private static String USER_NAME = "username";
     private static String ROLE = "role";
+    private static String ROLE_NAME = "roleName";
     private static String TOKEN = "token";
     private static String MARK_DELETE = "markDelete";
 
@@ -33,6 +36,10 @@ public class UserQuery extends AbstractQuery {
         super(queryString2);
     }
 
+    public UserQuery(boolean notification, boolean count, boolean checkActiveRole){
+    	super(queryString3);
+    }
+    
     /**
      * Filter by clinical staff first name.
      *
@@ -68,4 +75,15 @@ public class UserQuery extends AbstractQuery {
         setParameter(USER_NAME, userName.toLowerCase());
         setParameter(MARK_DELETE, false);
     }
+    
+    public void filterInActiveRoles(Integer userId, Role roleName){
+    	leftJoin(" socs.organizationClinicalStaff ocs left join ocs.clinicalStaff cs left join cs.user u ");
+    	andWhere(" u.id = :id");
+    	setParameter("id", userId);
+    	andWhere("socs.role = :ROLE_NAME");
+    	setParameter("ROLE_NAME", roleName);
+    	andWhere(" socs.roleStatus = 'ACTIVE'");
+    }
+    
+    
 }
