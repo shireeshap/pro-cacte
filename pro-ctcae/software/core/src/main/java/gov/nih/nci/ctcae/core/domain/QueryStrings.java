60c82ebf14df6eff53f8b8eccab95f8830e62127
sanea <sanea@ncias-d1508-v.nci.nih.gov>
2013-01-08 14:36:45 +0000

    PRKC-1981: Consolidate constructors for query classes and supply the query strings using enum class object

--- pro-ctcae/software/core/src/main/java/gov/nih/nci/ctcae/core/domain/QueryStrings.java
+++ pro-ctcae/software/core/src/main/java/gov/nih/nci/ctcae/core/domain/QueryStrings.java
@@ -10,22 +10,40 @@ Author: Amey
 
 public enum  QueryStrings implements CodedEnum<String>{
 	
-	PARTICIPANT_QUERY_STRING("SELECT p from Participant p order by p.id"), 
-	PARTICIPANT_QUERY_STRING1("SELECT count(distinct p) from Participant p"),
-	PARTICIPANT_QUERY_STRING2("SELECT p from Participant p"),
-	STUDY_QUERY_STRING("Select distinct study from Study study order by study.shortTitle "),
-	STUDY_QUERY_STRING1("SELECT count(distinct study) from Study study "),
-	STUDY_QUERY_STRING2("SELECT distinct study from Study study "),
-	STUDY_QUERY_STRING3("SELECT study from Study study left outer join study.studyOrganizations as so "),
-	CRF_QUERY_STRING("SELECT o from CRF o order by o.id "),
-	CRF_QUERY_STRING1("SELECT count(distinct o) from CRF o"),
-	CRF_QUERY_STRING2("SELECT o from CRF o "),
-	STAFF_QUERY_STRING("SELECT distinct cs from ClinicalStaff cs order by cs.id "),
-	STAFF_QUERY_STRING1("SELECT count(distinct cs) from ClinicalStaff cs "),
-	STAFF_QUERY_STRING2("SELECT distinct cs from ClinicalStaff cs ");
+	PARTICIPANT_QUERY_BASIC("SELECT p from Participant p order by p.id"), 
+	PARTICIPANT_QUERY_COUNT("SELECT count(distinct p) from Participant p"),
+	PARTICIPANT_QUERY_WITH_JOINS("SELECT p from Participant p"),
+	
+	STUDY_QUERY_BASIC("Select distinct study from Study study order by study.shortTitle "),
+	STUDY_QUERY_COUNT("SELECT count(distinct study) from Study study "),
+	STUDY_QUERY_SORTBY_FIELDS("SELECT distinct study from Study study "),
+	STUDY_QUERY_SORTBY_FSP_DCC("SELECT study from Study study left outer join study.studyOrganizations as so "),
+	
+	CRF_QUERY_BASIC("SELECT o from CRF o order by o.id "),
+	CRF_QUERY_COUNT("SELECT count(distinct o) from CRF o"),
+	CRF_QUERY_SORTBY_FIELDS("SELECT o from CRF o "),
+	
+	STAFF_QUERY_BASIC("SELECT distinct cs from ClinicalStaff cs order by cs.id "),
+	STAFF_QUERY_COUNT("SELECT count(distinct cs) from ClinicalStaff cs "),
+	STAFF_QUERY_SORTBY_FIELDS("SELECT distinct cs from ClinicalStaff cs "),
+    
+	USER_QUERY_BASIC("SELECT user from User user order by user.id"),
+    UN_QUERY_BASIC("SELECT un from UserNotification un"),
+    UN_QUERY_COUNT("SELECT count(distinct un) from UserNotification un"),
+    
+    SOCS_QUERY_COUNT("SELECT count(*) from StudyOrganizationClinicalStaff socs"),
+    SOCS_QUERY_BASIC("SELECT socs from StudyOrganizationClinicalStaff socs order by socs.organizationClinicalStaff.clinicalStaff.firstName"),
+   
+    OCS_QUERY_BASIC("SELECT scs from OrganizationClinicalStaff scs order by scs.clinicalStaff.firstName"),
+    
+    ORGANIZATION_QUERY_BASIC("SELECT distinct(o) from Organization o order by o.id "),
+	ORGANIZATION_QUERY_FILTER_STUDYSITES("SELECT distinct(o) from Organization o "),
+	
+	SO_QUERY_BASIC("SELECT o from StudyOrganization o order by o.organization.name ");
+	
+	
 	
 	private String queryString;
-	//private String queryString;
 	
 	QueryStrings(String queString){
 		this.queryString=queString;		
