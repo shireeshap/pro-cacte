4b544471f31dfa99c1755e570cb6bf4e3bafc8d6
sanea <sanea@ncias-d1508-v.nci.nih.gov>
2013-01-18 16:17:22 +0000

    PRKC-1991: Test case for gov.nih.nci.ctcae.core.dao

---
+++ pro-ctcae/software/core/src/test/java/gov/nih/nci/ctcae/core/dao/ProCtcDaoTest.java
@@ -0,0 +1,65 @@
+package gov.nih.nci.ctcae.core.dao;
+
+import java.util.ArrayList;
+import java.util.List;
+import gov.nih.nci.ctcae.core.helper.TestDataManager;
+import gov.nih.nci.ctcae.core.domain.MeddraVersion;
+import gov.nih.nci.ctcae.core.domain.meddra.LowLevelTerm;
+
+/** Test class for Dao Package.
+ *  Tests cases covers ProCtcDao, MeddraVersionDao and LowLevelTermDao 
+ * @author AmeyS
+ *
+ */
+public class ProCtcDaoTest extends TestDataManager{
+	
+	public ProCtcDaoTest(){
+	}
+
+
+	public void testFindAll(){
+		List<LowLevelTerm> lowLevelTerm = lowLevelTermDao.findAll(null);
+		assertEquals(lowLevelTerm.size(), 1084);
+	}
+	
+	
+	public void testFindBySubname(){
+		List<LowLevelTerm> lowLevelTerm = lowLevelTermDao.findBySubname(null, null, null,null, null);
+		assertTrue(lowLevelTerm.size() == 0);
+		
+	}
+	
+	public void testGetByMeddraCode(){
+		List<LowLevelTerm> lowLevelTerm = lowLevelTermDao.getByMeddraCode("10000424");
+		if(!lowLevelTerm.isEmpty()){
+			LowLevelTerm llt = lowLevelTerm.get(0);
+			assertEquals(llt.getLowLevelTermVocab().getMeddraTermEnglish(), "Ache");
+		}
+	}
+	
+	
+	public void testGetMeddraByName(){
+		MeddraVersion testMeddraVersion = new MeddraVersion();
+		testMeddraVersion.setName("testMeddra");
+		testMeddraVersion.setId(99);
+		meddraVersionDao.save(testMeddraVersion);
+		
+		List<MeddraVersion> meddraVersion = meddraVersionDao.getMeddraByName("testMeddra");
+		assertEquals(testMeddraVersion.getId(), meddraVersion.get(0).getId());
+	}
+	
+	// testing the save and delete operations for MeddraVersionDao
+	public void testSaveAndDelete(){
+		MeddraVersion testMeddraVersion = new MeddraVersion();
+		testMeddraVersion.setName("testMeddra");
+		testMeddraVersion.setId(99);
+	
+		meddraVersionDao.save(testMeddraVersion);
+		meddraVersionDao.flush();
+		assertEquals(true, testMeddraVersion.equals(meddraVersionDao.getById(99)));
+		
+		meddraVersionDao.delete(testMeddraVersion);
+		assertTrue(meddraVersionDao.getMeddraByName("testMeddra").size() == 0);	
+	}
+	
+}
