85bcd49a7d1d209fccebbc6ccd6f6905fdf6ff8b
sanea <sanea@ncias-d1508-v.nci.nih.gov>
2013-01-21 22:03:10 +0000

    PRKC-1985: Generating only 'studyOrganization' and 'studyParticipantCrf' access-privileges for studyParticipantCrfSchedule persistable class

--- software/core/src/main/java/gov/nih/nci/ctcae/core/security/DomainObjectPrivilegeGenerator.java
+++ software/core/src/main/java/gov/nih/nci/ctcae/core/security/DomainObjectPrivilegeGenerator.java
@@ -69,9 +69,13 @@ public class DomainObjectPrivilegeGenerator {
     }
 
     private Set<String> generatePrivilege(StudyParticipantCrfSchedule studyParticipantCrfSchedule) {
-    	Set<String> privileges = new HashSet<String>();
-        //for studyParticipantCrfSchedule persistable class, only generate studyOrganization privileges.
+        Set<String> privileges = new HashSet<String>();
+        //for studyParticipantCrfSchedule persistable class, only generate studyOrganization and studyParticipantCrf privileges.
         privileges.addAll(generatePrivilege(studyParticipantCrfSchedule.getStudyParticipantCrf().getStudyParticipantAssignment().getStudySite()));
+        List<StudyParticipantCrf> studyParticipantCrfs = studyParticipantCrfSchedule.getStudyParticipantCrf().getStudyParticipantAssignment().getStudyParticipantCrfs();
+        for (StudyParticipantCrf studyParticipantCrf : studyParticipantCrfs) {
+            privileges.add(generatePrivilege(studyParticipantCrf));
+        }
         return privileges;
     }
 
