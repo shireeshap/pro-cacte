878ee02736678248ab7e2cf403e3527fccabb45e
sanea <sanea@ncias-d1508-v.nci.nih.gov>
2013-01-29 19:34:30 +0000

    PRKC-2012: Adding testcases for Arm.java

--- pro-ctcae/software/core/src/test/java/gov/nih/nci/ctcae/core/domain/ArmIntegrationTest.java
+++ pro-ctcae/software/core/src/test/java/gov/nih/nci/ctcae/core/domain/ArmIntegrationTest.java
@@ -1,5 +1,7 @@
 package gov.nih.nci.ctcae.core.domain;
 
+import java.util.List;
+
 import gov.nih.nci.ctcae.core.helper.StudyTestHelper;
 import gov.nih.nci.ctcae.core.helper.TestDataManager;
 
@@ -17,7 +19,34 @@ public class ArmIntegrationTest extends TestDataManager {
         assertEquals("desc 1", study.getArms().get(0).getDescription());
         assertEquals("test arm 2", study.getArms().get(1).getTitle());
         assertEquals("desc 2", study.getArms().get(1).getDescription());
+    }
+    
+    public void testEqualsAndHashCode(){
+    	Study study = StudyTestHelper.getDefaultStudy();
+    	Arm studyArm = study.getArms().get(0);
+    	Arm fetchedArm = fetchArmFromRepository(studyArm.getTitle(), study.getId()).get(0);
+    	Arm otherArm = fetchArmFromRepositoryForOtherStudy(studyArm.getTitle(), study.getId()).get(0);
 
+    	assertTrue(studyArm.equals(fetchedArm));
+    	assert(studyArm.hashCode() == fetchedArm.hashCode());
+    	
+    	assertFalse(studyArm.equals(otherArm));
+    	assert(studyArm.hashCode() != otherArm.hashCode());
+    }
+    
+    public void testIsDefaultArm(){
+    	Study study = StudyTestHelper.getDefaultStudy();
+    	Arm studyArm = study.getArms().get(0);
+    	
+    	assertFalse(studyArm.isDefaultArm());
+    }
+    
+    public List<Arm> fetchArmFromRepository(String title, Integer studyId){
+    	return hibernateTemplate.find("from Arm where title = ? and study.id = ?", new Object[]{title,studyId});
+    }
+
+    public List<Arm> fetchArmFromRepositoryForOtherStudy(String title, Integer studyId){
+    	return hibernateTemplate.find("from Arm where title = ? and study.id <> ?", new Object[]{title,studyId});
     }
 
 
