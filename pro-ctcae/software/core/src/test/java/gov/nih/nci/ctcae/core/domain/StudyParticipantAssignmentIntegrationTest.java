6e3f5bedc0d2a0aa182e1eb5ba0ca958f924ab1e
sanea <sanea@ncias-d1508-v.nci.nih.gov>
2013-02-01 20:56:07 +0000

    PRKC-2012: adding test casses in StudyParticipantAssignmentIntegrationTest and ParticipantCommonTest.java for StudyParticipantAssignment.java

--- pro-ctcae/software/core/src/test/java/gov/nih/nci/ctcae/core/domain/StudyParticipantAssignmentIntegrationTest.java
+++ pro-ctcae/software/core/src/test/java/gov/nih/nci/ctcae/core/domain/StudyParticipantAssignmentIntegrationTest.java
@@ -13,16 +13,6 @@ import gov.nih.nci.ctcae.core.query.StudyOrganizationClinicalStaffQuery;
  */
 public class StudyParticipantAssignmentIntegrationTest extends TestDataManager {
 
-
-    private StudyParticipantAssignment assignment;
-
-    private Study study;
-
-    private StudySite nciStudySite;
-    private Organization nci;
-
-    private Participant participant;
-
     public void testGetClinicalStaff(){
     	Study study = StudyTestHelper.getDefaultStudy();
     	StudyParticipantAssignment spa = study.getArms().get(0).getStudyParticipantAssignments().get(0);
@@ -30,42 +20,30 @@ public class StudyParticipantAssignmentIntegrationTest extends TestDataManager {
     	StudyOrganizationClinicalStaffQuery query = new StudyOrganizationClinicalStaffQuery();
     	query.filterByStudyOrganization(studyOrganization.getId());
     	
-    	List<StudyOrganizationClinicalStaff> socsList = genericRepository.find(query);
     	List<StudyOrganizationClinicalStaff> socsTreatinPhyList = new ArrayList<StudyOrganizationClinicalStaff>();
     	List<StudyOrganizationClinicalStaff> socsResearchNursesList = new ArrayList<StudyOrganizationClinicalStaff>();
     	List<StudyOrganizationClinicalStaff> socsSitePIList = new ArrayList<StudyOrganizationClinicalStaff>();
     	List<StudyOrganizationClinicalStaff> socsSiteCRAList = new ArrayList<StudyOrganizationClinicalStaff>();
     	
-    	for(StudyOrganizationClinicalStaff socs : socsList){
-    		if(socs.getRole().equals(Role.TREATING_PHYSICIAN)){
-    			socsTreatinPhyList.add(socs);
-    		}
-    		if(socs.getRole().equals(Role.NURSE)){
-    			socsResearchNursesList.add(socs);
-    		}
-    		if(socs.getRole().equals(Role.SITE_PI)){
-    			socsSitePIList.add(socs);
-    		}
-    		if(socs.getRole().equals(Role.SITE_CRA)){
-    			socsSiteCRAList.add(socs);
-    		}
-    	}
-    	
+    	socsTreatinPhyList.addAll(studyOrganization.getTreatingPhysicians());
     	List<StudyParticipantClinicalStaff> spaTreatinPhyList = spa.getTreatingPhysicians();
     	for(StudyParticipantClinicalStaff spcs : spaTreatinPhyList){
     		assertTrue(socsTreatinPhyList.contains(spcs.getStudyOrganizationClinicalStaff()));
     	}
     	
+    	socsResearchNursesList.addAll(studyOrganization.getResearchNurses());
     	List<StudyParticipantClinicalStaff> spaResearchNurseList = spa.getResearchNurses();
     	for(StudyParticipantClinicalStaff spcs : spaResearchNurseList){
     		assertTrue(socsResearchNursesList.contains(spcs.getStudyOrganizationClinicalStaff()));
     	}
     	
+    	socsSitePIList.addAll(studyOrganization.getSitePIs());
     	List<StudyParticipantClinicalStaff> spaSitePIList = spa.getSitePIs();
     	for(StudyParticipantClinicalStaff spcs : spaSitePIList){
     		assertTrue(socsSitePIList.contains(spcs.getStudyOrganizationClinicalStaff()));
     	}
     	
+    	socsSiteCRAList.addAll(studyOrganization.getSiteCRAs());
     	List<StudyParticipantClinicalStaff> spaSiteCRAList = spa.getSiteCRAs();
     	for(StudyParticipantClinicalStaff spcs : spaSiteCRAList){
     		assertTrue(socsSiteCRAList.contains(spcs.getStudyOrganizationClinicalStaff()));
