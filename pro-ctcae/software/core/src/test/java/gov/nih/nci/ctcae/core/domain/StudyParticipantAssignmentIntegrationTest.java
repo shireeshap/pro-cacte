752efb154750eb91e98544be437fb9e030d6d3ec
sanea <sanea@ncias-d1508-v.nci.nih.gov>
2013-02-01 20:37:45 +0000

    PRKC-2012: adding test casses in StudyParticipantAssignmentIntegrationTest and ParticipantCommonTest.java for StudyParticipantAssignment.java

--- pro-ctcae/software/core/src/test/java/gov/nih/nci/ctcae/core/domain/StudyParticipantAssignmentIntegrationTest.java
+++ pro-ctcae/software/core/src/test/java/gov/nih/nci/ctcae/core/domain/StudyParticipantAssignmentIntegrationTest.java
@@ -1,6 +1,11 @@
 package gov.nih.nci.ctcae.core.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import gov.nih.nci.ctcae.core.helper.StudyTestHelper;
 import gov.nih.nci.ctcae.core.helper.TestDataManager;
+import gov.nih.nci.ctcae.core.query.StudyOrganizationClinicalStaffQuery;
 
 /**
  * @author Vinay Kumar
@@ -18,58 +23,54 @@ public class StudyParticipantAssignmentIntegrationTest extends TestDataManager {
 
     private Participant participant;
 
-    @Override
-    protected void onSetUpInTransaction() throws Exception {
-        super.onSetUpInTransaction();
-//
-//        OrganizationQuery query = new OrganizationQuery();
-//        query.setMaximumResults(10);
-//        List<Organization> organizations = new ArrayList<Organization>(organizationRepository.find(query));
-//
-//        nci = organizations.get(0);
-//
-//
-//        nciStudySite = new StudySite();
-//        nciStudySite.setOrganization(nci);
-//
-//
-//        study = Fixture.createStudy("study short title", "study long title", "assigned identifier");
-//        study.addStudySite(nciStudySite);
-//
-//        study = studyRepository.save(study);
-//
-//        assertNotNull(study.getId());
-//        assertNotNull(study.getId());
-//
-//        participant = Fixture.createParticipant("John", "Dow", "1234");
-//
-//        assignment = new StudyParticipantAssignment();
-//        assignment.setStudyParticipantIdentifier("id001");
-//        assignment.setParticipant(participant);
-//        assignment.setStudySite(nciStudySite);
-//
-//        participant.addStudyParticipantAssignment(assignment);
-//        participant = participantRepository.save(participant);
-//        assertNotNull(participant.getId());
-//        assertNotNull(participant.getId());
-
-    }
-
-    public void testSaveAssignment() {
-
-//        assignment = participant.getStudyParticipantAssignments().get(0);
-//        assignment.setStudyParticipantIdentifier("abc");
-//        StudyParticipantAssignment studyParticipantAssignment = studyParticipantAssignmentRepository.save(assignment);
-//        assertNotNull(studyParticipantAssignment.getId());
-//        assertNotNull(studyParticipantAssignment.getId());
-//        assertEquals(studyParticipantAssignment.getStudyParticipantIdentifier(), assignment.getStudyParticipantIdentifier());
-//        assertEquals(nciStudySite, studyParticipantAssignment.getStudySite());
-//        assertEquals(participant, studyParticipantAssignment.getParticipant());
-//
-//        studyParticipantAssignment = studyParticipantAssignmentRepository.findById(assignment.getId());
-//        assertEquals(studyParticipantAssignment, assignment);
-
-
+    public void testGetClinicalStaff(){
+    	Study study = StudyTestHelper.getDefaultStudy();
+    	StudyParticipantAssignment spa = study.getArms().get(0).getStudyParticipantAssignments().get(0);
+    	StudyOrganization studyOrganization = spa.getStudySite();
+    	StudyOrganizationClinicalStaffQuery query = new StudyOrganizationClinicalStaffQuery();
+    	query.filterByStudyOrganization(studyOrganization.getId());
+    	
+    	List<StudyOrganizationClinicalStaff> socsList = genericRepository.find(query);
+    	List<StudyOrganizationClinicalStaff> socsTreatinPhyList = new ArrayList<StudyOrganizationClinicalStaff>();
+    	List<StudyOrganizationClinicalStaff> socsResearchNursesList = new ArrayList<StudyOrganizationClinicalStaff>();
+    	List<StudyOrganizationClinicalStaff> socsSitePIList = new ArrayList<StudyOrganizationClinicalStaff>();
+    	List<StudyOrganizationClinicalStaff> socsSiteCRAList = new ArrayList<StudyOrganizationClinicalStaff>();
+    	
+    	for(StudyOrganizationClinicalStaff socs : socsList){
+    		if(socs.getRole().equals(Role.TREATING_PHYSICIAN)){
+    			socsTreatinPhyList.add(socs);
+    		}
+    		if(socs.getRole().equals(Role.NURSE)){
+    			socsResearchNursesList.add(socs);
+    		}
+    		if(socs.getRole().equals(Role.SITE_PI)){
+    			socsSitePIList.add(socs);
+    		}
+    		if(socs.getRole().equals(Role.SITE_CRA)){
+    			socsSiteCRAList.add(socs);
+    		}
+    	}
+    	
+    	List<StudyParticipantClinicalStaff> spaTreatinPhyList = spa.getTreatingPhysicians();
+    	for(StudyParticipantClinicalStaff spcs : spaTreatinPhyList){
+    		assertTrue(socsTreatinPhyList.contains(spcs.getStudyOrganizationClinicalStaff()));
+    	}
+    	
+    	List<StudyParticipantClinicalStaff> spaResearchNurseList = spa.getResearchNurses();
+    	for(StudyParticipantClinicalStaff spcs : spaResearchNurseList){
+    		assertTrue(socsResearchNursesList.contains(spcs.getStudyOrganizationClinicalStaff()));
+    	}
+    	
+    	List<StudyParticipantClinicalStaff> spaSitePIList = spa.getSitePIs();
+    	for(StudyParticipantClinicalStaff spcs : spaSitePIList){
+    		assertTrue(socsSitePIList.contains(spcs.getStudyOrganizationClinicalStaff()));
+    	}
+    	
+    	List<StudyParticipantClinicalStaff> spaSiteCRAList = spa.getSiteCRAs();
+    	for(StudyParticipantClinicalStaff spcs : spaSiteCRAList){
+    		assertTrue(socsSiteCRAList.contains(spcs.getStudyOrganizationClinicalStaff()));
+    	}
+    	
     }
 
 
--- pro-ctcae/software/web/src/test/java/gov/nih/nci/ctcae/web/participant/ParticipantCommonTest.java
+++ pro-ctcae/software/web/src/test/java/gov/nih/nci/ctcae/web/participant/ParticipantCommonTest.java
@@ -1,16 +1,21 @@
 package gov.nih.nci.ctcae.web.participant;
 
+import gov.nih.nci.ctcae.commons.utils.DateUtils;
 import gov.nih.nci.ctcae.core.domain.*;
 import gov.nih.nci.ctcae.core.helper.ParticipantTestHelper;
+import gov.nih.nci.ctcae.core.helper.StudyTestHelper;
 import gov.nih.nci.ctcae.web.AbstractWebTestCase;
+
 import org.springframework.web.servlet.ModelAndView;
 
 import java.text.ParseException;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
+import java.util.List;
 import java.util.Map;
-
+import static org.easymock.EasyMock.expect;
 /**
  * @author Harsh Agarwal
  *         Date: June 10, 2009
@@ -61,4 +66,54 @@ public class ParticipantCommonTest extends AbstractWebTestCase {
     }
 
 
+    public void testSetParticipantModesAndReminders(){
+    	ParticipantCommand participantCommand = new ParticipantCommand();
+    	StudySite studySite = new StudySite();
+    	studySite.setId(11);
+    	IvrsSchedule ivrsSchedule = new IvrsSchedule();
+    	Date today = new Date();
+    	ivrsSchedule.setPreferredCallTime(today);
+    	ivrsSchedule.setCallStatus(IvrsCallStatus.PENDING);
+    	StudyParticipantCrf spcrf = new StudyParticipantCrf();
+    	StudyParticipantCrfSchedule spcrfs = new StudyParticipantCrfSchedule();
+    	List<IvrsSchedule> ivrsScheduleList = new ArrayList<IvrsSchedule>();
+    	ivrsScheduleList.add(ivrsSchedule);
+    	spcrfs.setIvrsSchedules(ivrsScheduleList);
+    	spcrfs.setStudyParticipantCrf(spcrf);
+    	spcrf.addStudyParticipantCrfSchedule(spcrfs);
+    	StudyParticipantAssignment spa = new StudyParticipantAssignment();
+    	spa.addStudyParticipantCrf(spcrf);
+    	participantCommand.setResponseModes(new String[]{"IVRS"});
+    	
+    	request.getSession().setAttribute("email_" + studySite.getId(), false);
+    	request.getSession().setAttribute("call_" + studySite.getId(), false);
+    	request.getSession().setAttribute("text_" + studySite.getId(), false);
+    	request.getSession().setAttribute("call_hour_" + studySite.getId(), 1);
+    	request.getSession().setAttribute("call_hour_" + studySite.getId(), 1);
+    	request.setParameter("call_ampm_" + studySite.getId(), "am");
+    	request.setParameter("call_timeZone_" + studySite.getId(), "America/Chicago");
+    	request.setParameter("home_paper_lang_" + studySite.getId(), "ENGLISH");
+    	request.setParameter("home_web_lang_" + studySite.getId(), "ENGLISH");
+    	request.setParameter("ivrs_lang_" + studySite.getId(), "ENGLISH");
+    	request.setParameter("clinic_paper_lang_" + studySite.getId(), "ENGLISH");
+    	request.setParameter("clinic_web_lang_" + studySite.getId(), "ENGLISH");
+
+    	participantCommand.setParticipantModesAndReminders(studySite, spa, request);
+    	assertEquals(spa.getCallHour(), Integer.valueOf(1));
+    	assertEquals(spa.getCallAmPm(), "am");
+    	assertNotSame(spa.getCallMinute(), null);
+    	assertEquals(spa.getCallTimeZone(), "America/Chicago" );
+    	assertEquals(spa.getHomePaperLanguage(), "ENGLISH");
+    	assertEquals(spa.getHomeWebLanguage(), "ENGLISH");
+    	assertEquals(spa.getIvrsLanguage(), "ENGLISH");
+    	assertEquals(spa.getClinicPaperLanguage(), "ENGLISH");
+    	assertEquals(spa.getClinicWebLanguage(), "ENGLISH");
+    	
+    	request.setParameter("call_minute_" + studySite.getId(), "44");
+    	participantCommand.setParticipantModesAndReminders(studySite, spa, request);
+    	today.setHours(2);
+    	today.setMinutes(5);
+    	today.setSeconds(0);
+    	assertEquals(spa.getIvrsScheduleList().get(0).getPreferredCallTime().getMinutes(), 44);
+    }
 }
