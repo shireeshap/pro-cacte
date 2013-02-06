79629b3b8a6f8864bf729f12ffe80e30bc7d296b
gangoliv <gangoliv@ncias-d1508-v.nci.nih.gov>
2013-02-06 14:32:31 +0000

    PRKC-2004: refactored the loadUser method.

--- pro-ctcae/software/core/src/main/java/gov/nih/nci/ctcae/core/repository/UserRepository.java
+++ pro-ctcae/software/core/src/main/java/gov/nih/nci/ctcae/core/repository/UserRepository.java
@@ -1,8 +1,19 @@
 package gov.nih.nci.ctcae.core.repository;
 
 import gov.nih.nci.cabig.ctms.audit.domain.DataAuditInfo;
-import gov.nih.nci.cabig.ctms.tools.configuration.ConfigurationProperty.Int;
-import gov.nih.nci.ctcae.core.domain.*;
+import gov.nih.nci.ctcae.core.domain.ClinicalStaff;
+import gov.nih.nci.ctcae.core.domain.Organization;
+import gov.nih.nci.ctcae.core.domain.OrganizationClinicalStaff;
+import gov.nih.nci.ctcae.core.domain.Participant;
+import gov.nih.nci.ctcae.core.domain.Privilege;
+import gov.nih.nci.ctcae.core.domain.QueryStrings;
+import gov.nih.nci.ctcae.core.domain.Role;
+import gov.nih.nci.ctcae.core.domain.Study;
+import gov.nih.nci.ctcae.core.domain.StudyOrganization;
+import gov.nih.nci.ctcae.core.domain.StudyOrganizationClinicalStaff;
+import gov.nih.nci.ctcae.core.domain.User;
+import gov.nih.nci.ctcae.core.domain.UserNotification;
+import gov.nih.nci.ctcae.core.domain.UserRole;
 import gov.nih.nci.ctcae.core.domain.security.passwordpolicy.PasswordPolicy;
 import gov.nih.nci.ctcae.core.exception.CtcAeSystemException;
 import gov.nih.nci.ctcae.core.exception.UsernameAlreadyExistsException;
@@ -16,19 +27,15 @@ import gov.nih.nci.ctcae.core.query.StudyQuery;
 import gov.nih.nci.ctcae.core.query.UserQuery;
 import gov.nih.nci.ctcae.core.security.DomainObjectPrivilegeGenerator;
 import gov.nih.nci.ctcae.core.security.passwordpolicy.PasswordPolicyService;
-import gov.nih.nci.ctcae.core.domain.QueryStrings;
 
 import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Date;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Properties;
 import java.util.Set;
-
-import javax.swing.text.html.HTMLDocument.HTMLReader.IsindexAction;
 
 import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
@@ -44,12 +51,11 @@ import org.springframework.security.userdetails.UsernameNotFoundException;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
-//
 
 /**
  * The Class CRFRepository.
  *
- * @author Harsh Agarwal
+ * @author Vinay Gangoli
  * @created Oct 14, 2008
  */
 @Transactional(propagation = Propagation.REQUIRED, readOnly = false)
@@ -66,34 +72,85 @@ public class UserRepository implements UserDetailsService, Repository<User, User
     public User loadUserByUsername(String userName) throws UsernameNotFoundException, DataAccessException {
 
         DomainObjectPrivilegeGenerator privilegeGenerator = new DomainObjectPrivilegeGenerator();
-        List<User> users = findByUserName(userName);
-
-        if (users.isEmpty() || users.size() > 1) {
-            String errorMessage = String.format("Could not find user %s", userName);
-            throw new UsernameNotFoundException(errorMessage);
-        }
-        User user = users.get(0);
         Set<Role> roles = new HashSet<Role>();
         List<GrantedAuthority> grantedAuthorities = new ArrayList<GrantedAuthority>();
-
         List<GrantedAuthority> instanceGrantedAuthorities = new ArrayList<GrantedAuthority>();
 
-        //only adding the roles which are active on atleast one study
-        boolean isAdminOrCCAOrParticipant = user.hasRole(Role.ADMIN, Role.CCA, Role.PARTICIPANT);  
-        for (UserRole userRole : user.getUserRoles()) {
-        	//for all roles (except ADMIN and CCA and Participant) check if user is Active on atleast one of the assigned studies.
-        	if(!isAdminOrCCAOrParticipant){
-	            if(isRoleActive(user.getId(), userRole.getRole()))
-	            	roles.add(userRole.getRole());
-        	} else {
-        		roles.add(userRole.getRole());
-        	}
-        }
+        User user = getUserByUserName(userName);
+        addRolesToUser(user, roles);
 
         if (ClinicalStaffQuery.class.isAssignableFrom(StudyOrganizationClinicalStaffQuery.class)) {
             throw new CtcAeSystemException("query used to retrieve user must not be secured query. ");
         }
 
+        ClinicalStaff clinicalStaff = generateInstanceGrantedAuthoritiesForNonAdminUsers(user, privilegeGenerator, roles, instanceGrantedAuthorities);
+        Participant participant = generateInstanceGrantedAuthoritiesForParticipants(user, privilegeGenerator, instanceGrantedAuthorities);
+        generateGrantedAuthoritiesForRole(user, roles, grantedAuthorities);
+        
+        checkPasswordExpirationAndLock(user, roles);
+        
+        grantedAuthorities.addAll(instanceGrantedAuthorities);
+        user.setGrantedAuthorities(grantedAuthorities.toArray(new GrantedAuthority[]{}));
+        
+        for(Role role: roles){
+        	user.addUserRole(new UserRole(role));
+        }        
+        if (clinicalStaff == null && participant == null && !user.isAdmin()) {
+            throw new MyException("User is inactive");
+        }
+        //return user;
+        return findByUserName(user.getUsername()).get(0);
+    }
+
+    private void checkPasswordExpirationAndLock(User user, Set<Role> roles) {
+    	PasswordPolicy passwordPolicy;
+        if (!roles.isEmpty()) {
+            passwordPolicy = passwordPolicyService.getPasswordPolicy(roles.iterator().next());
+        } else {
+            passwordPolicy = passwordPolicyService.getPasswordPolicy(user.getRoleForPasswordPolicy());
+        }
+        
+        long passwordAge = user.getPasswordAge();
+        if (passwordAge > passwordPolicy.getLoginPolicy().getMaxPasswordAge()) {
+            throw new MyException("Password expired");
+        }
+        checkAccountLock(user, passwordPolicy);		
+	}
+
+	private List<GrantedAuthority> generateGrantedAuthoritiesForRole(User user, Set<Role> roles, List<GrantedAuthority> grantedAuthorities) {
+    	if (roles.isEmpty()) {
+    		return grantedAuthorities;
+    	}
+        RolePrivilegeQuery rolePrivilegeQuery = new RolePrivilegeQuery();
+        if (RolePrivilegeQuery.class.isAssignableFrom(StudyOrganizationClinicalStaffQuery.class)) {
+            throw new CtcAeSystemException("query used to retrieve user must not be secured query. ");
+        }
+        if (!user.isAdmin()) {
+            rolePrivilegeQuery.filterByRoles(roles);
+        } else {
+            rolePrivilegeQuery.filterForAdmin();
+        }
+        List<Privilege> privileges = genericRepository.find(rolePrivilegeQuery);
+        for (Privilege privilege : privileges) {
+            GrantedAuthority grantedAuthority = new GrantedAuthorityImpl(privilege.getPrivilegeName());
+            grantedAuthorities.add(grantedAuthority);
+        }
+        return grantedAuthorities;
+	}
+
+	private Participant generateInstanceGrantedAuthoritiesForParticipants(User user, DomainObjectPrivilegeGenerator privilegeGenerator, List<GrantedAuthority> instanceGrantedAuthorities) {
+        //check for participant
+        Participant participant = findParticipantForUser(user);
+        if (participant != null) {
+            Set<String> privileges = privilegeGenerator.generatePrivilege(participant);
+            for (String privilege : privileges) {
+                instanceGrantedAuthorities.add(new GrantedAuthorityImpl(privilege));
+            }
+        }
+        return participant;
+	}
+
+	private ClinicalStaff generateInstanceGrantedAuthoritiesForNonAdminUsers(User user, DomainObjectPrivilegeGenerator privilegeGenerator, Set<Role> roles, List<GrantedAuthority> instanceGrantedAuthorities) {
         ClinicalStaff clinicalStaff = null;
         //only need to load orgs and studies for non admin staff
         if (!roles.contains(Role.ADMIN)) {
@@ -146,60 +203,34 @@ public class UserRepository implements UserDetailsService, Repository<User, User
                     }
                 }
             }
+        }	
+        return clinicalStaff;
+	}
+
+	private void addRolesToUser(User user, Set<Role> roles) {
+        boolean isAdminOrCCAOrParticipant = user.hasRole(Role.ADMIN, Role.CCA, Role.PARTICIPANT);  
+        for (UserRole userRole : user.getUserRoles()) {
+        	//for all roles (except ADMIN and CCA and Participant) check if user is Active on atleast one of the assigned studies.
+        	// then only add the roles which are active on atleast one study
+        	if(!isAdminOrCCAOrParticipant){
+	            if(isRoleActive(user.getId(), userRole.getRole()))
+	            	roles.add(userRole.getRole());
+        	} else {
+        		roles.add(userRole.getRole());
+        	}
+        }		
+	}
+
+	private User getUserByUserName(String userName) {
+        List<User> users = findByUserName(userName);
+        if (users.isEmpty() || users.size() > 1) {
+            String errorMessage = String.format("Could not find user %s", userName);
+            throw new UsernameNotFoundException(errorMessage);
         }
+        return users.get(0);
+	}
 
-        //check for participant
-        Participant participant = findParticipantForUser(user);
-        if (participant != null) {
-            Set<String> privileges = privilegeGenerator.generatePrivilege(participant);
-            for (String privilege : privileges) {
-                instanceGrantedAuthorities.add(new GrantedAuthorityImpl(privilege));
-            }
-        }
-
-        PasswordPolicy passwordPolicy;
-        if (!roles.isEmpty()) {
-            passwordPolicy = passwordPolicyService.getPasswordPolicy(roles.iterator().next());
-
-            RolePrivilegeQuery rolePrivilegeQuery = new RolePrivilegeQuery();
-            if (RolePrivilegeQuery.class.isAssignableFrom(StudyOrganizationClinicalStaffQuery.class)) {
-                throw new CtcAeSystemException("query used to retrieve user must not be secured query. ");
-            }
-            if (!user.isAdmin()) {
-                rolePrivilegeQuery.filterByRoles(roles);
-            } else {
-                rolePrivilegeQuery.filterForAdmin();
-            }
-            List<Privilege> privileges = genericRepository.find(rolePrivilegeQuery);
-
-            for (Privilege privilege : privileges) {
-                GrantedAuthority grantedAuthority = new GrantedAuthorityImpl(privilege.getPrivilegeName());
-                grantedAuthorities.add(grantedAuthority);
-            }
-        } else {
-            passwordPolicy = passwordPolicyService.getPasswordPolicy(user.getRoleForPasswordPolicy());
-        }
-        long passwordAge = user.getPasswordAge();
-        if (passwordAge > passwordPolicy.getLoginPolicy().getMaxPasswordAge()) {
-            throw new MyException("Password expired");
-        }
-        checkAccountLock(user, passwordPolicy);
-
-        grantedAuthorities.addAll(instanceGrantedAuthorities);
-        user.setGrantedAuthorities(grantedAuthorities.toArray(new GrantedAuthority[]{}));
-        Iterator<Role> iter = roles.iterator();
-        while(iter.hasNext()){
-        	user.addUserRole(new UserRole(iter.next()));
-        }
-        
-        if (clinicalStaff == null && participant == null && !user.isAdmin()) {
-            throw new MyException("User is inactive");
-        }
-
-        return findByUserName(user.getUsername()).get(0);
-    }
-
-    public List<User> findByUserName(String userName) {
+	public List<User> findByUserName(String userName) {
         UserQuery userQuery = new UserQuery();
         userQuery.filterByUserName(userName);
         List<User> users = new ArrayList<User>(find(userQuery));
