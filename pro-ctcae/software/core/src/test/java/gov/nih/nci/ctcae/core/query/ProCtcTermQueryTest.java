5689414046ac25de7c02a9128bbc835657f2a591
sanea <sanea@ncias-d1508-v.nci.nih.gov>
2013-02-18 19:36:20 +0000

    PRKC-1990: Fix failing testcases for ProCtcTermQueryTest.java (found failing in jenkins output log)

--- pro-ctcae/software/core/src/test/java/gov/nih/nci/ctcae/core/query/ProCtcTermQueryTest.java
+++ pro-ctcae/software/core/src/test/java/gov/nih/nci/ctcae/core/query/ProCtcTermQueryTest.java
@@ -11,29 +11,29 @@ public class ProCtcTermQueryTest extends TestCase {
     public void testFilterByCtcTermHavingQuestionsOnly() {
         ProCtcTermQuery query = new ProCtcTermQuery();
         query.filterByCtcTermHavingQuestionsOnly();
-        assertEquals("SELECT distinct(o) from ProCtcTerm o left join o.proCtcQuestions as proCtcQuestion left join proCtcQuestion.validValues inner join o.proCtcQuestions WHERE " +
-        		"o.ctcTerm.category.ctc.name = :ctcName", query.getQueryString());
+        assertEquals("SELECT distinct(o) from ProCtcTerm o left join o.proCtcQuestions as proCtcQuestion left join proCtcQuestion.validValues left join o.ctcTerm.categoryTermSets as categoryTerm inner join o.proCtcQuestions WHERE " +
+        		"categoryTerm.category.ctc.name = :ctcName", query.getQueryString());
     }
 
     public void testFilterByCtcCategoryId() {
         ProCtcTermQuery query = new ProCtcTermQuery();
         query.filterByCtcCategoryId(1);
-        assertEquals("SELECT distinct(o) from ProCtcTerm o left join o.proCtcQuestions as proCtcQuestion left join proCtcQuestion.validValues WHERE " +
-        		"o.ctcTerm.category.id = :ctcCategoryId AND o.ctcTerm.category.ctc.name = :ctcName", query.getQueryString());
+		assertEquals("SELECT distinct(o) from ProCtcTerm o left join o.proCtcQuestions as proCtcQuestion left join proCtcQuestion.validValues left join o.ctcTerm.categoryTermSets as categoryTerm WHERE " +
+				"categoryTerm.category.ctc.name = :ctcName AND categoryTerm.category.id = :ctcCategoryId",query.getQueryString());
     }
 
     public void testFilterByCtcTermId() {
         ProCtcTermQuery query = new ProCtcTermQuery();
         query.filterByCtcTermId(1);
-        assertEquals("SELECT distinct(o) from ProCtcTerm o left join o.proCtcQuestions as proCtcQuestion left join proCtcQuestion.validValues " +
-                "WHERE o.ctcTerm.id = :ctcTermId AND o.ctcTerm.category.ctc.name = :ctcName", query.getQueryString());
+        assertEquals("SELECT distinct(o) from ProCtcTerm o left join o.proCtcQuestions as proCtcQuestion left join proCtcQuestion.validValues left join o.ctcTerm.categoryTermSets as categoryTerm WHERE" +
+        		" o.ctcTerm.id = :ctcTermId AND categoryTerm.category.ctc.name = :ctcName", query.getQueryString());
     }
 
     public void testFilterByTerm() {
         ProCtcTermQuery query = new ProCtcTermQuery();
         query.filterByTerm("test");
-        assertEquals("SELECT distinct(o) from ProCtcTerm o left join o.proCtcQuestions as proCtcQuestion left join proCtcQuestion.validValues WHERE " +
-        		"o.ctcTerm.category.ctc.name = :ctcName AND lower(o.proCtcTermVocab.termEnglish) = :symptom", query.getQueryString());
+        assertEquals("SELECT distinct(o) from ProCtcTerm o left join o.proCtcQuestions as proCtcQuestion left join proCtcQuestion.validValues left join o.ctcTerm.categoryTermSets as categoryTerm WHERE " +
+        		"categoryTerm.category.ctc.name = :ctcName AND lower(o.proCtcTermVocab.termEnglish) = :symptom", query.getQueryString());
 
     }
 
