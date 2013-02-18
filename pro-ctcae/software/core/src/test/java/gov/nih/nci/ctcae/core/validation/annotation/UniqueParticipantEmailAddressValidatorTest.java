0ae774ef08b5caa836e56308c4b24426a9b27b11
sanea <sanea@ncias-d1508-v.nci.nih.gov>
2013-02-18 19:40:28 +0000

    PRKC-1990: Fixing failing testcase for UniqueParticipantEmailAddressValidatorTest.java (found failing in jenkins output log)

--- pro-ctcae/software/core/src/test/java/gov/nih/nci/ctcae/core/validation/annotation/UniqueParticipantEmailAddressValidatorTest.java
+++ pro-ctcae/software/core/src/test/java/gov/nih/nci/ctcae/core/validation/annotation/UniqueParticipantEmailAddressValidatorTest.java
@@ -1,15 +1,15 @@
 package gov.nih.nci.ctcae.core.validation.annotation;
 
+import static org.easymock.EasyMock.expect;
+import static org.easymock.EasyMock.isA;
 import gov.nih.nci.ctcae.core.AbstractTestCase;
 import gov.nih.nci.ctcae.core.domain.Participant;
 import gov.nih.nci.ctcae.core.query.ParticipantQuery;
+import gov.nih.nci.ctcae.core.repository.GenericRepository;
 import gov.nih.nci.ctcae.core.repository.secured.ParticipantRepository;
 
 import java.util.ArrayList;
 import java.util.Collection;
-
-import static org.easymock.EasyMock.expect;
-import static org.easymock.EasyMock.isA;
 
 /**
  * Created by IntelliJ IDEA.
@@ -23,28 +23,31 @@ public class UniqueParticipantEmailAddressValidatorTest extends AbstractTestCase
     private ParticipantRepository participantRepository;
     private Participant participant;
     Collection<Participant> participants;
+    private GenericRepository genericRepository;
 
     public void UniqueParticipantEmailAddressValidatorTest(){
-
     }
 
     public void setUp() throws Exception{
         super.setUp();
-        participantRepository = registerMockFor(ParticipantRepository.class);
+        genericRepository = registerMockFor(GenericRepository.class);
         validator = new UniqueParticipantEmailAddressValidator();
-        validator.setParticipantRepository(participantRepository);
+        validator.setGenericRepository(genericRepository);
         participant = new Participant();
         participants = new ArrayList<Participant>();
         participant.setEmailAddress("reshma.koganti@gmail.com");
+         
     }
+    
 
     public void testValidateEmail(){
         String emailAddress ="reshma.koganti@gmail.com";
-        Integer participantId=null;
-        expect(participantRepository.find(isA(ParticipantQuery.class))).andReturn(participants).anyTimes();
+        Integer participantId=999;
+        expect(genericRepository.find(isA(ParticipantQuery.class))).andStubReturn(participants);
         participants.add(participant);
         replayMocks();
         assertTrue("Email Address already exists in database", validator.validateEmail(emailAddress,participantId));
         verifyMocks();
     }
+ 
 }
