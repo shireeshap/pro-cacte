ad6007ee86db4d93f40227b752cab4d55d5e795e
sanea <sanea@ncias-d1508-v.nci.nih.gov>
2013-03-07 21:19:03 +0000

    PRKC-2031: Update status of survey when moved prior to onHoldDate

--- pro-ctcae/software/core/src/main/java/gov/nih/nci/ctcae/core/service/ParticipantScheduleService.java
+++ pro-ctcae/software/core/src/main/java/gov/nih/nci/ctcae/core/service/ParticipantScheduleService.java
@@ -159,12 +159,14 @@ public StudyParticipantCrfSchedule updateSchedule(Calendar oldCalendar, Calendar
 	                if (schToUpdate.getStatus().equals(CrfStatus.PASTDUE)) {
 	                    schToUpdate.setStatus(CrfStatus.SCHEDULED);
 	                }
-	            }
 	            
-	            //if a survey is moved during on-hold period to a date later or equal to on-hold date, then set its status to 'ONHOLD'
-	            if(spa.getOnHoldTreatmentDate() != null && (DateUtils.compareDate(schToUpdate.getStartDate(),spa.getOnHoldTreatmentDate()) >= 0)){
-	            	schToUpdate.setStatus(CrfStatus.ONHOLD);
-	            }
+		            //if a survey is moved during on-hold period to a date later or equal to on-hold date, then set its status to 'ONHOLD'
+		            if(spa.getOnHoldTreatmentDate() != null && (DateUtils.compareDate(schToUpdate.getStartDate(),spa.getOnHoldTreatmentDate()) >= 0)){
+		            	schToUpdate.setStatus(CrfStatus.ONHOLD);
+		            }else if(spa.getOnHoldTreatmentDate() != null && (DateUtils.compareDate(schToUpdate.getStartDate(),spa.getOnHoldTreatmentDate()) < 0)){
+		            	schToUpdate.setStatus(CrfStatus.SCHEDULED);
+		            }
+	            } 
 	            
 	            updatedForms.add(studyParticipantCrf.getCrf().getTitle());
 	            return schToUpdate;
