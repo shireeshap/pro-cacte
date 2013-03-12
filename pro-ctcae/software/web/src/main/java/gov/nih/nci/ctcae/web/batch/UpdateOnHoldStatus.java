ef07d7f71d6ddab70376f814d5d236e3df8ae0b3
sanea <sanea@ncias-d1508-v.nci.nih.gov>
2013-03-12 17:43:21 +0000

    PRKC-1867: code modification to nightly trigger UpdateOnHoldStatus.java

--- pro-ctcae/software/web/src/main/java/gov/nih/nci/ctcae/web/batch/UpdateOnHoldStatus.java
+++ pro-ctcae/software/web/src/main/java/gov/nih/nci/ctcae/web/batch/UpdateOnHoldStatus.java
@@ -1,11 +1,11 @@
 package gov.nih.nci.ctcae.web.batch;
 
 import gov.nih.nci.cabig.ctms.audit.domain.DataAuditInfo;
-import gov.nih.nci.ctcae.core.domain.Participant;
 import gov.nih.nci.ctcae.core.domain.RoleStatus;
 import gov.nih.nci.ctcae.core.domain.Study;
 import gov.nih.nci.ctcae.core.domain.StudyParticipantAssignment;
 import gov.nih.nci.ctcae.core.domain.StudySite;
+import gov.nih.nci.ctcae.core.query.StudyParticipantAssignmentQuery;
 import gov.nih.nci.ctcae.core.repository.GenericRepository;
 
 import java.util.Date;
@@ -17,42 +17,44 @@ import org.hibernate.Transaction;
 import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
 import org.springframework.transaction.annotation.Transactional;
 
+/**
+ * @author AmeyS
+ * 
+ */
 public class UpdateOnHoldStatus extends HibernateDaoSupport{
 	
 	public GenericRepository genericRepository;
-	
-	 @Transactional
-	    public void updateStatus(){
-		 
-		 DataAuditInfo auditInfo = new DataAuditInfo("admin", "localhost", new Date(), "127.0.0.0");
-	     DataAuditInfo.setLocal(auditInfo);
-	     Session session = getHibernateTemplate().getSessionFactory().openSession();
-	     Transaction tx = session.beginTransaction();
-	     tx.begin();
-	     logger.debug("Nightly trigger bean job starts....");
-		
-	     Query query = session.createQuery(new String("Select study from Study study"));
-	     List<Study> studies = query.list();
-	     Date today = new Date();
-	     
-	     for (Study study : studies) {
-	    	 for (StudySite studySite : study.getStudySites()) {
-	    		   for (StudyParticipantAssignment studyParticipantAssignment : studySite.getStudyParticipantAssignments()) {
-					if (studyParticipantAssignment.getStatus() != null && studyParticipantAssignment.getStatus().equals(RoleStatus.ACTIVE))
-							if(studyParticipantAssignment.getOnHoldTreatmentDate() != null){
-								if(today.equals(studyParticipantAssignment.getOnHoldTreatmentDate()) || today.after(studyParticipantAssignment.getOnHoldTreatmentDate())){
-								  studyParticipantAssignment.setStatus(RoleStatus.ONHOLD);
-								  genericRepository.save(studyParticipantAssignment);
-								}
-							
-	    			   }
-	    		   }
-	    	 }
-	     }
-	     
-	    }
+	private static final String STATUS_ACTIVE = "ACTIVE";
 	 
-	 public void setGenericRepository(GenericRepository genericRepository){
-		 this.genericRepository = genericRepository;
+	@SuppressWarnings("deprecation")
+	@Transactional
+    public void updateStatus(){
+	 DataAuditInfo auditInfo = new DataAuditInfo("admin", "localhost", new Date(), "127.0.0.0");
+     DataAuditInfo.setLocal(auditInfo);
+     Session session = getHibernateTemplate().getSessionFactory().openSession();
+     Transaction tx = session.beginTransaction();
+     tx.begin();
+     logger.debug("UpdateOnHoldStatus: Nightly trigger bean job starts....");
+	
+     StudyParticipantAssignmentQuery query = new StudyParticipantAssignmentQuery();
+     query.filterByStatus(STATUS_ACTIVE);
+     List<StudyParticipantAssignment> studyParticipantAssignmentsList = genericRepository.find(query);
+     Date today = new Date();
+	 for (StudyParticipantAssignment studyParticipantAssignment : studyParticipantAssignmentsList) {
+		 if (studyParticipantAssignment.getStatus() != null && studyParticipantAssignment.getStatus().equals(RoleStatus.ACTIVE)){
+			 if (studyParticipantAssignment.getOnHoldTreatmentDate() != null){
+				if (today.equals(studyParticipantAssignment.getOnHoldTreatmentDate())
+						|| today.after(studyParticipantAssignment.getOnHoldTreatmentDate())) {
+				  studyParticipantAssignment.setStatus(RoleStatus.ONHOLD);
+				  genericRepository.save(studyParticipantAssignment);
+				}
+		     }
+		 }
 	 }
+     logger.debug("UpdateOnHoldStatus: Nightly trigger bean job ends....");
+    }
+	 
+	public void setGenericRepository(GenericRepository genericRepository){
+		this.genericRepository = genericRepository;
+	}
 }
