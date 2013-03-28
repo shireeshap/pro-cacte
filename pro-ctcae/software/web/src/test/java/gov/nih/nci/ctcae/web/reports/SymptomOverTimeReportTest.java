96264c8745cb5559bcf57bf5893f5df81f65d440
gangoliv <gangoliv@ncias-d1508-v.nci.nih.gov>
2013-03-28 20:22:21 +0000

    PRKC-2036: Updated test cases to not depend on specific crfPageItem.

--- pro-ctcae/software/web/src/test/java/gov/nih/nci/ctcae/web/reports/SymptomOverTimeReportTest.java
+++ pro-ctcae/software/web/src/test/java/gov/nih/nci/ctcae/web/reports/SymptomOverTimeReportTest.java
@@ -22,20 +22,25 @@ public class SymptomOverTimeReportTest extends AbstractWebTestCase {
 
         Study study = StudyTestHelper.getDefaultStudy();
         CRF crf = study.getCrfs().get(0);
-        Integer symptomId = crf.getAllCrfPageItems().get(8).getProCtcQuestion().getProCtcTerm().getId();
+        Integer symptomId;
+        int size = crf.getAllCrfPageItems().size();
+        if(size > 0){
+        	symptomId = crf.getAllCrfPageItems().get(size - 1).getProCtcQuestion().getProCtcTerm().getId();
+            SymptomOverTimeReportResultsController controller = new SymptomOverTimeReportResultsController();
+            controller.setGenericRepository(genericRepository);
+            request.setParameter("crf", crf.getId().toString());
+            request.setParameter("symptom", symptomId.toString());
+            request.setParameter("attributes", "_Severity_Frequency");
+            request.setParameter("group", "cycle");
+            request.setMethod("GET");
 
-        SymptomOverTimeReportResultsController controller = new SymptomOverTimeReportResultsController();
-        controller.setGenericRepository(genericRepository);
-        request.setParameter("crf", crf.getId().toString());
-        request.setParameter("symptom", symptomId.toString());
-        request.setParameter("attributes", "_Severity_Frequency");
-        request.setParameter("group", "cycle");
-        request.setMethod("GET");
-
-        ModelAndView modelAndView = controller.handleRequest(request, response);
-        Map m = modelAndView.getModel();
-        ArrayList<Object[]> charts = (ArrayList<Object[]>) m.get("results");
-        GraphicalReportTestHelper.showCharts(charts);
+            ModelAndView modelAndView = controller.handleRequest(request, response);
+            Map m = modelAndView.getModel();
+            ArrayList<Object[]> charts = (ArrayList<Object[]>) m.get("results");
+            GraphicalReportTestHelper.showCharts(charts);
+        } else {
+        	fail("Insufficient data in DB while running this test");
+        }
     }
 
     public void testReportControllerMultipleArms() throws Exception {
@@ -46,21 +51,27 @@ public class SymptomOverTimeReportTest extends AbstractWebTestCase {
             arms += arm.getId() + "_";
         }
         CRF crf = study.getCrfs().get(0);
-        Integer symptomId = crf.getAllCrfPageItems().get(8).getProCtcQuestion().getProCtcTerm().getId();
-
-        SymptomOverTimeReportResultsController controller = new SymptomOverTimeReportResultsController();
-        controller.setGenericRepository(genericRepository);
-        request.setParameter("crf", crf.getId().toString());
-        request.setParameter("symptom", symptomId.toString());
-        request.setParameter("attributes", "_Severity_Frequency");
-        request.setParameter("group", "cycle");
-        request.setParameter("arms", arms);
-        request.setMethod("GET");
-
-        ModelAndView modelAndView = controller.handleRequest(request, response);
-        Map m = modelAndView.getModel();
-        ArrayList<Object[]> charts = (ArrayList<Object[]>) m.get("results");
-        GraphicalReportTestHelper.showCharts(charts);
+        Integer symptomId;
+        int size = crf.getAllCrfPageItems().size();
+        if(size > 0){
+        	symptomId = crf.getAllCrfPageItems().get(size - 1).getProCtcQuestion().getProCtcTerm().getId();
+	
+	        SymptomOverTimeReportResultsController controller = new SymptomOverTimeReportResultsController();
+	        controller.setGenericRepository(genericRepository);
+	        request.setParameter("crf", crf.getId().toString());
+	        request.setParameter("symptom", symptomId.toString());
+	        request.setParameter("attributes", "_Severity_Frequency");
+	        request.setParameter("group", "cycle");
+	        request.setParameter("arms", arms);
+	        request.setMethod("GET");
+	
+	        ModelAndView modelAndView = controller.handleRequest(request, response);
+	        Map m = modelAndView.getModel();
+	        ArrayList<Object[]> charts = (ArrayList<Object[]>) m.get("results");
+	        GraphicalReportTestHelper.showCharts(charts);
+        } else {
+        	fail("Insufficient data in DB while running this test");
+        }
     }
 
     public void testReportControllerMultipleArmsLineChart() throws Exception {
@@ -71,22 +82,27 @@ public class SymptomOverTimeReportTest extends AbstractWebTestCase {
             arms += arm.getId() + "_";
         }
         CRF crf = study.getCrfs().get(0);
-        Integer symptomId = crf.getAllCrfPageItems().get(8).getProCtcQuestion().getProCtcTerm().getId();
-
-        SymptomOverTimeReportResultsController controller = new SymptomOverTimeReportResultsController();
-        controller.setGenericRepository(genericRepository);
-        request.setParameter("crf", crf.getId().toString());
-        request.setParameter("symptom", symptomId.toString());
-        request.setParameter("attributes", "_Severity_Frequency");
-        request.setParameter("group", "cycle");
-        request.setParameter("arms", arms);
-        request.setParameter("chartType", "line");
-        request.setMethod("GET");
-
-        ModelAndView modelAndView = controller.handleRequest(request, response);
-        Map m = modelAndView.getModel();
-        ArrayList<Object[]> charts = (ArrayList<Object[]>) m.get("results");
-        GraphicalReportTestHelper.showCharts(charts);
+        Integer symptomId;
+        int size = crf.getAllCrfPageItems().size();
+        if(size > 0){
+        	symptomId = crf.getAllCrfPageItems().get(size - 1).getProCtcQuestion().getProCtcTerm().getId();
+	        SymptomOverTimeReportResultsController controller = new SymptomOverTimeReportResultsController();
+	        controller.setGenericRepository(genericRepository);
+	        request.setParameter("crf", crf.getId().toString());
+	        request.setParameter("symptom", symptomId.toString());
+	        request.setParameter("attributes", "_Severity_Frequency");
+	        request.setParameter("group", "cycle");
+	        request.setParameter("arms", arms);
+	        request.setParameter("chartType", "line");
+	        request.setMethod("GET");
+	
+	        ModelAndView modelAndView = controller.handleRequest(request, response);
+	        Map m = modelAndView.getModel();
+	        ArrayList<Object[]> charts = (ArrayList<Object[]>) m.get("results");
+	        GraphicalReportTestHelper.showCharts(charts);
+        } else {
+        	fail("Insufficient data in DB while running this test");
+        }
     }
 
     public void testReportControllerSingleArm() throws Exception {
@@ -94,43 +110,53 @@ public class SymptomOverTimeReportTest extends AbstractWebTestCase {
         Study study = StudyTestHelper.getDefaultStudy();
         String arms = study.getArms().get(0).getId() + "_";
         CRF crf = study.getCrfs().get(0);
-        Integer symptomId = crf.getAllCrfPageItems().get(8).getProCtcQuestion().getProCtcTerm().getId();
-
-        SymptomOverTimeReportResultsController controller = new SymptomOverTimeReportResultsController();
-        controller.setGenericRepository(genericRepository);
-        request.setParameter("crf", crf.getId().toString());
-        request.setParameter("symptom", symptomId.toString());
-        request.setParameter("attributes", "_Severity_Frequency");
-        request.setParameter("group", "cycle");
-        request.setParameter("arms", arms);
-        request.setMethod("GET");
-
-        ModelAndView modelAndView = controller.handleRequest(request, response);
-        Map m = modelAndView.getModel();
-        ArrayList<Object[]> charts = (ArrayList<Object[]>) m.get("results");
-        GraphicalReportTestHelper.showCharts(charts);
+        Integer symptomId;
+        int size = crf.getAllCrfPageItems().size();
+        if(size > 0){
+        	symptomId = crf.getAllCrfPageItems().get(size - 1).getProCtcQuestion().getProCtcTerm().getId();
+	        SymptomOverTimeReportResultsController controller = new SymptomOverTimeReportResultsController();
+	        controller.setGenericRepository(genericRepository);
+	        request.setParameter("crf", crf.getId().toString());
+	        request.setParameter("symptom", symptomId.toString());
+	        request.setParameter("attributes", "_Severity_Frequency");
+	        request.setParameter("group", "cycle");
+	        request.setParameter("arms", arms);
+	        request.setMethod("GET");
+	
+	        ModelAndView modelAndView = controller.handleRequest(request, response);
+	        Map m = modelAndView.getModel();
+	        ArrayList<Object[]> charts = (ArrayList<Object[]>) m.get("results");
+	        GraphicalReportTestHelper.showCharts(charts);
+        } else {
+        	fail("Insufficient data in DB while running this test");
+        }
     }
 
     public void testReportDetailsController() throws Exception {
 
         Study study = StudyTestHelper.getDefaultStudy();
         CRF crf = study.getCrfs().get(0);
-        Integer symptomId = crf.getAllCrfPageItems().get(8).getProCtcQuestion().getProCtcTerm().getId();
-
-        ReportDetailsController controller = new ReportDetailsController();
-        controller.setGenericRepository(genericRepository);
-        request.setParameter("crf", crf.getId().toString());
-        request.setParameter("symptom", symptomId.toString());
-        request.setParameter("attributes", "_Severity_Frequency");
-        request.setParameter("group", "week");
-        request.setParameter("col", "Week 26");
-        request.setParameter("ser", "Frequency");
-        request.setParameter("type", "WOR");
-        request.setParameter("sum", "2");
-        request.setMethod("GET");
-
-//        ModelAndView modelAndView = controller.handleRequest(request, response);
-        controller.handleRequest(request, response);
+        Integer symptomId;
+        int size = crf.getAllCrfPageItems().size();
+        if(size > 0){
+        	symptomId = crf.getAllCrfPageItems().get(size - 1).getProCtcQuestion().getProCtcTerm().getId();
+	        ReportDetailsController controller = new ReportDetailsController();
+	        controller.setGenericRepository(genericRepository);
+	        request.setParameter("crf", crf.getId().toString());
+	        request.setParameter("symptom", symptomId.toString());
+	        request.setParameter("attributes", "_Severity_Frequency");
+	        request.setParameter("group", "week");
+	        request.setParameter("col", "Week 26");
+	        request.setParameter("ser", "Frequency");
+	        request.setParameter("type", "WOR");
+	        request.setParameter("sum", "2");
+	        request.setMethod("GET");
+	
+	//        ModelAndView modelAndView = controller.handleRequest(request, response);
+	        controller.handleRequest(request, response);
+        } else {
+        	fail("Insufficient data in DB while running this test");
+        }
     }
 
 
