01d5f6e847af16a6f5c73d59c6644422a3237601
sanea <sanea@ncias-d1508-v.nci.nih.gov>
2013-05-21 18:31:20 +0000

    PRKC-1808: Address testCases

--- pro-ctcae/software/core/src/test/java/gov/nih/nci/ctcae/core/domain/UserTest.java
+++ pro-ctcae/software/core/src/test/java/gov/nih/nci/ctcae/core/domain/UserTest.java
@@ -4,7 +4,10 @@ import junit.framework.TestCase;
 import org.springframework.security.GrantedAuthority;
 import org.springframework.security.GrantedAuthorityImpl;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author Vinay Kumar
@@ -13,6 +16,11 @@ import java.util.List;
 public class UserTest extends TestCase {
 
     private User user;
+    Map<String, List<Role>> userSpecificPrivilegeRoleMap;
+    public static final String PRIVILEGE_CREATE_STUDY = "PRIVILEGE_CREATE_STUDY";
+    public static final String PRIVILEGE_ADD_STUDY_SITE = "PRIVILEGE_ADD_STUDY_SITE";
+    private List<Role> rolesCreateStudy;
+    private List<Role> rolesAddStudtSite;
 
 
     @Override
@@ -22,7 +30,14 @@ public class UserTest extends TestCase {
                 new GrantedAuthorityImpl(Organization.class.getName() + "." + 2), new GrantedAuthorityImpl(Study.class.getName() + "." + 3)};
         user = new User();
         user.setGrantedAuthorities(grantedAuthorities);
-
+        userSpecificPrivilegeRoleMap = new HashMap<String, List<Role>>();
+        rolesCreateStudy = new ArrayList<Role>();
+        rolesCreateStudy.add(Role.ADMIN);
+        rolesCreateStudy.add(Role.PI);
+        userSpecificPrivilegeRoleMap.put(PRIVILEGE_CREATE_STUDY, rolesCreateStudy);
+        rolesAddStudtSite = new ArrayList<Role>();
+        rolesAddStudtSite.add(Role.PI);
+        userSpecificPrivilegeRoleMap.put(PRIVILEGE_ADD_STUDY_SITE, rolesAddStudtSite);
     }
 
     public void testGetAllowedOrganizationId() {
@@ -36,5 +51,19 @@ public class UserTest extends TestCase {
 
         List<Integer> organizationIds = user.findAccessibleObjectIds(Study.class);
         assertTrue(organizationIds.contains(3));
+    }
+    
+    public void testGetUserSpecificPrivilegeRoleMap(){
+    	Map<String, List<Role>> map;
+    	List<Role> roles;
+    	user.setUserSpecificPrivilegeRoleMap(userSpecificPrivilegeRoleMap);
+    	
+    	map = user.getUserSpecificPrivilegeRoleMap();
+    	roles = map.get(PRIVILEGE_CREATE_STUDY);
+    	assertTrue(roles.contains(Role.ADMIN));
+    	
+    	roles = map.get(PRIVILEGE_ADD_STUDY_SITE);
+    	assertFalse(roles.contains(Role.TREATING_PHYSICIAN));
+    	
     }
 }
