64350614264b397c2f09af37554c897f88a45c51
sanea <sanea@ncias-d1508-v.nci.nih.gov>
2013-05-21 18:04:04 +0000

    PRKC-1808: Controlling display of dropdown option menus.

--- pro-ctcae/software/web/src/main/java/gov/nih/nci/ctcae/web/participant/DisplayCalendarController.java
+++ pro-ctcae/software/web/src/main/java/gov/nih/nci/ctcae/web/participant/DisplayCalendarController.java
@@ -1,12 +1,19 @@
 package gov.nih.nci.ctcae.web.participant;
 
+import java.util.List;
+
 import gov.nih.nci.ctcae.core.domain.ParticipantSchedule;
+import gov.nih.nci.ctcae.core.domain.Role;
+import gov.nih.nci.ctcae.core.domain.Study;
+import gov.nih.nci.ctcae.core.domain.User;
 import gov.nih.nci.ctcae.core.repository.GenericRepository;
+import gov.nih.nci.ctcae.core.service.AuthorizationServiceImpl;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Required;
+import org.springframework.security.context.SecurityContextHolder;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.AbstractController;
 
@@ -20,6 +27,10 @@ import org.springframework.web.servlet.mvc.AbstractController;
 public class DisplayCalendarController extends AbstractController {
 
     GenericRepository genericRepository;
+    AuthorizationServiceImpl authorizationServiceImpl;
+    private String PRIVILEGE_PARTICIPANT_DISPLAY_CALENDAR = "PRIVILEGE_PARTICIPANT_DISPLAY_CALENDAR";
+    private String PRIVILEGE_ENTER_PARTICIPANT_RESPONSE = "PRIVILEGE_ENTER_PARTICIPANT_RESPONSE";
+
     /* (non-Javadoc)
      * @see org.springframework.web.servlet.mvc.AbstractController#handleRequestInternal(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
      */
@@ -42,6 +53,20 @@ public class DisplayCalendarController extends AbstractController {
             participantSchedule.getProCtcAECalendar().add(0);
         }
         
+        boolean hasShowCalendarActionsPrivilege = false;
+        boolean hasEnterResponsePrivilege = false;
+
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        Study currentStudy = AuthorizationServiceImpl.getStudy(participantCommand.getParticipant());
+
+        List<Role> roles = authorizationServiceImpl.findRolesForPrivilege(user, PRIVILEGE_PARTICIPANT_DISPLAY_CALENDAR);
+        hasShowCalendarActionsPrivilege = authorizationServiceImpl.hasRole(currentStudy, roles, user);
+        roles = authorizationServiceImpl.findRolesForPrivilege(user, PRIVILEGE_ENTER_PARTICIPANT_RESPONSE);
+        hasEnterResponsePrivilege = authorizationServiceImpl.hasRole(currentStudy, roles, user);
+        
+       	modelAndView.addObject("hasShowCalendarActionsPrivilege",hasShowCalendarActionsPrivilege);
+        modelAndView.addObject("hasEnterResponsePrivilege", hasEnterResponsePrivilege);
+        
         modelAndView.addObject("participantSchedule", participantCommand.getParticipantSchedules().get(index));
         modelAndView.addObject("spa", participantCommand.getSelectedStudyParticipantAssignment());
         modelAndView.addObject("index", index);
@@ -62,5 +87,10 @@ public class DisplayCalendarController extends AbstractController {
     @Required
     public void setGenericRepository(GenericRepository genericRepository) {
         this.genericRepository = genericRepository;
+    }
+    
+    @Required
+    public void setAuthorizationServiceImpl(AuthorizationServiceImpl authorizationServiceImpl) {
+        this.authorizationServiceImpl = authorizationServiceImpl;
     }
 }
