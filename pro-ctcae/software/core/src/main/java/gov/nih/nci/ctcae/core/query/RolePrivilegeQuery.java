05b0cc282f54cb1441bb45a8311874a1ed643f37
sanea <sanea@ncias-d1508-v.nci.nih.gov>
2013-05-21 17:36:23 +0000

    PRKC-1808: Process user privileges to add instancelevelsecurity if applicable.

--- pro-ctcae/software/core/src/main/java/gov/nih/nci/ctcae/core/query/RolePrivilegeQuery.java
+++ pro-ctcae/software/core/src/main/java/gov/nih/nci/ctcae/core/query/RolePrivilegeQuery.java
@@ -12,10 +12,11 @@ import java.util.Set;
 public class RolePrivilegeQuery extends AbstractQuery {
 
     private static String queryString = "SELECT distinct(rp.privilege) from RolePrivilege rp ";
+    private static String findRolePrivileges = "SELECT rp from RolePrivilege rp ";
 
     private static String ROLES = "roleTypes";
     public static final String PRIVILEGE_PARTICIPANT_INBOX = "PRIVILEGE_PARTICIPANT_INBOX";
-
+    private static String PRIVILEGE_NAME = "privilegeName";
 
     /**
      * Instantiates a new clinical staff query.
@@ -23,16 +24,28 @@ public class RolePrivilegeQuery extends AbstractQuery {
     public RolePrivilegeQuery() {
         super(queryString);
     }
-
-
+    
+    public RolePrivilegeQuery(boolean findRolePrivilges){
+    	super(findRolePrivileges);
+    }
+    
     public void filterByRoles(Set<Role> roles) {
         andWhere("rp.role in (:" + ROLES + ")");
         setParameterList(ROLES, roles);
     }
     
+    public void leftJoinPrivilege(){
+    	leftJoin(" rp.privilege as p");
+    }
+    
     public void filterForAdmin(){
     	andWhere("rp.privilege.privilegeName not in (:" + PRIVILEGE_PARTICIPANT_INBOX + ")");
     	setParameter(PRIVILEGE_PARTICIPANT_INBOX, PRIVILEGE_PARTICIPANT_INBOX);
+    }
+    
+    public void filterByPrivilegeName(String privilegeName){
+    	andWhere("rp.privilege.privilegeName in (:" + PRIVILEGE_NAME + ")");
+    	setParameter(PRIVILEGE_NAME, privilegeName);
     }
 
 }
