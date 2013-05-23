103efa038d3197f484c86f85c26b9a1dfaa559dc
gangoliv <gangoliv@ncias-d1508-v.nci.nih.gov>
2013-05-23 19:07:28 +0000

    PRKC-2048: Development. First pass. Supporting both 3L and 5L.

--- pro-ctcae/software/core/src/main/db/migrate/007_elaboration_1/005_add_column_eq5d.groovy
+++ pro-ctcae/software/core/src/main/db/migrate/007_elaboration_1/005_add_column_eq5d.groovy
@@ -2,8 +2,9 @@
 class AddColumnEq5d extends edu.northwestern.bioinformatics.bering.Migration {
 
      void up() {
-        addColumn("crfs", 'is_eq5d', 'boolean', nullable: false);
+        addColumn("crfs", 'is_eq5d', 'boolean', nullable: true);
 	    execute("update crfs set is_eq5d = false");
+	    execute("ALTER TABLE crfs MODIFY is_eq5d boolean NOT NULL");
         
      }
     
