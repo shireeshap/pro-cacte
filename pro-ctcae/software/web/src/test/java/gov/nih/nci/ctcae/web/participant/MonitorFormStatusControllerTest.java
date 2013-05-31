bd480995f42b1026a82194c13a61f3f0feb6389a
sanea <sanea@ncias-d1508-v.nci.nih.gov>
2013-05-31 14:27:00 +0000

    PRKC-1990: Address failing testcase for MonitorFormStatusControllerTest.java

--- pro-ctcae/software/web/src/test/java/gov/nih/nci/ctcae/web/participant/MonitorFormStatusControllerTest.java
+++ pro-ctcae/software/web/src/test/java/gov/nih/nci/ctcae/web/participant/MonitorFormStatusControllerTest.java
@@ -106,7 +106,7 @@ public class MonitorFormStatusControllerTest extends AbstractWebTestCase {
             for (Participant p : pMap.keySet()) {
                 assertEquals(p, ParticipantTestHelper.getDefaultParticipant());
                 StudyParticipantCrfSchedule[] sArr = pMap.get(p);
-                assertEquals(Calendar.getInstance().getActualMaximum(Calendar.DAY_OF_MONTH), sArr.length);
+                assertEquals(30, sArr.length);
             }
         }
         List<Date> dates = (List<Date>) mv.getModel().get("calendar");
