e14e3d64ed3e6b3a64fabd5bad148d42396f3bef
sanea <sanea@ncias-d1508-v.nci.nih.gov>
2013-07-30 19:43:20 +0000

    PRKC-2124: Reverting all the changes for OverallStudyReportExcelController.

--- pro-ctcae/software/web/src/main/java/gov/nih/nci/ctcae/web/reports/OverallStudyReportExcelController.java
+++ pro-ctcae/software/web/src/main/java/gov/nih/nci/ctcae/web/reports/OverallStudyReportExcelController.java
@@ -1,11 +1,5 @@
 package gov.nih.nci.ctcae.web.reports;
 
-import gov.nih.nci.ctcae.core.domain.AddedMeddraQuestionWrapper;
-import gov.nih.nci.ctcae.core.domain.AddedProCtcQuestionWrapper;
-import gov.nih.nci.ctcae.core.domain.ParticipantAndOganizationWrapper;
-import gov.nih.nci.ctcae.core.domain.SpcrfsWrapper;
-
-import java.util.Date;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
@@ -18,25 +12,17 @@ import org.springframework.web.servlet.mvc.AbstractController;
 /**
  * @author mehul
  * Date: Apr 15, 2011
- */	
+ */
 public class OverallStudyReportExcelController extends AbstractController {
     private OverallStudyData overallStudyData;
-    private StudyWideFormatReportData studyWideFormatReportData;
 
     protected ModelAndView handleRequestInternal(HttpServletRequest request, HttpServletResponse httpServletResponse) throws Exception {
 //        int id = Integer.parseInt(request.getParameter("id"));
         Integer id = ServletRequestUtils.getIntParameter(request, "id", -1);
-        Long start = new Date().getTime();
-        List<SpcrfsWrapper> listSchedules = studyWideFormatReportData.getSchedulesOnly(2);
-        List<ResponsesRowMapper> listResoponses = studyWideFormatReportData.getResponsesOnly(2);
-        List<AddedProCtcQuestionWrapper> listAddedProCtcQuestions = studyWideFormatReportData.getAddedProQuestions(2);
-        List<AddedMeddraQuestionWrapper> listAddedMeddraQuestions = studyWideFormatReportData.getAddedMeddraQuestions(2);
-        List<ParticipantAndOganizationWrapper> listParticipantsAndOrganizations = studyWideFormatReportData.getParticipantsAndOrg(2);
-        Long end = new Date().getTime();
-        System.out.println("EndTime: " + (end - start));
-        request.getSession().setAttribute("list", null);
-        //OverallStudyDataExcelView view = new OverallStudyDataExcelView();
-        return null; //new ModelAndView(view);
+        List<Object[]> list = overallStudyData.getStudyData(id);
+        request.getSession().setAttribute("list", list);
+        OverallStudyDataExcelView view = new OverallStudyDataExcelView();
+        return new ModelAndView(view);
     }
 
     public OverallStudyData getOverallStudyData() {
@@ -45,13 +31,5 @@ public class OverallStudyReportExcelController extends AbstractController {
 
     public void setOverallStudyData(OverallStudyData overallStudyData) {
         this.overallStudyData = overallStudyData;
-    }
-    
-    public StudyWideFormatReportData getStudyWideFormatReportData() {
-        return studyWideFormatReportData;
-    }
-
-    public void setStudyWideFormatReportData(StudyWideFormatReportData studyWideFormatReportData) {
-        this.studyWideFormatReportData = studyWideFormatReportData;
     }
 }
