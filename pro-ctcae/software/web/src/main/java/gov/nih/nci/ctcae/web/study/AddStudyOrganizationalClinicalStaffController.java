dc90360c31541493e4719c0354a5321a97112b91
sanea <sanea@ncias-d1508-v.nci.nih.gov>
2013-08-15 18:24:50 +0000

    PRKC-2131: Minor code refactor.

--- pro-ctcae/software/web/src/main/java/gov/nih/nci/ctcae/web/study/AddStudyOrganizationalClinicalStaffController.java
+++ pro-ctcae/software/web/src/main/java/gov/nih/nci/ctcae/web/study/AddStudyOrganizationalClinicalStaffController.java
@@ -7,6 +7,7 @@ import gov.nih.nci.ctcae.web.ControllersUtils;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.lang.StringUtils;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.AbstractController;
 
@@ -23,7 +24,9 @@ public class AddStudyOrganizationalClinicalStaffController extends AbstractContr
 	private static String Add_LCRA = "addLeadCRA";
 	private static String Add_PI = "addPI";
 	private static String Add_ODC = "addODC";
-	
+	private static String LCRA_INDEX_TO_REMOVE = "lcraIndexToRemove";
+	private static String ODC_INDEX_TO_REMOVE = "odcIndexToRemove";
+	private static String PI_INDEX_TO_REMOVE = "piIndexToRemove";
 
 
     protected ModelAndView handleRequestInternal(final HttpServletRequest request, final HttpServletResponse response) throws Exception {
@@ -32,14 +35,23 @@ public class AddStudyOrganizationalClinicalStaffController extends AbstractContr
         ModelAndView modelAndView = null;
         String action = request.getParameter("action");
         if (DELETE_LCRA.equals(action)) {
-            Integer lcraIndexToRemove = Integer.parseInt(request.getParameter("lcraIndexToRemove"));
-            studyCommand.getLCRAIndexesToRemove().add(lcraIndexToRemove);
+        	String lcraIndexToRemoveParam = request.getParameter(LCRA_INDEX_TO_REMOVE);
+        	if(!StringUtils.isEmpty(lcraIndexToRemoveParam)){
+        		Integer lcraIndexToRemove = Integer.parseInt(lcraIndexToRemoveParam);
+        		studyCommand.getLCRAIndexesToRemove().add(lcraIndexToRemove);
+        	}
         } else if(DELETE_ODC.equals(action)){
-        	Integer odcIndexToRemove = Integer.parseInt(request.getParameter("odcIndexToRemove"));
-            studyCommand.getOdcIndexesToRemove().add(odcIndexToRemove);
+        	String odcIndexToRemoveParam = request.getParameter(ODC_INDEX_TO_REMOVE);
+        	if(StringUtils.isEmpty(odcIndexToRemoveParam)){
+        		Integer odcIndexToRemove = Integer.parseInt(odcIndexToRemoveParam);
+        		studyCommand.getOdcIndexesToRemove().add(odcIndexToRemove);
+        	}
         } else if(DELETE_PI.equals(action)){
-        	Integer piIndexToRemove = Integer.parseInt(request.getParameter("piIndexToRemove"));
-            studyCommand.getPiIndexesToRemove().add(piIndexToRemove);
+        	String piIndexToRemoveParam = request.getParameter(PI_INDEX_TO_REMOVE);
+        	if(StringUtils.isEmpty(piIndexToRemoveParam)){
+        		Integer piIndexToRemove = Integer.parseInt(piIndexToRemoveParam);
+        		studyCommand.getPiIndexesToRemove().add(piIndexToRemove);
+        	}
         } else if(Add_LCRA.equals(action)) {
         	modelAndView = new ModelAndView("study/ajax/oneStudyOrganizationalClinicalStaffSection");
 	        StudyOrganizationClinicalStaff socs = new StudyOrganizationClinicalStaff();
