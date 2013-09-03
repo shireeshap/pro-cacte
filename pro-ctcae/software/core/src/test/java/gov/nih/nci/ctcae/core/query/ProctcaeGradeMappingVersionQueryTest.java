e333b0e222b6154c9dc767b3e1fba1101580d865
sanea <sanea@ncias-d1508-v.nci.nih.gov>
2013-09-03 19:57:19 +0000

    PRKC-2156: ProctcaeGradeMappingVersion domain class and related testcases

---
+++ pro-ctcae/software/core/src/test/java/gov/nih/nci/ctcae/core/query/ProctcaeGradeMappingVersionQueryTest.java
@@ -0,0 +1,21 @@
+package gov.nih.nci.ctcae.core.query;
+
+import junit.framework.TestCase;
+
+public class ProctcaeGradeMappingVersionQueryTest extends TestCase{
+	
+	private static String VERSION = "version";
+	private static String VERSION_NUMBER = "v1.0";
+	
+	public void testQueryConstructor(){
+		ProctcaeGradeMappingVersionQuery query = new ProctcaeGradeMappingVersionQuery();
+		assertEquals(query.getQueryString(), "select gmv from GradeMappingVersion gmv");
+	}
+	
+	public void testFilterByVersion(){
+		ProctcaeGradeMappingVersionQuery query = new ProctcaeGradeMappingVersionQuery();
+		query.filterByVersion(VERSION_NUMBER);
+		assertEquals(query.getQueryString(), "select gmv from GradeMappingVersion gmv WHERE gmv.version := version");
+		assertEquals(query.getParameterMap().get(VERSION), VERSION_NUMBER);
+	}
+}
