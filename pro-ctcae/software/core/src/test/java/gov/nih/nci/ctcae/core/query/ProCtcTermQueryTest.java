c885aa61da03e40b60d353f13775013c3e4d273c
gangoliv <gangoliv@ncias-d1508-v.nci.nih.gov>
2013-09-04 15:17:10 +0000

    PRKC-1990: updated test cases.

--- pro-ctcae/software/core/src/test/java/gov/nih/nci/ctcae/core/query/ProCtcTermQueryTest.java
+++ pro-ctcae/software/core/src/test/java/gov/nih/nci/ctcae/core/query/ProCtcTermQueryTest.java
@@ -12,28 +12,29 @@ public class ProCtcTermQueryTest extends TestCase {
         ProCtcTermQuery query = new ProCtcTermQuery();
         query.filterByCtcTermHavingQuestionsOnly();
         assertEquals("SELECT distinct(o) from ProCtcTerm o left join o.proCtcQuestions as proCtcQuestion left join proCtcQuestion.validValues left join o.ctcTerm.categoryTermSets as categoryTerm inner join o.proCtcQuestions WHERE " +
-        		"categoryTerm.category.ctc.name = :ctcName", query.getQueryString());
+        		" categoryTerm.category.ctc.name = :ctcName AND  categoryTerm.category.name not in ('EQ5D-5L', 'EQ5D-3L') ", query.getQueryString());
     }
 
     public void testFilterByCtcCategoryId() {
         ProCtcTermQuery query = new ProCtcTermQuery();
         query.filterByCtcCategoryId(1);
 		assertEquals("SELECT distinct(o) from ProCtcTerm o left join o.proCtcQuestions as proCtcQuestion left join proCtcQuestion.validValues left join o.ctcTerm.categoryTermSets as categoryTerm WHERE " +
-				"categoryTerm.category.ctc.name = :ctcName AND categoryTerm.category.id = :ctcCategoryId",query.getQueryString());
+				" categoryTerm.category.ctc.name = :ctcName AND categoryTerm.category.id = :ctcCategoryId AND  categoryTerm.category.name not in ('EQ5D-5L', 'EQ5D-3L') ",query.getQueryString());
     }
 
     public void testFilterByCtcTermId() {
         ProCtcTermQuery query = new ProCtcTermQuery();
         query.filterByCtcTermId(1);
         assertEquals("SELECT distinct(o) from ProCtcTerm o left join o.proCtcQuestions as proCtcQuestion left join proCtcQuestion.validValues left join o.ctcTerm.categoryTermSets as categoryTerm WHERE" +
-        		" o.ctcTerm.id = :ctcTermId AND categoryTerm.category.ctc.name = :ctcName", query.getQueryString());
+        		" o.ctcTerm.id = :ctcTermId AND  categoryTerm.category.ctc.name = :ctcName AND  categoryTerm.category.name not in ('EQ5D-5L', 'EQ5D-3L') ", query.getQueryString());
     }
 
     public void testFilterByTerm() {
         ProCtcTermQuery query = new ProCtcTermQuery();
         query.filterByTerm("test");
+
         assertEquals("SELECT distinct(o) from ProCtcTerm o left join o.proCtcQuestions as proCtcQuestion left join proCtcQuestion.validValues left join o.ctcTerm.categoryTermSets as categoryTerm WHERE " +
-        		"categoryTerm.category.ctc.name = :ctcName AND lower(o.proCtcTermVocab.termEnglish) = :symptom", query.getQueryString());
+        		" categoryTerm.category.ctc.name = :ctcName AND lower(o.proCtcTermVocab.termEnglish) = :symptom AND  categoryTerm.category.name not in ('EQ5D-5L', 'EQ5D-3L') ", query.getQueryString());
 
     }
 
