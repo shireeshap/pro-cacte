daf3f628bf0d501bdff270592420cccc580d8558
sanea <sanea@ncias-d1508-v.nci.nih.gov>
2013-09-20 18:54:41 +0000

    PRKC-2176: Testcases for controller handling ACCRU AE report generation

---
+++ pro-ctcae/software/web/src/test/java/gov/nih/nci/ctcae/web/reports/ParticipantLevelCtcaeGradesReportResultsControllerTest.java
@@ -0,0 +1,90 @@
+package gov.nih.nci.ctcae.web.reports;
+
+import java.util.List;
+
+import org.springframework.web.servlet.ModelAndView;
+
+import gov.nih.nci.ctcae.core.domain.AppMode;
+import gov.nih.nci.ctcae.core.domain.CrfStatus;
+import gov.nih.nci.ctcae.core.domain.Participant;
+import gov.nih.nci.ctcae.core.domain.ProCtcValidValue;
+import gov.nih.nci.ctcae.core.domain.ProctcaeGradeMappingVersion;
+import gov.nih.nci.ctcae.core.domain.Study;
+import gov.nih.nci.ctcae.core.domain.StudyParticipantCrfItem;
+import gov.nih.nci.ctcae.core.domain.StudyParticipantCrfSchedule;
+import gov.nih.nci.ctcae.core.helper.ParticipantTestHelper;
+import gov.nih.nci.ctcae.core.helper.StudyTestHelper;
+import gov.nih.nci.ctcae.core.query.ProctcaeGradeMappingVersionQuery;
+import gov.nih.nci.ctcae.core.query.StudyParticipantCrfScheduleQuery;
+import gov.nih.nci.ctcae.web.AbstractWebTestCase;
+
+public class ParticipantLevelCtcaeGradesReportResultsControllerTest extends AbstractWebTestCase{
+	private Study defaultStudy;
+	private Participant defaultParticipant;
+	private static String STUDY_ID = "studyId";
+	private static String PARTICIPANT_ID = "participantId";
+	private static String VISIT_RANGE = "visitRange";
+	private static String AE_WITH_GRADES_LIST = "adverseEventsWithGradesList";
+	ParticipantLevelCtcaeGradesReportResultsController controller;
+	
+	@Override
+	protected void onSetUpInTransaction() throws Exception {
+		super.onSetUpInTransaction();
+		defaultStudy = StudyTestHelper.getDefaultStudy();
+		defaultParticipant = getDefaultParticipant();
+		request.setParameter(STUDY_ID, defaultStudy.getId().toString());
+		request.setParameter(PARTICIPANT_ID, defaultParticipant.getId().toString());
+		request.setAttribute(VISIT_RANGE, "dateRange");
+		controller = new ParticipantLevelCtcaeGradesReportResultsController();
+		controller.setGenericRepository(genericRepository);
+		request.setMethod("GET");
+	}
+	
+	public void testHandleRequestInternal(){
+		ModelAndView modelAndView = new ModelAndView();
+		try {
+			setUpData();
+			modelAndView = controller.handleRequest(request, response);
+			assertEquals("reports/participantLevelCtcaeGradesReportResults", modelAndView.getViewName());
+			assertFalse(((List<AeReportEntryWrapper>)request.getSession().getAttribute(AE_WITH_GRADES_LIST)).isEmpty());
+			
+		} catch (Exception e) {
+			System.out.println("Failure in testHandleRequestInternal() " +e.getStackTrace());
+		}
+		
+	}
+	
+	private void setUpData() throws Exception{
+		ParticipantTestHelper.completeParticipantSchedule(defaultParticipant, defaultStudy.getStudySites().get(0), false, AppMode.HOMEWEB);
+		
+		ProctcaeGradeMappingVersionQuery versionQuery = new ProctcaeGradeMappingVersionQuery();
+		ProctcaeGradeMappingVersion proctcaeGradeMappingVersion = genericRepository.findSingle(versionQuery);
+		
+		StudyParticipantCrfScheduleQuery query = new StudyParticipantCrfScheduleQuery();
+        Integer studyId = Integer.parseInt(request.getParameter("studyId"));
+        Integer participantId = Integer.parseInt(request.getParameter("participantId"));
+        query.filterByStudy(studyId);
+        query.filterByParticipant(participantId);
+        query.filterByStatus(CrfStatus.COMPLETED);
+        List<StudyParticipantCrfSchedule> list = genericRepository.find(query);
+        for(StudyParticipantCrfSchedule studyParticipantCrfSchedule : list){
+        	/*  responseCode values in ProCtcValidValue table are updated through loaders.
+        	 *  Mocking up the same behavior below for this testCase. 
+        	 */
+        	for(StudyParticipantCrfItem spCrfItem : studyParticipantCrfSchedule.getStudyParticipantCrfItems()){
+        		ProCtcValidValue proCtcValidValue = spCrfItem.getProCtcValidValue();
+        		if(proCtcValidValue != null){
+        			proCtcValidValue.setResponseCode(proCtcValidValue.getDisplayOrder());
+        		}
+        	}
+        	/*
+        	 *  generate grades for each studyParticipantCrfSchedule. 
+        	 */
+        	studyParticipantCrfSchedule.generateStudyParticipantCrfGrades(proctcaeGradeMappingVersion);
+        }
+	}
+	
+	 public Participant getDefaultParticipant(){
+	    	return StudyTestHelper.getDefaultStudy().getArms().get(0).getStudyParticipantAssignments().get(0).getParticipant();
+	 }
+}
