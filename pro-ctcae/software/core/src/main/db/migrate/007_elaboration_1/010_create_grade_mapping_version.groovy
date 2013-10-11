e284ad50bd240e8bced8d26f73b0f7dee62e04c1
gangoliv <gangoliv@ncias-d1508-v.nci.nih.gov>
2013-10-11 15:51:38 +0000

    Updated the document name to be the name of the csv in core/src/resources

--- pro-ctcae/software/core/src/main/db/migrate/007_elaboration_1/010_create_grade_mapping_version.groovy
+++ pro-ctcae/software/core/src/main/db/migrate/007_elaboration_1/010_create_grade_mapping_version.groovy
@@ -4,7 +4,7 @@ class CreateGradeMappingVersion extends edu.northwestern.bioinformatics.bering.M
 			t.addColumn('version', 'string', nullable:false)
 			t.addColumn('name', 'string', nullable: false)
 			}
-			execute("insert into proctcae_grade_mapping_versions (version, name) values ('v1.0', 'dummy_documentName_placeHolder')");
+			execute("insert into proctcae_grade_mapping_versions (version, name) values ('v1.0', 'PRO-CTCAE-GradeMapping_v1.1_08.29.13.csv')");
  	}
  		void down() {
 		dropTable("proctcae_grade_mapping_versions")
