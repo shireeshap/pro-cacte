84644255b3d8cb57fa83021b2a83df2cc8b9662a
sanea <sanea@ncias-d1508-v.nci.nih.gov>
2013-10-29 15:40:30 +0000

    PRKC-2337: Fix display Issues for Participant Added Symptoms report

--- pro-ctcae/software/web/src/main/java/gov/nih/nci/ctcae/web/reports/ParticipantAddedQuestionsReportDetailsController.java
+++ pro-ctcae/software/web/src/main/java/gov/nih/nci/ctcae/web/reports/ParticipantAddedQuestionsReportDetailsController.java
@@ -21,18 +21,25 @@ import org.springframework.web.servlet.ModelAndView;
  * Time: 9:34:01 AM
  */
 public class ParticipantAddedQuestionsReportDetailsController extends ParticipantAddedQuestionsReportResultsController {
-
+	private static String NOT_ANSWERED = "Not answered"; 
 
     protected ModelAndView handleRequestInternal(HttpServletRequest request, HttpServletResponse response) throws Exception {
 
         ModelAndView modelAndView = new ModelAndView("reports/participantAddedQuestionsDetails");
 
+        boolean isMeddraAddedQuestionQuery = false;
         ParticipantAddedQuestionsDetailsQuery query = new ParticipantAddedQuestionsDetailsQuery();
-        parseRequestParametersAndFormQuery(request, query);
-
+        parseRequestParametersAndFormQuery(request, query, isMeddraAddedQuestionQuery);
         List l = genericRepository.find(query);
+        
+        if(l.isEmpty()){
+        	isMeddraAddedQuestionQuery = true;
+        	query = new ParticipantAddedQuestionsDetailsQuery();
+            parseRequestParametersAndFormQuery(request, query, isMeddraAddedQuestionQuery);
+            l = genericRepository.find(query);
+        }
+        
         HashMap<Participant, List<String[]>> results = new HashMap<Participant, List<String[]>>();
-
         for (Object obj : l) {
             StudyParticipantCrfScheduleAddedQuestion sq = (StudyParticipantCrfScheduleAddedQuestion) obj;
 
@@ -45,8 +52,16 @@ public class ParticipantAddedQuestionsReportDetailsController extends Participan
                 results.put(p, ls);
             }
             String[] s = new String[2];
-            s[0] = DateUtils.format(sq.getStudyParticipantCrfSchedule().getStartDate());
-            s[1] = sq.getProCtcValidValue() == null ? "" : sq.getProCtcValidValue().getValue(SupportedLanguageEnum.ENGLISH);
+            s[0] = (sq.getReponseDate() != null? DateUtils.format(sq.getReponseDate()) : DateUtils.format(sq.getStudyParticipantCrfSchedule().getStartDate()));
+            String value;
+            if(sq.getProCtcQuestion() != null){
+            	value = sq.getProCtcValidValue() == null ? "" : sq.getProCtcValidValue().getValue(SupportedLanguageEnum.ENGLISH);
+            	s[1] = sq.getProCtcQuestion().getProCtcQuestionType().getDisplayName() + ": " + (value.isEmpty()? NOT_ANSWERED : value);
+            } else {
+            	value = sq.getMeddraValidValue() == null ? "" : sq.getMeddraValidValue().getValue(SupportedLanguageEnum.ENGLISH);
+            	s[1] = sq.getMeddraQuestion().getQuestionType().getDisplayName() + ": " + (value.isEmpty()? NOT_ANSWERED : value);
+            }
+            
             ls.add(s);
         }
 
