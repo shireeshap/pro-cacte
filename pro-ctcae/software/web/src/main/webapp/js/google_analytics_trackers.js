b09149b9fc184844864c8d51f615d0e70d8876df
sanea <sanea@ncias-d1508-v.nci.nih.gov>
2015-01-19 19:31:12 +0000

    PRKC-2436: Modify SB-DEV hostname condition to turn off GA. (Need to test page load time without using GA.)

--- pro-ctcae/software/web/src/main/webapp/js/google_analytics_trackers.js
+++ pro-ctcae/software/web/src/main/webapp/js/google_analytics_trackers.js
@@ -1,7 +1,7 @@
 // Determine the tier using hostname from the URL.
 
  function isTier_SB_DEV() {
-	 if(document.location.hostname == 'dev.semanticbits.com') {
+	 if(document.location.hostname == 'dev_tier_turn_off_ga.semanticbits.com') {
 		 return true;
 	 }
 	 return false;
