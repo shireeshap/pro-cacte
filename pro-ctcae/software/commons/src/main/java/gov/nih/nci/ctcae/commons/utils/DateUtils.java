861a3c137c34fdb857a4cf3b0c8105d9656f7aa9
sanea <sanea@ncias-d1508-v.nci.nih.gov>
2015-07-24 21:38:32 +0000

    PRKC-2398: Revert revision 5759. (This function has other dependencies hence cannot modify)

--- pro-ctcae/software/commons/src/main/java/gov/nih/nci/ctcae/commons/utils/DateUtils.java
+++ pro-ctcae/software/commons/src/main/java/gov/nih/nci/ctcae/commons/utils/DateUtils.java
@@ -162,10 +162,10 @@ public class DateUtils extends edu.nwu.bioinformatics.commons.DateUtils {
 	    cal.set(Calendar.YEAR, mbCal.get(Calendar.YEAR));
 	    cal.set(Calendar.MONTH, mbCal.get(Calendar.MONTH));
 	    cal.set(Calendar.DAY_OF_MONTH, mbCal.get(Calendar.DAY_OF_MONTH));
-	    cal.set(Calendar.HOUR_OF_DAY, 0);
-	    cal.set(Calendar.MINUTE, 0);
-	    cal.set(Calendar.SECOND, 0);
-	    cal.set(Calendar.MILLISECOND, 0) ;
+	    cal.set(Calendar.HOUR_OF_DAY, mbCal.get(Calendar.HOUR_OF_DAY));
+	    cal.set(Calendar.MINUTE, mbCal.get(Calendar.MINUTE));
+	    cal.set(Calendar.SECOND, mbCal.get(Calendar.SECOND));
+	    cal.set(Calendar.MILLISECOND, mbCal.get(Calendar.MILLISECOND)) ;
 	
 	    return cal.getTime();
     }
